6d40866d4c88ab41c6497eb7846ba14d6458d72a
Samuel Gomez <samuel.gomez@axa.fr>
2024-01-10 15:27:14 +0100

    fix(eslint): rules-of-hook for useid

--- packages/react/src/Form/core/useOptionsWithId.ts
+++ packages/react/src/Form/core/useOptionsWithId.ts
@@ -1,13 +1,13 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useId } from "react";
 import type { Option } from ".";
 
 export function useOptionsWithId(options?: Option[], id?: string) {
+  const reactId = useId();
   if (!options) {
     return [];
   }
   return options.map((option, index) => {
-    const newOptionId = id ? `${id}_${index}` : useId();
+    const newOptionId = id ? `${id}_${index}` : reactId;
     return { ...option, id: option.id ?? newOptionId };
   });
 }
