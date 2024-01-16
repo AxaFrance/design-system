eaa319c04a247f079594ae7faee671041822a2d2
Corentin MARQUANT <B079VU@login.axa>
2024-01-16 16:20:50 +0100

    docs(form): add textarea and textareaInput stories

---
+++ packages/react/src/Form/Textarea/Textarea.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Textarea } from "./Textarea";
+
+const MODIFIERS = ["", "required", "disabled"];
+
+const meta: Meta<typeof Textarea> = {
+  component: Textarea,
+  title: "Components/Form/Input/Textarea",
+  argTypes: { onChange: { action: "onChange" } },
+};
+
+export default meta;
+
+type StoryProps = Omit<
+  React.ComponentProps<typeof Textarea>,
+  "classModifier"
+> & {
+  classModifier: string[];
+};
+type Story = StoryObj<StoryProps>;
+
+export const TextareaStory: Story = {
+  name: "Textarea",
+  render: ({ classModifier, onChange, ...args }) => (
+    <Textarea
+      classModifier={classModifier.join(" ")}
+      onChange={onChange}
+      {...args}
+    />
+  ),
+  args: {
+    classModifier: [] as string[],
+    value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    placeholder: "Your comment",
+    name: "comment",
+    id: "commentid",
+    readOnly: false,
+    disabled: false,
+    tabIndex: 0,
+    autoFocus: false,
+    className: "",
+  },
+  argTypes: {
+    classModifier: {
+      options: MODIFIERS,
+      control: { type: "multi-select" },
+    },
+  },
+};
---
+++ packages/react/src/Form/Textarea/TextareaInput.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { MessageTypes } from "../core";
+import { TextareaInput } from "./TextareaInput";
+
+const meta: Meta<typeof TextareaInput> = {
+  component: TextareaInput,
+  title: "Components/Form/Input/TextareaInput",
+  argTypes: { onChange: { action: "onChange" } },
+};
+
+export default meta;
+
+type Story = StoryObj<React.ComponentProps<typeof TextareaInput>>;
+
+export const TextareaInputStory: Story = {
+  name: "TextareaInput",
+  render: ({ onChange, ...args }) => (
+    <TextareaInput onChange={onChange} {...args} />
+  ),
+  args: {
+    label: "Comment",
+    name: "comment",
+    id: "commentid",
+    value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    rows: 6,
+    cols: 60,
+    placeholder: "",
+    helpMessage: "Enter a comment",
+    message: "",
+    messageType: MessageTypes.error,
+    forceDisplayMessage: false,
+    readOnly: false,
+    disabled: false,
+    isVisible: true,
+    classModifier: "",
+    className: "",
+    tabIndex: 0,
+    autoFocus: true,
+    classNameContainerLabel: "col-md-2",
+    classNameContainerInput: "col-md-10",
+  },
+};
