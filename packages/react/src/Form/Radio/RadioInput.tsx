a6ac7b21a20ecc6e4e832253ce7d1c4a32abc8fd
JLou <jeanlou.pierme@gmail.com>
2024-01-23 17:09:57 +0100

    feat(Radio): add agent Radio components
    fixes #22

---
+++ packages/react/src/Form/Radio/RadioInput.tsx
@@ -0,0 +1,53 @@
+import { ComponentPropsWithoutRef } from "react";
+import { Field, getFirstId, useOptionsWithId } from "../core";
+import { Radio, RadioModes } from "./Radio";
+
+type RadioInputProps = ComponentPropsWithoutRef<typeof Field> &
+  ComponentPropsWithoutRef<typeof Radio>;
+
+const RadioInput = ({
+  mode,
+  messageType,
+  message,
+  className,
+  classModifier,
+  isVisible,
+  options,
+  classNameContainerLabel,
+  classNameContainerInput,
+  label,
+  forceDisplayMessage,
+  children,
+  ...radioProps
+}: RadioInputProps) => {
+  const rowModifier = `${classModifier ?? ""}${
+    mode === RadioModes.classic ? " label-top" : ""
+  }`;
+  const newOptions = useOptionsWithId(options);
+  const firstId = getFirstId(newOptions);
+
+  return (
+    <Field
+      label={label}
+      id={firstId}
+      message={message}
+      messageType={messageType}
+      isVisible={isVisible}
+      forceDisplayMessage={forceDisplayMessage}
+      className={className}
+      classModifier={rowModifier}
+      classNameContainerLabel={classNameContainerLabel}
+      classNameContainerInput={classNameContainerInput}
+    >
+      <Radio
+        options={newOptions}
+        mode={mode}
+        classModifier={classModifier}
+        {...radioProps}
+      />
+      {children}
+    </Field>
+  );
+};
+
+export { RadioInput };
