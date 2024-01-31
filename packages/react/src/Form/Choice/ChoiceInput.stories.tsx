0ab678cfd9d058860fcf7422d08e609213cb4cc0
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-01-31 12:01:22 +0100

    feat(Choice): add agent Choice components

---
+++ packages/react/src/Form/Choice/ChoiceInput.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ComponentPropsWithRef } from "react";
+import { MessageTypes } from "../core";
+import { ChoiceInput } from "./ChoiceInput";
+
+const meta: Meta<typeof ChoiceInput> = {
+  component: ChoiceInput,
+  title: "Components/Form/Input/Choice",
+  argTypes: { onChange: { action: "onChange" } },
+};
+
+export default meta;
+
+type ChoiceInputProps = Omit<
+  ComponentPropsWithRef<typeof ChoiceInput>,
+  "classModifier"
+> & {
+  classModifier?: string[];
+};
+
+export const ChoiceInputStory: StoryObj<ChoiceInputProps> = {
+  name: "ChoiceInput",
+  render: ({ classModifier, ...args }) => (
+    <ChoiceInput classModifier={classModifier?.join(" ")} {...args} />
+  ),
+  args: {
+    name: "placeName",
+    label: "Place type",
+    placeholder: "Paris",
+    message: "",
+    messageType: MessageTypes.error,
+    forceDisplayMessage: false,
+    id: "uniqueid",
+    classModifier: [],
+    isVisible: true,
+    readOnly: false,
+    disabled: false,
+  },
+  argTypes: {
+    onChange: { action: "onChange" },
+    classModifier: {
+      options: ["disabled", "required"],
+      control: { type: "inline-check" },
+    },
+  },
+};
