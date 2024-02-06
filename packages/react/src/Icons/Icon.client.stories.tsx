77cbc343821fa8671dbe577f0bcb37744c1b7544
fffan64 <ff.fan@hotmail.fr>
2024-02-06 15:39:39 +0100

    Feature/icons client (#114)
    * feat(icons): ajout stories icons mui material client
    
    * ci: fix warning on bundling files containing 'use client'
    
    ---------
    
    Co-authored-by: Jonathan VACHERAT <jonathan.vacherat@axa.fr>

---
+++ packages/react/src/Icons/Icon.client.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import * as Icons from "@mui/icons-material";
+import "./Icon.client.stories.css";
+
+const meta: Meta = {
+  title: "Look'n feel/Icons",
+};
+
+export default meta;
+
+export const Example: StoryObj = {
+  name: "Icon",
+  render: ({ ...args }) => <Icons.Headphones {...args} />,
+  args: {},
+};
+
+export const AllIcons: StoryObj = {
+  render: () => {
+    return (
+      <ul className="icon-list">
+        {Object.values(Icons).map((Comp: Icons.SvgIconComponent) => {
+          return (
+            <li key={Comp.muiName}>
+              <Comp />
+              <span>key</span>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  },
+};
