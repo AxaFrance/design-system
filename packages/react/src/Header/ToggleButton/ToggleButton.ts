5520f719d319c3f187e639c1eea8814962b70143
NOURELAYNE Salah eddine <salaheddine.nourelayne@axa.fr>
2024-02-13 22:40:13 +0100

    feat(Header): implement Toggle Button component
    fixes #32

---
+++ packages/react/src/Header/ToggleButton/ToggleButton.ts
@@ -0,0 +1,15 @@
+import { cloneElement, isValidElement, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  idControl: string;
+};
+const ToggleButton = ({ children, idControl }: Props) =>
+  isValidElement(children) &&
+  cloneElement(children, {
+    ...children.props,
+    "aria-controls": idControl,
+    "aria-haspopup": true,
+  });
+
+export { ToggleButton };
