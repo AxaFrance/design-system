87fa7d9a1dd9806106afb503331e1c5bb7b805f0
TRIKI Ahmed <ahmed.triki.altersolutions@axa.fr>
2024-02-16 10:55:39 +0100

    feat(badge-component): badge component done

---
+++ packages/react/src/Badge/Badge.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithRef, PropsWithChildren, forwardRef } from "react";
+import { getComponentClassName } from "../Form/core";
+import "@axa-fr/design-system-css/dist/Badge/Badge.scss";
+
+type BadgeProps = ComponentPropsWithRef<"span"> & {
+  classModifier?: string;
+  disabled?: boolean;
+};
+
+export const Badge = forwardRef<HTMLSpanElement, PropsWithChildren<BadgeProps>>(
+  ({ children, className, classModifier, ...otherProps }, ref) => {
+    const componentClassName = getComponentClassName(
+      className,
+      classModifier,
+      "af-badge",
+    );
+    return (
+      <span ref={ref} className={componentClassName} {...otherProps}>
+        {children}
+      </span>
+    );
+  },
+);
+
+Badge.displayName = "Badge";
