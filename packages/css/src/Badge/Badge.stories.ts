47cedd5fa6536526071cac43e2606f3d150b8630
TRIKI Ahmed <ahmed.triki.altersolutions@axa.fr>
2024-02-16 10:55:39 +0100

    feat(badge-component): badge component done

---
+++ packages/css/src/Badge/Badge.stories.ts
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from "@storybook/html";
+import "./Badge.scss";
+
+const meta: Meta = {
+  title: "Agent/Components/Badge",
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+const MODIFIERS = ["success", "info", "danger", "error"];
+
+export const Default: StoryObj = {
+  name: "Badge",
+  render: (args) => {
+    const badge = document.createElement("span");
+
+    if (args.disabled) badge.setAttribute("disabled", "");
+
+    badge.innerHTML = args.children;
+
+    badge.className += [
+      "af-badge",
+      args.classModifier ? `af-badge--${args.classModifier}` : "",
+    ].join(" ");
+
+    return badge;
+  },
+  args: {
+    children: "Lorem ipsum",
+    classModifier: "success",
+    disabled: false,
+  },
+  argTypes: {
+    classModifier: {
+      options: MODIFIERS,
+      control: { type: "select" },
+      defaultValue: "success",
+    },
+  },
+};
+
+export const BadgeIconStory: StoryObj = {
+  name: "Badge with icon",
+  render: (args) => {
+    const badge = document.createElement("span");
+
+    if (args.disabled) badge.setAttribute("disabled", "");
+
+    badge.innerHTML = args.children;
+
+    badge.className += [
+      "af-badge",
+      args.classModifier ? `af-badge--${args.classModifier}` : "",
+    ].join(" ");
+
+    return badge;
+  },
+  args: {
+    children: `<i class="glyphicon glyphicon-bell"> </i>`,
+    classModifier: "error",
+    disabled: false,
+  },
+  argTypes: {
+    classModifier: {
+      options: MODIFIERS,
+      control: { type: "select" },
+      defaultValue: "success",
+    },
+  },
+};
---
+++ packages/react/src/Badge/Badge.stories.tsx
@@ -0,0 +1,109 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Badge } from "./Badge";
+
+const meta: Meta<typeof Badge> = {
+  title: "Agent/Components/Badge",
+  component: Badge,
+  parameters: {
+    layout: "centered",
+  },
+};
+export default meta;
+
+export const Default: StoryObj<typeof Badge> = {
+  name: "Badge",
+  args: {
+    children: "Lorem ipsum",
+    classModifier: "success",
+    disabled: false,
+  },
+  argTypes: {
+    classModifier: {
+      options: ["success", "info", "danger", "error"],
+      control: { type: "select" },
+      defaultValue: "succes",
+    },
+  },
+};
+export const BadgeIconStory: StoryObj<typeof Badge> = {
+  name: "Badge With Icon",
+  args: {
+    children: <i className="glyphicon glyphicon-bell" />,
+    classModifier: "error",
+    disabled: false,
+  },
+  argTypes: {
+    classModifier: {
+      options: ["success", "info", "danger", "error"],
+      control: { type: "select" },
+      defaultValue: "error",
+    },
+  },
+};
+
+export const MultiExamples: StoryObj<typeof Badge> = {
+  name: "Badge with modifiers",
+  render: (args) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexDirection: "column",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            padding: "2rem",
+            flexWrap: "wrap",
+            gap: "2rem",
+          }}
+        >
+          <Badge disabled={args.disabled} classModifier="success">
+            Badge Success
+          </Badge>
+          <Badge disabled={args.disabled} classModifier="danger">
+            Badge Danger
+          </Badge>
+          <Badge disabled={args.disabled} classModifier="info">
+            Badge Info
+          </Badge>
+          <Badge disabled={args.disabled} classModifier="error">
+            Badge Error
+          </Badge>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            padding: "2rem",
+            flexWrap: "wrap",
+            gap: "5rem",
+          }}
+        >
+          <Badge disabled={args.disabled} classModifier="success">
+            <i className="glyphicon glyphicon-bell" />
+          </Badge>
+          <Badge disabled={args.disabled} classModifier="danger">
+            <i className="glyphicon glyphicon-bell" />
+          </Badge>
+          <Badge disabled={args.disabled} classModifier="info">
+            <i className="glyphicon glyphicon-bell" />
+          </Badge>
+          <Badge disabled={args.disabled} classModifier="error">
+            <i className="glyphicon glyphicon-bell" />
+          </Badge>
+        </div>
+      </div>
+    );
+  },
+  args: {
+    disabled: false,
+  },
+};
