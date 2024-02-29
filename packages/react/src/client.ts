bfe8cab6ca194b36721864bfe0edefd9f9696feb
Rémy CODRON <remy.codron@axa.fr>
2024-02-29 11:16:39 +0100

    feat(client): tabs component

--- packages/react/src/client.ts
+++ packages/react/src/client.ts
@@ -7,3 +7,7 @@ export {
 export { Header } from "./Header/Header.client";
 export { Radio, RadioInput, RadioItem, RadioModes } from "./Form/Radio/Client";
 export { Footer } from "./Footer/Footer.client";
+export {
+  TabsClient as Tabs,
+  Direction as TabsDirection,
+} from "./Tabs/Tabs.client";
