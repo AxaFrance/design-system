fae98ccb5982d4ebc8b00fe353a75e11d867abfa
Jonathan VACHERAT <jonathan.vacherat@axa.fr>
2024-03-15 15:17:02 +0100

    feat: add Link client

---
+++ packages/react/src/Link/Link.client.tsx
@@ -0,0 +1,38 @@
+import { ComponentPropsWithoutRef, PropsWithChildren } from "react";
+import OpenInNew from "@mui/icons-material/OpenInNew";
+import "@axa-fr/design-system-css/dist/Link/Link.client.scss";
+
+export type AlertType =
+  | "validation"
+  | "error"
+  | "warning"
+  | "information"
+  | "neutral";
+
+type LinkProps = {
+  url: string;
+  openInNewTab?: boolean;
+} & ComponentPropsWithoutRef<"a">;
+
+export const Link = ({
+  url,
+  openInNewTab,
+  children,
+  ...props
+}: PropsWithChildren<LinkProps>) => {
+  const newTabProps = openInNewTab && {
+    target: "_blank",
+    rel: "noopener noreferrer",
+  };
+  return (
+    <a
+      className={`af-link ${openInNewTab ? " af-link--openInNewTab" : ""}`}
+      href={url}
+      {...newTabProps}
+      {...props}
+    >
+      {children}
+      {newTabProps && <OpenInNew />}
+    </a>
+  );
+};
