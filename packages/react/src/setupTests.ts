a49fcd40371996ec3d060760e1ef9becb42e8b72
Jérémy Delfly <jeremy.delfly@gmail.com>
2024-03-26 14:27:48 +0100

    chore(test): update import of setup

--- packages/react/src/setupTests.ts
+++ packages/react/src/setupTests.ts
@@ -1,10 +1,9 @@
-import * as matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { toHaveNoViolations } from "jest-axe";
 import { afterEach, expect } from "vitest";
 
 expect.extend(toHaveNoViolations);
-expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
