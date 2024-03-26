7243f95bbbe26374460298098bd4013db2d91faa
Jérémy Delfly <jeremy.delfly@gmail.com>
2024-03-26 09:47:28 +0100

    build(vitest): add vitest

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-default-export */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
+});
