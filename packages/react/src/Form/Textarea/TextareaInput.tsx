074a9ae4c510f8ec233b8cc61de39537b9c81ba4
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-05-13 15:19:50 +0200

    feat(Agent Textarea): add forward ref

--- packages/react/src/Form/Textarea/TextareaInput.tsx
+++ packages/react/src/Form/Textarea/TextareaInput.tsx
@@ -1,8 +1,8 @@
-import { ComponentProps, ReactNode, useId } from "react";
-import { Field, HelpMessage, FieldInput, useInputClassModifier } from "../core";
-import "@axa-fr/design-system-css/dist/common/reboot.scss";
 import "@axa-fr/design-system-css/dist/common/grid.scss";
+import "@axa-fr/design-system-css/dist/common/reboot.scss";
 import "@axa-fr/design-system-css/dist/Form/core/FormCore.agent.scss";
+import { ComponentProps, forwardRef, ReactNode, useId } from "react";
+import { Field, FieldInput, HelpMessage, useInputClassModifier } from "../core";
 
 import { Textarea } from "./Textarea";
 
@@ -11,61 +11,68 @@ type Props = ComponentProps<typeof Field> &
     helpMessage?: ReactNode;
   };
 
-export const TextareaInput = ({
-  id,
-  classModifier = "",
-  message,
-  children,
-  helpMessage,
-  classNameContainerLabel,
-  classNameContainerInput,
-  label,
-  messageType,
-  isVisible,
-  forceDisplayMessage,
-  className,
-  rows = 5,
-  cols = 50,
-  disabled = false,
-  ...textareaInputProps
-}: Props) => {
-  const rowModifier = `${classModifier} label-top`;
-  const inputUseId = useId();
-  const inputId = id ?? inputUseId;
-  const { inputClassModifier, inputFieldClassModifier } = useInputClassModifier(
-    classModifier,
-    disabled,
-    Boolean(children),
-  );
+const TextareaInput = forwardRef<HTMLTextAreaElement, Props>(
+  (
+    {
+      id,
+      classModifier = "",
+      message,
+      children,
+      helpMessage,
+      classNameContainerLabel,
+      classNameContainerInput,
+      label,
+      messageType,
+      isVisible,
+      forceDisplayMessage,
+      className,
+      rows = 5,
+      cols = 50,
+      disabled = false,
+      ...textareaInputProps
+    },
+    inputRef,
+  ) => {
+    const rowModifier = `${classModifier} label-top`;
+    const inputUseId = useId();
+    const inputId = id ?? inputUseId;
+    const { inputClassModifier, inputFieldClassModifier } =
+      useInputClassModifier(classModifier, disabled, Boolean(children));
 
-  return (
-    <Field
-      label={label}
-      id={inputId}
-      message={message}
-      messageType={messageType}
-      isVisible={isVisible}
-      forceDisplayMessage={forceDisplayMessage}
-      className={className}
-      classModifier={rowModifier}
-      classNameContainerLabel={classNameContainerLabel}
-      classNameContainerInput={classNameContainerInput}
-    >
-      <FieldInput
-        className="af-form__textarea"
-        classModifier={inputFieldClassModifier}
+    return (
+      <Field
+        label={label}
+        id={inputId}
+        message={message}
+        messageType={messageType}
+        isVisible={isVisible}
+        forceDisplayMessage={forceDisplayMessage}
+        className={className}
+        classModifier={rowModifier}
+        classNameContainerLabel={classNameContainerLabel}
+        classNameContainerInput={classNameContainerInput}
       >
-        <Textarea
-          {...textareaInputProps}
-          id={inputId}
-          rows={rows}
-          cols={cols}
-          classModifier={inputClassModifier}
-          disabled={disabled}
-        />
-        {children}
-      </FieldInput>
-      <HelpMessage message={helpMessage} isVisible={!message} />
-    </Field>
-  );
-};
+        <FieldInput
+          className="af-form__textarea"
+          classModifier={inputFieldClassModifier}
+        >
+          <Textarea
+            {...textareaInputProps}
+            id={inputId}
+            rows={rows}
+            cols={cols}
+            classModifier={inputClassModifier}
+            disabled={disabled}
+            ref={inputRef}
+          />
+          {children}
+        </FieldInput>
+        <HelpMessage message={helpMessage} isVisible={!message} />
+      </Field>
+    );
+  },
+);
+
+TextareaInput.displayName = "TextareaInput";
+
+export { TextareaInput };
