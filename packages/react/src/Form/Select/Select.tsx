fc3af0ea27dd9daf0f65d36cd8fddde12b1b2b14
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-05-13 14:57:31 +0200

    feat(Agent Select): add forward ref

--- packages/react/src/Form/Select/Select.tsx
+++ packages/react/src/Form/Select/Select.tsx
@@ -1,13 +1,21 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 import { SelectBase } from "./SelectBase";
 import { SelectDefault } from "./SelectDefault";
 
 type SelectProps = ComponentProps<typeof SelectDefault> & {
   mode?: "default" | "base";
 };
-const Select = ({ mode = "default", children, ...props }: SelectProps) => {
-  const DynamicComponent = mode === "default" ? SelectDefault : SelectBase;
-  return <DynamicComponent {...props}>{children}</DynamicComponent>;
-};
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ mode = "default", children, ...props }, inputRef) => {
+    const DynamicComponent = mode === "default" ? SelectDefault : SelectBase;
+    return (
+      <DynamicComponent ref={inputRef} {...props}>
+        {children}
+      </DynamicComponent>
+    );
+  },
+);
+
+Select.displayName = "Select";
 
 export { Select };
