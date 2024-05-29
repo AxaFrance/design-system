d2eceb7b2e8d470a0b7f7539287fb38bc93fe1fd
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-05-29 16:13:11 +0200

    feat(Choice): update ChoiceInput and Choice components to use forwardRef

--- packages/react/src/Form/Choice/Choice.tsx
+++ packages/react/src/Form/Choice/Choice.tsx
@@ -1,5 +1,5 @@
 import "@axa-fr/design-system-css/dist/Form/Choice/Choice.agent.scss";
-import { ComponentPropsWithoutRef } from "react";
+import { forwardRef, type ComponentProps } from "react";
 import { Radio } from "../Radio/Agent";
 import type { Option } from "../core";
 
@@ -8,27 +8,29 @@ const defaultOptions = [
   { label: "Non", value: false, id: "radioItemFalse" },
 ];
 
-type Props = Omit<ComponentPropsWithoutRef<typeof Radio>, "options"> & {
+type Props = Omit<ComponentProps<typeof Radio>, "options"> & {
   id: string;
   options?: Array<Omit<Option, "value"> & { value: boolean }>;
 };
 
-const Choice = ({
-  children,
-  value,
-  options = defaultOptions,
-  ...otherProps
-}: Props) => {
-  const choiceOptions = options.map((option) => ({
-    ...option,
-    value: `${option.value}`,
-  }));
-  return (
-    <Radio {...otherProps} value={value?.toString()} options={choiceOptions}>
-      {children}
-    </Radio>
-  );
-};
+const Choice = forwardRef<HTMLInputElement, Props>(
+  ({ children, value, options = defaultOptions, ...otherProps }, inputRef) => {
+    const choiceOptions = options.map((option) => ({
+      ...option,
+      value: `${option.value}`,
+    }));
+    return (
+      <Radio
+        {...otherProps}
+        ref={inputRef}
+        value={value?.toString()}
+        options={choiceOptions}
+      >
+        {children}
+      </Radio>
+    );
+  },
+);
 
 Choice.displayName = "Choice";
 
--- packages/react/src/Form/Choice/ChoiceInput.tsx
+++ packages/react/src/Form/Choice/ChoiceInput.tsx
@@ -1,60 +1,68 @@
-import { ComponentPropsWithoutRef } from "react";
+import { forwardRef, type ComponentProps } from "react";
 import { Field, useOptionsWithId } from "../core";
 import { Choice } from "./Choice";
 
-type Props = ComponentPropsWithoutRef<typeof Choice> &
-  Omit<ComponentPropsWithoutRef<typeof Field>, "children">;
+type Props = ComponentProps<typeof Choice> &
+  Omit<ComponentProps<typeof Field>, "children">;
 
 const defaultOptions = [
   { label: "Oui", value: true, id: "radioItemTrue" },
   { label: "Non", value: false, id: "radioItemFalse" },
 ];
 
-const ChoiceInput = ({
-  id,
-  messageType,
-  message,
-  className,
-  classModifier,
-  isVisible,
-  classNameContainerLabel,
-  classNameContainerInput,
-  label,
-  forceDisplayMessage,
-  options = defaultOptions,
-  ...otherProps
-}: Props) => {
-  const newOptions = useOptionsWithId(
-    options.map((o) => ({ ...o, value: `${o.value}` })),
-    id,
-  );
-  const firstId = newOptions?.[0]?.id ?? "";
-  const choiceOptions = newOptions.map((o) => ({
-    ...o,
-    value: o.value === "true",
-  }));
-  return (
-    <Field
-      label={label}
-      id={firstId}
-      message={message}
-      messageType={messageType}
-      isVisible={isVisible}
-      forceDisplayMessage={forceDisplayMessage}
-      className={className}
-      classModifier={classModifier}
-      classNameContainerLabel={classNameContainerLabel}
-      classNameContainerInput={classNameContainerInput}
-    >
-      <Choice
-        {...otherProps}
-        id={id}
+const ChoiceInput = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      id,
+      messageType,
+      message,
+      className,
+      classModifier,
+      isVisible,
+      classNameContainerLabel,
+      classNameContainerInput,
+      label,
+      forceDisplayMessage,
+      options = defaultOptions,
+      ...otherProps
+    },
+    inputRef,
+  ) => {
+    const newOptions = useOptionsWithId(
+      options.map((o) => ({ ...o, value: `${o.value}` })),
+      id,
+    );
+    const firstId = newOptions?.[0]?.id ?? "";
+    const choiceOptions = newOptions.map((o) => ({
+      ...o,
+      value: o.value === "true",
+    }));
+    return (
+      <Field
+        label={label}
+        id={firstId}
+        message={message}
+        messageType={messageType}
+        isVisible={isVisible}
+        forceDisplayMessage={forceDisplayMessage}
+        className={className}
         classModifier={classModifier}
-        options={options ? choiceOptions : undefined}
-        required={classModifier?.includes("required")}
-      />
-    </Field>
-  );
-};
+        classNameContainerLabel={classNameContainerLabel}
+        classNameContainerInput={classNameContainerInput}
+      >
+        <Choice
+          {...otherProps}
+          id={id}
+          ref={inputRef}
+          classModifier={classModifier}
+          options={options ? choiceOptions : undefined}
+          required={classModifier?.includes("required")}
+        />
+      </Field>
+    );
+  },
+);
+
+ChoiceInput.displayName = "ChoiceInput";
 
 export { ChoiceInput };
