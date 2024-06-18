f76d8851d61cb6732d3480be0998b382d58ed0f0
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-06-18 16:12:58 +0200

    fix: style lint

--- packages/css/src/Steps/Steps.stories.ts
+++ packages/css/src/Steps/Steps.stories.ts
@@ -18,35 +18,35 @@ export const NewStepsDesign: StoryObj = {
     <div class="af-steps-new">
         <ol class="af-steps-list">
             <li class="${args.mode} af-steps-list-step number" title="Previous step">
-                <a class="af-steps-list-stepLabel" href="/etape1">
-                    <div class="af-steps-list-stepNumber">1</div>
-                    <div class="af-steps-list-stepTitle">Previous step</div>
+                <a class="af-steps-list-step__label" href="/etape1">
+                    <div class="af-steps-list-step__number">1</div>
+                    <div class="af-steps-list-step__title">Previous step</div>
                 </a>
             </li>
             <li class="past af-steps-list-step number" title="Previous step">
-                <a class="af-steps-list-stepLabel" href="/etape2">
-                    <div class="af-steps-list-stepNumber">2</div>
-                    <div class="af-steps-list-stepTitle">Previous step</div>
+                <a class="af-steps-list-step__label" href="/etape2">
+                    <div class="af-steps-list-step__number">2</div>
+                    <div class="af-steps-list-step__title">Previous step</div>
                 </a>
             </li>
             <li class="on af-steps-list-step number" title="Current step">
-                <div class="af-steps-list-stepLabel">
-                    <div class="af-steps-list-stepNumber">3</div>
-                    <div class="af-steps-list-stepTitle">Current step</div>
+                <div class="af-steps-list-step__label">
+                    <div class="af-steps-list-step__number">3</div>
+                    <div class="af-steps-list-step__title">Current step</div>
                 </div>
             </li>
             <li class="af-steps-list-step" title="Un titre">
-                <a class="af-steps-list-stepLabel" href="#/">
-                    <span class="af-steps-list-stepNumber">
+                <a class="af-steps-list-step__label" href="#/">
+                    <span class="af-steps-list-step__number">
                         <i class="glyphicon glyphicon-ok"></i>
                     </span>
-                    <span class="af-steps-list-stepTitle">Custom</span>
+                    <span class="af-steps-list-step__title">Custom</span>
                 </a>
             </li>
             <li class="disabled af-steps-list-step number" title="Final step">
-                <div class="af-steps-list-stepLabel">
-                    <div class="af-steps-list-stepNumber">5</div>
-                    <div class="af-steps-list-stepTitle">Final step</div>
+                <div class="af-steps-list-step__label">
+                    <div class="af-steps-list-step__number">5</div>
+                    <div class="af-steps-list-step__title">Final step</div>
                 </div>
             </li>
         </ol>
@@ -73,35 +73,35 @@ export const OldStepsDesign: StoryObj = {
     <div class="af-steps">
         <ol class="af-steps-list">
             <li class="${args.mode} af-steps-list-step number" title="Previous step">
-                <div class="af-steps-list-stepLabel">
-                    <div class="af-steps-list-stepNumber">1</div>
-                    <div class="af-steps-list-stepTitle">Previous step</div>
+                <div class="af-steps-list-step__label">
+                    <div class="af-steps-list-step__number">1</div>
+                    <div class="af-steps-list-step__title">Previous step</div>
                 </div>
             </li>
             <li class="past af-steps-list-step number" title="Previous step">
-                <a class="af-steps-list-stepLabel" href="/etape2">
-                    <div class="af-steps-list-stepNumber">2</div>
-                    <div class="af-steps-list-stepTitle">Previous step</div>
+                <a class="af-steps-list-step__label" href="/etape2">
+                    <div class="af-steps-list-step__number">2</div>
+                    <div class="af-steps-list-step__title">Previous step</div>
                 </a>
             </li>
             <li class="on af-steps-list-step number" title="Current step">
-                <div class="af-steps-list-stepLabel">
-                    <div class="af-steps-list-stepNumber">3</div>
-                    <div class="af-steps-list-stepTitle">Current step</div>
+                <div class="af-steps-list-step__label">
+                    <div class="af-steps-list-step__number">3</div>
+                    <div class="af-steps-list-step__title">Current step</div>
                 </div>
             </li>
             <li class="af-steps-list-step" title="Un titre">
-                <a class="af-steps-list-stepLabel" href="#/">
-                    <span class="af-steps-list-stepNumber">
+                <a class="af-steps-list-step__label" href="#/">
+                    <span class="af-steps-list-step__number">
                         <i class="glyphicon glyphicon-ok"></i>
                     </span>
-                    <span class="af-steps-list-stepTitle">Custom</span>
+                    <span class="af-steps-list-step__title">Custom</span>
                 </a>
             </li>
             <li class="disabled af-steps-list-step number" title="Final step">
-                <div class="af-steps-list-stepLabel">
-                    <div class="af-steps-list-stepNumber">5</div>
-                    <div class="af-steps-list-stepTitle">Final step</div>
+                <div class="af-steps-list-step__label">
+                    <div class="af-steps-list-step__number">5</div>
+                    <div class="af-steps-list-step__title">Final step</div>
                 </div>
             </li>
         </ol>
