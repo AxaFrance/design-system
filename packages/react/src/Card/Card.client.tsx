b78b9edc76a8d75a4d7ca6daec3b13076e0acf8e
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-06-25 16:00:20 +0200

    feat(react): add Card component

---
+++ packages/react/src/Card/Card.client.tsx
@@ -0,0 +1,42 @@
+import { useMemo, type ComponentProps } from "react";
+import { getComponentClassName } from "../Form/core";
+
+type CardProps = ComponentProps<"div"> &
+  ComponentProps<"button"> & {
+    classModifier?: string;
+  };
+
+export const Card = ({
+  children,
+  className,
+  classModifier,
+  onClick,
+  ...otherProps
+}: CardProps) => {
+  const componentClassName = useMemo(
+    () =>
+      getComponentClassName(
+        className,
+        onClick ? `${classModifier} button` : classModifier,
+        "af-card",
+      ),
+    [className, onClick, classModifier],
+  );
+
+  return onClick ? ( // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+    <button
+      type="button"
+      className={componentClassName}
+      tabIndex={0}
+      onClick={onClick}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  ) : (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+    <div className={componentClassName} tabIndex={0} {...otherProps}>
+      {children}
+    </div>
+  );
+};
