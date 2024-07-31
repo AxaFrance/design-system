800ae03a1a589e70b8d422d4b9a48a9024f0195f
Guillaume_KESTEMAN <81412571+GuillaumeKESTEMAN@users.noreply.github.com>
2024-07-31 10:26:15 +0200

    Feature(client): create IconBg component  (#432)
    * feat(css): create IconBg style
    
    * feat(react): create IconBg component
    
    * feat: switch between secondary and primary colors

---
+++ packages/react/src/IconBg/IconBg.client.tsx
@@ -0,0 +1,28 @@
+import { useMemo, type ReactNode } from "react";
+import { getComponentClassName } from "../agent";
+
+type IconBgProps = {
+  isDisabled?: boolean;
+  className?: string;
+  classModifier?: string;
+  children: ReactNode;
+};
+
+export const IconBg = ({
+  children,
+  className,
+  classModifier,
+  isDisabled = false,
+}: IconBgProps) => {
+  const componentClassName = useMemo(() => {
+    let newClassModifier = classModifier;
+
+    if (isDisabled) {
+      newClassModifier += " disabled";
+    }
+
+    return getComponentClassName(className, newClassModifier, "af-icon-bg");
+  }, [classModifier, isDisabled, className]);
+
+  return <div className={componentClassName}>{children}</div>;
+};
---
+++ packages/react/src/IconBg/index.ts
@@ -0,0 +1,3 @@
+import "@axa-fr/design-system-css/dist/IconBg/IconBg.client.scss";
+
+export { IconBg } from "./IconBg.client";
