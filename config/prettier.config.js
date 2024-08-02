9a37c5951713f8b7f1eade9bdf85d33bd54c2fd7
Mehdi Sebyi <B223DK@login.axa>
2024-08-02 15:29:48 +0200

    feat: fix rebase main

--- config/prettier.config.js
+++ config/prettier.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   endOfLine: "auto",
-};
+  overrides: [{
+    "files": "*.snap",
+    "options": { "parser": "babel" }
+  }]
+}
--- slash/react/src/Form/Select/Client/CustomOption.tsx
+++ slash/react/src/Form/Select/Client/CustomOption.tsx
@@ -2,10 +2,10 @@ import React, { useMemo } from "react";
 import { components, OptionProps } from "react-select";
 
 const CustomOption: React.FC<OptionProps> = ({ getValue, ...otherProps }) => {
-    const value = useMemo(() => getValue(), [getValue]);
-    return (
-        <components.Option {...otherProps} getValue={getValue} data-value={value} />
-    );
+  const value = useMemo(() => getValue(), [getValue]);
+  return (
+      <components.Option {...otherProps} getValue={getValue} data-value={value} />
+  );
 };
 
 export { CustomOption };
