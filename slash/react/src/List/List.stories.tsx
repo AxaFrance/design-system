4a7c2cbe8d04ffe84d03aa6faa0b3c6febbcdeb6
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-08-07 09:04:42 +0200

    refactor(List): classModifiers management in stories

--- slash/react/src/List/List.stories.tsx
+++ slash/react/src/List/List.stories.tsx
@@ -78,11 +78,7 @@ export const Default: StoryObj<
   },
   argTypes: {
     classModifier: {
-      options: [
-        "large",
-        "first-separator-full-width",
-        "first-separator-full-width",
-      ],
+      options: ["large", "extra-padding", "first-separator-full-width"],
       control: { type: "multi-select" },
       defaultValue: [],
     },
