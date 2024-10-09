8e0e7707233f5937d5e4b4aace275df8edc3bfd2
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-10-09 16:27:58 +0200

    feat(react): use Divider in components

--- look-and-feel/react/src/List/ContentItemMono/ContentItemMono.stories.tsx
+++ look-and-feel/react/src/List/ContentItemMono/ContentItemMono.stories.tsx
@@ -113,7 +113,7 @@ export const ContentItemMonoList: StoryObj<
   },
   argTypes: {
     classModifier: {
-      options: ["large", "first-separator-full-width"],
+      options: ["large", "first-divider-full-width"],
       control: { type: "multi-select" },
       defaultValue: [],
     },
