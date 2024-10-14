c98772a09917cee45976ffda2dee19baf4799335
JLou <jeanlou.pierme@gmail.com>
2024-10-14 16:44:38 +0200

    ci: added sonarcloud analysis

---
+++ slash/react/vitest.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react";
+import { defineProject } from "vitest/config";
+
+// https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
+export default defineProject({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    name: "slash",
+    css: true,
+  },
+});
