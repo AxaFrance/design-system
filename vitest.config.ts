cc23728e6a6d38efedec2fa7ba1f13022f28f4b1
JLou <jeanlou.pierme@gmail.com>
2024-10-14 16:44:38 +0200

    ci: added sonarcloud analysis

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      reporter: ["text", "lcov", "html"],
+      include: ["look-and-feel/react/src", "slash/react/src"],
+      exclude: [
+        "**/*.stories.ts?(x)",
+        "./config/*",
+        "**/storybook-static/*",
+        "**/vitest.setup.ts",
+        "**/.eslintrc.js",
+        "**/.storybook/",
+        "**/coverage/",
+        "**/dist/*",
+        "**/prettier.config.js",
+        "**/index.ts",
+        "**/__tests__/**",
+      ],
+    },
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
+  plugins: [react()],
+});
---
+++ vitest.workspace.ts
@@ -0,0 +1,3 @@
+import { defineWorkspace } from "vitest/config";
+
+export default defineWorkspace(["slash/react", "look-and-feel/react"]);
