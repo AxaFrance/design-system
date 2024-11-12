68a4101276436711cb8231a9da34750593a7ca8b
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-11-12 16:15:58 +0100

    feat(Link): add className and classModifier props

--- look-and-feel/react/src/Link/Link.tsx
+++ look-and-feel/react/src/Link/Link.tsx
@@ -2,14 +2,18 @@ import openInNew from "@material-symbols/svg-400/outlined/open_in_new.svg";
 import {
   ComponentPropsWithoutRef,
   PropsWithChildren,
+  useMemo,
   type ReactNode,
 } from "react";
 import { Svg } from "../Svg";
+import { getComponentClassName } from "../utilities";
 
 type LinkProps = {
   openInNewTab?: boolean;
   leftIcon?: ReactNode;
   rightIcon?: ReactNode;
+  className?: string;
+  classModifier?: string;
 } & ComponentPropsWithoutRef<"a">;
 
 export const Link = ({
@@ -18,6 +22,8 @@ export const Link = ({
   leftIcon,
   rightIcon,
   children,
+  className,
+  classModifier = "",
   ...props
 }: PropsWithChildren<LinkProps>) => {
   const newTabProps = openInNewTab && {
@@ -25,13 +31,18 @@ export const Link = ({
     rel: "noopener noreferrer",
   };
 
+  const componentClassName = useMemo(
+    () =>
+      getComponentClassName(
+        className,
+        `${classModifier}${!className && openInNewTab ? " openInNewTab" : ""}`,
+        "af-link",
+      ),
+    [classModifier, className, openInNewTab],
+  );
+
   return (
-    <a
-      className={`af-link${openInNewTab ? " af-link--openInNewTab" : ""}`}
-      href={href}
-      {...newTabProps}
-      {...props}
-    >
+    <a className={componentClassName} href={href} {...newTabProps} {...props}>
       {leftIcon}
       {children}
       {(openInNewTab || Boolean(rightIcon)) &&
--- look-and-feel/react/src/Link/__tests__/Link.test.tsx
+++ look-and-feel/react/src/Link/__tests__/Link.test.tsx
@@ -62,4 +62,40 @@ describe("Link component", () => {
       ).toBeInTheDocument();
     },
   );
+
+  it.each`
+    className       | classModifier
+    ${undefined}    | ${undefined}
+    ${"test-class"} | ${undefined}
+    ${undefined}    | ${"test-modifier"}
+    ${"test-class"} | ${"test-modifier"}
+  `(
+    "renders correctly with className as $className and classModifier as $classModifier",
+    ({ className, classModifier }) => {
+      render(
+        <Link href="/" className={className} classModifier={classModifier}>
+          Test Link
+        </Link>,
+      );
+
+      const link = screen.getByRole("link", { name: "Test Link" });
+      if (className) {
+        expect(link).toHaveClass(className);
+      }
+      if (classModifier) {
+        expect(link).toHaveClass(`${className ?? "af-link"}--${classModifier}`);
+      }
+    },
+  );
+
+  it("remove openInNewTab class when className is provided", () => {
+    render(
+      <Link href="/" openInNewTab className="test-class">
+        Test Link
+      </Link>,
+    );
+
+    const link = screen.getByRole("link", { name: "Test Link" });
+    expect(link).not.toHaveClass("af-link--openInNewTab");
+  });
 });
