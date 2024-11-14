34ce2b13e36449536a759cabd1089a44c7be547e
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-11-14 15:23:33 +0100

    feat(css): use ghost button style

--- look-and-feel/css/src/Layout/Header/Header.stories.ts
+++ look-and-feel/css/src/Layout/Header/Header.stories.ts
@@ -59,10 +59,10 @@ const getPreviousLink = () => {
   const container = document.createElement("div");
   container.className = "af-header-previous-link-container";
   container.innerHTML = `
-        <svg class="af-header-previous-link-icon" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3.19123 7.82522L8.16936 12.8036L6.99998 13.9611L0.0389404 7.00002L6.99998 0.0270996L8.16936 1.19647L3.19123 6.17481H13.9729V7.82522H3.19123Z" fill="#5F5F5F"/>
-        </svg>
-        <a class="af-header-previous-link" href="#">
+        <a class="af-btn-client af-btn-client--ghost af-btn-client--header-previous-link" href="#">
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.19123 7.82522L8.16936 12.8036L6.99998 13.9611L0.0389404 7.00002L6.99998 0.0270996L8.16936 1.19647L3.19123 6.17481H13.9729V7.82522H3.19123Z" fill="#5F5F5F"/>
+          </svg>
           Retour à l'accueil
         </a>
     `;
