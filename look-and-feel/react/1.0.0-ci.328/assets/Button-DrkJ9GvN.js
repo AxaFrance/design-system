08271c3307b744ce5c5b44ddd443333de72f3187
MartinWeb <MartinWeb@users.noreply.github.com>
2024-12-05 11:02:33 +0000

    deploy: 86c58fa4ba834096380f136ebeee2f0da548bb7c

---
+++ look-and-feel/react/1.0.0-ci.328/assets/Button-DrkJ9GvN.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as r}from"./index-FeUjBnvO.js";import"./chunk-NUUEMKO5-4y6jwdZT.js";import{ae as a,af as e,ag as c}from"./index-76RxyfIK.js";import{B as d,P as i,M as l,W as h,a as p}from"./Button.stories-B5OQnv8k.js";import"./index-uubelm5h.js";import"./iframe-DZQkH6iP.js";import"../sb-preview/runtime.js";import"./react-18-CtsMCmcS.js";import"./index-CfOt2XX2.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";import"./arrow_back-DnyUknY1.js";import"./Svg-C4poEASt.js";import"./Button-GV2C7V7f.js";import"./index-vAxXS4R5.js";function s(o){const n={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:d,name:"Button"}),`
+`,t.jsx(n.h1,{id:"button",children:"Button"}),`
+`,t.jsx(n.p,{children:"To use the button import it like that:"}),`
+`,t.jsx(n.pre,{children:t.jsx(n.code,{className:"language-tsx",children:`import {
+  ButtonClient,
+  Variants,
+} from "@axa-fr/design-system-look-and-feel-react";
+
+const MyComponent = () => (
+  <ButtonClient variant={Variants.secondary}>My button</ButtonClient>
+);
+`})}),`
+`,t.jsx(n.h2,{id:"playground",children:"Playground"}),`
+`,t.jsx(e,{of:i}),`
+`,t.jsx(c,{of:i}),`
+`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,t.jsxs(n.p,{children:["Use the ",t.jsx(n.code,{children:"variant"})," prop to change the look of the button. ",t.jsx(n.a,{href:"#examples-of-class-modifiers",children:"See the examples"}),"."]}),`
+`,t.jsxs(n.p,{children:["You can also add your own modifier, and the output will be a custom class called ",t.jsx(n.code,{children:"af-btn-client--<yourModifier>"}),` that you can target in your css
+The rest of the props (eg: `,t.jsx(n.code,{children:"type"}),", ",t.jsx(n.code,{children:"title"}),") will be passed down to the actual ",t.jsx(n.code,{children:"button"})," HTML element."]}),`
+`,t.jsx(n.h2,{id:"examples-of-class-modifiers",children:"Examples of class modifiers"}),`
+`,t.jsx(e,{of:l,layout:"fullscreen"}),`
+`,t.jsx(n.h2,{id:"with-icons",children:"With icons"}),`
+`,t.jsxs(n.p,{children:["To add an icon you need to add the prop, either ",t.jsx(n.code,{children:"iconLeft"})," or ",t.jsx(n.code,{children:"iconRight"})," whether you want the icon on the left of or on the right."]}),`
+`,t.jsx(e,{of:h}),`
+`,t.jsx(e,{of:p})]})}function E(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(s,{...o})}):s(o)}export{E as default};
---
+++ look-and-feel/react/1.0.0-ci.328/assets/ContentTabList-DqFAFh52.js
@@ -0,0 +1,91 @@
+import{j as n}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as m}from"./index-FeUjBnvO.js";import{ae as p,af as i,ag as e}from"./index-76RxyfIK.js";import{C as c,a as r,b as s}from"./ContentTabList.stories-CKkDdfBT.js";import"./index-uubelm5h.js";import"./iframe-DZQkH6iP.js";import"../sb-preview/runtime.js";import"./index-CfOt2XX2.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";import"./index-C_FWhylE.js";import"./Title-CkInkqf4.js";/* empty css               */import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";import"./useIsSmallScreen-BMz0fP8s.js";import"./Tag-PCUeqJ0K.js";import"./Alert-yYw8AEPN.js";import"./error-BHKM2E7q.js";import"./Svg-C4poEASt.js";import"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-DPn8M_Uc.js";import"./check_box_outline_blank-caNJpyLE.js";import"./CheckboxSelect-FCKGgETg.js";import"./Loader-DTzMALSS.js";import"./Radio-CYDN5Meu.js";import"./radio_button_unchecked-hw5p0x-H.js";import"./InputError-BsZAaKA6.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./NavBar-DFgCLS-S.js";import"./PreviousLink-D5jlWA8o.js";import"./arrow_back-DnyUknY1.js";import"./Link-BQTbBxg2.js";import"./close-DqeJ9fK5.js";import"./Modal-CD3ho01h.js";import"./Pagination-R-Zl63vI.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";import"./Skeleton-_BHz5ywW.js";function a(o){const t={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...m(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(p,{of:c,name:"ContentTabList"}),`
+`,n.jsx(t.h1,{id:"contenttablist",children:"ContentTabList"}),`
+`,n.jsx(t.h2,{id:"import",children:"Import"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { ContentTabList } from "@axa-fr/design-system-look-and-feel-react";
+`})}),`
+`,n.jsx(t.h2,{id:"use",children:"Use"}),`
+`,n.jsx(t.h3,{id:"use-with-buttons",children:"Use with buttons"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`const MyComponentWithButtons = () => (
+  <ContentTabList
+    items={[
+      {
+        id: "1",
+        title: "Remboursement soins",
+        subtitle: "Titre onglet",
+        tag: "En attente",
+        date: "01/01/2024",
+        actions: [
+          {
+            id: "download_button",
+            component: (
+              <Button
+                variant={ButtonVariants.ghost}
+                iconLeft={<Svg src={download} />}
+                onClick={fn()}
+              >
+                Télécharger
+              </Button>
+            ),
+          },
+        ],
+      },
+      {
+        id: "2",
+        title: "Remboursement soins",
+        tag: "En attente",
+        actions: [
+          {
+            id: "download_button",
+            component: (
+              <Button
+                variant={ButtonVariants.ghost}
+                iconLeft={<Svg src={download} />}
+                onClick={fn()}
+              >
+                Télécharger
+              </Button>
+            ),
+          },
+          {
+            id: "display_button",
+            component: (
+              <Button
+                variant={ButtonVariants.ghost}
+                iconLeft={<Svg src={visibility} />}
+                onClick={fn()}
+              >
+                Afficher
+              </Button>
+            ),
+          },
+        ],
+      },
+    ]}
+  />
+);
+`})}),`
+`,n.jsx(i,{of:r}),`
+`,n.jsx(e,{of:r}),`
+`,n.jsx(t.h3,{id:"use-with-value",children:"Use with value"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`const MyComponentWithValue = () => (
+  <ContentTabList
+    items={[
+      {
+        id: "1",
+        title: "Remboursement soins",
+        subtitle: "Titre onglet",
+        date: "01/01/2024",
+        value: "+ 11,86 €",
+      },
+      {
+        id: "2",
+        title: "Remboursement soins",
+        tag: "En attente",
+        value: "+ 11,86 €",
+      },
+    ]}
+  />
+);
+`})}),`
+`,n.jsx(i,{of:s}),`
+`,n.jsx(e,{of:s})]})}function on(o={}){const{wrapper:t}={...m(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(a,{...o})}):a(o)}export{on as default};
