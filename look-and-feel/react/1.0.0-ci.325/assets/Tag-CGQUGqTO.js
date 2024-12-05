fc90bc16165cc0bccee6cbfdbe1cf6522106ad0e
JLou <JLou@users.noreply.github.com>
2024-12-05 09:48:29 +0000

    deploy: 7bc522d2bdcc813abe141d865e0ac96962d08a3a

---
+++ look-and-feel/react/1.0.0-ci.325/assets/Tag-CGQUGqTO.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as s}from"./index-FeUjBnvO.js";import{ae as a,af as i,ag as m}from"./index-76RxyfIK.js";import{T as p,D as t}from"./Tag.stories-BMQsrwum.js";import"./index-uubelm5h.js";import"./iframe-DZQkH6iP.js";import"../sb-preview/runtime.js";import"./index-CfOt2XX2.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";import"./Tag-PCUeqJ0K.js";import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";function r(n){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...s(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:p,name:"Tag"}),`
+`,o.jsx(e.h1,{id:"tag",children:"Tag"}),`
+`,o.jsx(e.h3,{id:"import",children:"Import"}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-tsx",children:`import { Tag } from "@axa-fr/design-system-look-and-feel-react";
+`})}),`
+`,o.jsx(e.h3,{id:"use",children:"Use"}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-tsx",children:`export const TagComponent = () => <Tag>Texte</Tag>;
+`})}),`
+`,o.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(i,{of:t}),`
+`,o.jsx(m,{of:t})]})}function N(n={}){const{wrapper:e}={...s(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(r,{...n})}):r(n)}export{N as default};
