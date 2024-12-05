e86e0c084337443d4b20f8cc61292fb16fc11f98
JLou <JLou@users.noreply.github.com>
2024-12-05 13:25:26 +0000

    deploy: e5c66753d24835da76abc0e57e3a85a8376d8664

---
+++ look-and-feel/react/1.0.0-ci.329/assets/Select.stories-BBz7fytF.js
@@ -0,0 +1,28 @@
+import{j as a}from"./jsx-runtime-QvZ8i92b.js";import{S as t}from"./Select-DPf08Y0e.js";import"./index-uubelm5h.js";import"./error-BHKM2E7q.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-CfOt2XX2.js";import"./index-vAxXS4R5.js";import"./Svg-C4poEASt.js";const i=[{value:"fun",label:"For fun"},{value:"work",label:"For work"},{value:"drink",label:"For drink"}],S={component:t,title:"Components/Form/Input/Select"},e={name:"Select",tags:["Form","Input"],render:({onChange:n,...l})=>a.jsx("div",{style:{width:300},children:a.jsx(t,{onChange:n,...l})}),args:{className:"",options:i,label:"Label",id:"nameid",disabled:!1,required:!1,placeholder:"Placeholder",noOptionsMessage:"Pas d'option",errorLabel:""},argTypes:{onChange:{action:"onChange"}}};var o,r,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: "Select",
+  tags: ["Form", "Input"],
+  render: ({
+    onChange,
+    ...args
+  }) => <div style={{
+    width: 300
+  }}>
+      <Select onChange={onChange} {...args} />
+    </div>,
+  args: {
+    className: "",
+    options,
+    label: "Label",
+    id: "nameid",
+    disabled: false,
+    required: false,
+    placeholder: "Placeholder",
+    noOptionsMessage: "Pas d'option",
+    errorLabel: ""
+  },
+  argTypes: {
+    onChange: {
+      action: "onChange"
+    }
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const f=["SelectStory"];export{e as SelectStory,f as __namedExportsOrder,S as default};
---
+++ look-and-feel/react/1.0.0-ci.329/assets/Text.stories-CLbPPb9f.js
@@ -0,0 +1,24 @@
+import{j as f}from"./jsx-runtime-QvZ8i92b.js";import{T as h}from"./Title-CkInkqf4.js";import"./index-uubelm5h.js";/* empty css               */import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";import"./useIsSmallScreen-BMz0fP8s.js";import"./Tag-PCUeqJ0K.js";import"./Alert-yYw8AEPN.js";import"./error-BHKM2E7q.js";import"./Svg-C4poEASt.js";import"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-DPn8M_Uc.js";import"./check_box_outline_blank-caNJpyLE.js";import"./CheckboxSelect-FCKGgETg.js";import"./Loader-DTzMALSS.js";import"./Radio-CYDN5Meu.js";import"./radio_button_unchecked-hw5p0x-H.js";import"./InputError-BsZAaKA6.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-CfOt2XX2.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./NavBar-DFgCLS-S.js";import"./PreviousLink-D5jlWA8o.js";import"./arrow_back-DnyUknY1.js";import"./Link-BQTbBxg2.js";import"./close-DqeJ9fK5.js";import"./Modal-CD3ho01h.js";import"./Pagination-R-Zl63vI.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";import"./Skeleton-_BHz5ywW.js";const or={component:h,title:"Components/Form/Input/Text",args:{value:"John Doe",label:"Label",placeholder:"¨Placeholder",helper:"Informations complémentaires",name:"name",id:"nameid",disabled:!1,required:!1,className:"",type:"text",buttonLabel:""},argTypes:{onChange:{action:"onChange"}}},i=({onChange:b,...S})=>f.jsx(h,{onChange:b,...S}),r={name:"Text",render:i},e={name:"Text with description",render:i,args:{description:"Description"}},t={name:"Text on error",render:i,args:{classModifier:"error",description:"Description",error:"Error Message"}},o={name:"Text with button",render:i,args:{buttonLabel:"En savoir plus"}};var n,s,a;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: "Text",
+  render
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var p,m,c;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: "Text with description",
+  render,
+  args: {
+    description: "Description"
+  }
+}`,...(c=(m=e.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,l,u;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: "Text on error",
+  render,
+  args: {
+    classModifier: "error",
+    description: "Description",
+    error: "Error Message"
+  }
+}`,...(u=(l=t.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var x,T,g;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  name: "Text with button",
+  render,
+  args: {
+    buttonLabel: "En savoir plus"
+  }
+}`,...(g=(T=o.parameters)==null?void 0:T.docs)==null?void 0:g.source}}};const ir=["TextStory","TextWithDescriptionStory","TextOnErrorStory","TextWithButton"];export{t as TextOnErrorStory,r as TextStory,o as TextWithButton,e as TextWithDescriptionStory,ir as __namedExportsOrder,or as default};
