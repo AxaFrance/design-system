a842635fbbff903cc63bedf136ec8e66b4f1b992
JLou <JLou@users.noreply.github.com>
2024-12-05 09:21:08 +0000

    deploy: 65f785a3d783842be59cc5d55abe24d9ad22c1e9

---
+++ look-and-feel/react/1.0.0-ci.322/assets/Loader.stories-BL2K_dW7.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-QvZ8i92b.js";import{L as s}from"./Loader-DTzMALSS.js";import"./index-uubelm5h.js";const c={component:s,title:"Components/Loader"},r={name:"Loader",render:({...n})=>t.jsx(s,{...n}),args:{size:60,border:5}};var e,o,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  name: "Loader",
+  render: ({
+    ...args
+  }) => <Loader {...args} />,
+  args: {
+    size: 60,
+    border: 5
+  }
+}`,...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const i=["LoaderStory"];export{r as LoaderStory,i as __namedExportsOrder,c as default};
