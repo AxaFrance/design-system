7f99be0686c542f736ec037888949fb530cb52f7
JLou <JLou@users.noreply.github.com>
2024-12-06 14:43:40 +0000

    deploy: 1301015a69234c6820a5712a39ee9f7b2dc3b481

---
+++ look-and-feel/react/1.0.0-ci.332/assets/Header.stories-Bq89iEkr.js
@@ -0,0 +1,93 @@
+import{j as e}from"./jsx-runtime-QvZ8i92b.js";import{S as n}from"./Svg-C4poEASt.js";import{H as r}from"./Title-CkInkqf4.js";/* empty css               */import"./Alert-yYw8AEPN.js";import{B as a,V as t}from"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-DPn8M_Uc.js";import"./CheckboxSelect-FCKGgETg.js";import"./Radio-CYDN5Meu.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./Link-BQTbBxg2.js";import"./Loader-DTzMALSS.js";import"./Modal-CD3ho01h.js";import"./index-uubelm5h.js";import"./index-CfOt2XX2.js";import"./Pagination-R-Zl63vI.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";import"./Tag-PCUeqJ0K.js";import"./Skeleton-_BHz5ywW.js";const S="data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20width='48'%20height='48'%20viewBox='0%20-960%20960%20960'%3e%3cpath%20d='M480-481q-66%200-108-42t-42-108q0-66%2042-108t108-42q66%200%20108%2042t42%20108q0%2066-42%20108t-108%2042ZM160-160v-94q0-38%2019-65t49-41q67-30%20128.5-45T480-420q62%200%20123%2015.5T731-360q31%2014%2050%2041t19%2065v94H160Z'/%3e%3c/svg%3e",k="data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20width='48'%20height='48'%20viewBox='0%20-960%20960%20960'%3e%3cpath%20d='M220-80q-24.75%200-42.37-17.63Q160-115.25%20160-140v-434q0-24.75%2017.63-42.38Q195.25-634%20220-634h70v-96q0-78.85%2055.61-134.42Q401.21-920%20480.11-920q78.89%200%20134.39%2055.58Q670-808.85%20670-730v96h70q24.75%200%2042.38%2017.62Q800-598.75%20800-574v434q0%2024.75-17.62%2042.37Q764.75-80%20740-80H220Zm260.17-200q31.83%200%2054.33-22.03T557-355q0-30-22.67-54.5t-54.5-24.5q-31.83%200-54.33%2024.5t-22.5%2055q0%2030.5%2022.67%2052.5t54.5%2022ZM350-634h260v-96q0-54.17-37.88-92.08-37.88-37.92-92-37.92T388-822.08q-38%2037.91-38%2092.08v96Z'/%3e%3c/svg%3e",C={component:r,title:"Components/Header"},s={name:"Header",render:()=>e.jsx(r,{})},o={name:"Header with navigation bar",render:()=>e.jsxs(r,{children:[e.jsx("a",{className:"af-navbar-item__link",href:"/mes-contrats",target:"_",children:"MES CONTRATS"},"MES CONTRATS link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-avantages",target:"_",children:"MES AVANTAGES"},"MES AVANTAGES link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-documents",target:"_",children:"MES DOCUMENTS"},"MES DOCUMENTS link")]})},m={name:"Header with navigation bar and right item",render:({rightItem:i})=>e.jsxs(r,{rightItem:i,children:[e.jsx("a",{className:"af-navbar-item__link",href:"/mes-contrats",children:"MES CONTRATS"},"MES CONTRATS link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-avantages",children:"MES AVANTAGES"},"MES AVANTAGES link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-documents",children:"MES DOCUMENTS"},"MES DOCUMENTS link")]}),args:{rightItem:[e.jsx(a,{iconLeft:e.jsx(n,{src:k,fill:"#00008f"}),variant:t.secondary,children:"Espace Banque"},"Banking Space Button"),e.jsx(a,{iconLeft:e.jsx(n,{src:S,fill:"#00008f"}),variant:t.ghost,children:"Mon Profil"},"Profile Button")]}},c={name:"Header with links, right item and Previous link",render:({previousLink:i,rightItem:d})=>e.jsxs(r,{previousLink:i,rightItem:d,children:[e.jsx("a",{className:"af-navbar-item__link",href:"/mes-contrats",children:"MES CONTRATS"},"MES CONTRATS link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-avantages",children:"MES AVANTAGES"},"MES AVANTAGES link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-documents",children:"MES DOCUMENTS"},"MES DOCUMENTS link")]}),args:{previousLink:e.jsx("a",{href:"/mes-contrats",children:"Retour à mes contrats"},"Previous link"),rightItem:[e.jsx(a,{iconLeft:e.jsx(n,{src:k,fill:"#00008f"}),variant:t.secondary,children:"Espace Banque"},"Banking Space Button"),e.jsx(a,{iconLeft:e.jsx(n,{src:S,fill:"#00008f"}),variant:t.ghost,children:"Mon Profil"},"Profile Button")]}},l={name:"Header with links, right item, Previous link and active link",render:({defaultActiveLink:i,previousLink:d,rightItem:j})=>e.jsxs(r,{defaultActiveLink:i,previousLink:d,rightItem:j,children:[e.jsx("a",{className:"af-navbar-item__link",href:"/mes-contrats",children:"MES CONTRATS"},"MES CONTRATS link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-avantages",children:"MES AVANTAGES"},"MES AVANTAGES link"),e.jsx("a",{className:"af-navbar-item__link",href:"/mes-documents",children:"MES DOCUMENTS"},"MES DOCUMENTS link")]}),args:{defaultActiveLink:0,previousLink:e.jsx("a",{href:"/mes-contrats",children:"Retour à mes contrats"},"Previous link"),rightItem:[e.jsx(a,{iconLeft:e.jsx(n,{src:k,fill:"#00008f"}),variant:t.secondary,children:"Espace Banque"},"Banking Space Button"),e.jsx(a,{iconLeft:e.jsx(n,{src:S,fill:"#00008f"}),variant:t.ghost,children:"Mon Profil"},"Profile Button")]}};var f,h,v;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  name: "Header",
+  render: () => <Header />
+}`,...(v=(h=s.parameters)==null?void 0:h.docs)==null?void 0:v.source}}};var u,g,E;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  name: "Header with navigation bar",
+  render: () => <Header>
+      <a key="MES CONTRATS link" className="af-navbar-item__link" href="/mes-contrats" target="_">
+        MES CONTRATS
+      </a>
+      <a key="MES AVANTAGES link" className="af-navbar-item__link" href="/mes-avantages" target="_">
+        MES AVANTAGES
+      </a>
+      <a key="MES DOCUMENTS link" className="af-navbar-item__link" href="/mes-documents" target="_">
+        MES DOCUMENTS
+      </a>
+    </Header>
+}`,...(E=(g=o.parameters)==null?void 0:g.docs)==null?void 0:E.source}}};var A,p,N;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  name: "Header with navigation bar and right item",
+  render: ({
+    rightItem
+  }) => <Header rightItem={rightItem}>
+      <a key="MES CONTRATS link" className="af-navbar-item__link" href="/mes-contrats">
+        MES CONTRATS
+      </a>
+      <a key="MES AVANTAGES link" className="af-navbar-item__link" href="/mes-avantages">
+        MES AVANTAGES
+      </a>
+      <a key="MES DOCUMENTS link" className="af-navbar-item__link" href="/mes-documents">
+        MES DOCUMENTS
+      </a>
+    </Header>,
+  args: {
+    rightItem: [<Button key="Banking Space Button" iconLeft={<Svg src={lock} fill="#00008f" />} variant={ButtonVariants.secondary}>
+        Espace Banque
+      </Button>, <Button key="Profile Button" iconLeft={<Svg src={person} fill="#00008f" />} variant={ButtonVariants.ghost}>
+        Mon Profil
+      </Button>]
+  }
+}`,...(N=(p=m.parameters)==null?void 0:p.docs)==null?void 0:N.source}}};var M,T,_;c.parameters={...c.parameters,docs:{...(M=c.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  name: "Header with links, right item and Previous link",
+  render: ({
+    previousLink,
+    rightItem
+  }) => <Header previousLink={previousLink} rightItem={rightItem}>
+      <a key="MES CONTRATS link" className="af-navbar-item__link" href="/mes-contrats">
+        MES CONTRATS
+      </a>
+      <a key="MES AVANTAGES link" className="af-navbar-item__link" href="/mes-avantages">
+        MES AVANTAGES
+      </a>
+      <a key="MES DOCUMENTS link" className="af-navbar-item__link" href="/mes-documents">
+        MES DOCUMENTS
+      </a>
+    </Header>,
+  args: {
+    previousLink: <a key="Previous link" href="/mes-contrats">
+        Retour à mes contrats
+      </a>,
+    rightItem: [<Button key="Banking Space Button" iconLeft={<Svg src={lock} fill="#00008f" />} variant={ButtonVariants.secondary}>
+        Espace Banque
+      </Button>, <Button key="Profile Button" iconLeft={<Svg src={person} fill="#00008f" />} variant={ButtonVariants.ghost}>
+        Mon Profil
+      </Button>]
+  }
+}`,...(_=(T=c.parameters)==null?void 0:T.docs)==null?void 0:_.source}}};var B,x,L;l.parameters={...l.parameters,docs:{...(B=l.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  name: "Header with links, right item, Previous link and active link",
+  render: ({
+    defaultActiveLink,
+    previousLink,
+    rightItem
+  }) => <Header defaultActiveLink={defaultActiveLink} previousLink={previousLink} rightItem={rightItem}>
+      <a key="MES CONTRATS link" className="af-navbar-item__link" href="/mes-contrats">
+        MES CONTRATS
+      </a>
+      <a key="MES AVANTAGES link" className="af-navbar-item__link" href="/mes-avantages">
+        MES AVANTAGES
+      </a>
+      <a key="MES DOCUMENTS link" className="af-navbar-item__link" href="/mes-documents">
+        MES DOCUMENTS
+      </a>
+    </Header>,
+  args: {
+    defaultActiveLink: 0,
+    previousLink: <a key="Previous link" href="/mes-contrats">
+        Retour à mes contrats
+      </a>,
+    rightItem: [<Button key="Banking Space Button" iconLeft={<Svg src={lock} fill="#00008f" />} variant={ButtonVariants.secondary}>
+        Espace Banque
+      </Button>, <Button key="Profile Button" iconLeft={<Svg src={person} fill="#00008f" />} variant={ButtonVariants.ghost}>
+        Mon Profil
+      </Button>]
+  }
+}`,...(L=(x=l.parameters)==null?void 0:x.docs)==null?void 0:L.source}}};const O=["Default","HeaderWithNavBar","HeaderWithNavBarAndRightItem","HeaderWithLinksAndRightItemAndPreviousLink","HeaderWithLinksAndRightItemAndPreviousLinkAndActiveLink"],ae=Object.freeze(Object.defineProperty({__proto__:null,Default:s,HeaderWithLinksAndRightItemAndPreviousLink:c,HeaderWithLinksAndRightItemAndPreviousLinkAndActiveLink:l,HeaderWithNavBar:o,HeaderWithNavBarAndRightItem:m,__namedExportsOrder:O,default:C},Symbol.toStringTag,{value:"Module"}));export{s as D,ae as H,o as a,m as b,c};
