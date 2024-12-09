66c9467ea0a094d9b19062d7ada5511147623c7f
a-pourrier <a-pourrier@users.noreply.github.com>
2024-12-09 13:12:43 +0000

    deploy: 167402a39ad024a23d98298a89c2e31effc30150

---
+++ look-and-feel/react/1.0.0-ci.336/assets/ContentItemMono-BTdWv275.js
@@ -0,0 +1,73 @@
+import{j as n}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as x}from"./index-FeUjBnvO.js";import{ae as p,af as o,ag as i}from"./index-Cb73D1Ps.js";import{C as l,D as r,a as m,b as s,c}from"./ContentItemMono.stories-B4FnLQ3B.js";import"./index-uubelm5h.js";import"./iframe-D-IgPWKx.js";import"../sb-preview/runtime.js";import"./index-CfOt2XX2.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";import"./Svg-C4poEASt.js";import"./Title-5HvLqByk.js";/* empty css               */import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";import"./useIsSmallScreen-BMz0fP8s.js";import"./Tag-PCUeqJ0K.js";import"./Alert-yYw8AEPN.js";import"./error-BHKM2E7q.js";import"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-B4MiBKMF.js";import"./check_box_outline_blank-BvMHvB9Z.js";import"./CheckboxSelect-Dq6IwaT4.js";import"./Loader-DTzMALSS.js";import"./Radio-CYDN5Meu.js";import"./radio_button_unchecked-hw5p0x-H.js";import"./InputError-BsZAaKA6.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./NavBar-DFgCLS-S.js";import"./PreviousLink-D5jlWA8o.js";import"./arrow_back-DnyUknY1.js";import"./Link-BQTbBxg2.js";import"./close-DqeJ9fK5.js";import"./Modal-CD3ho01h.js";import"./Pagination-R-Zl63vI.js";import"./Skeleton-_BHz5ywW.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";function a(e){const t={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",ol:"ol",pre:"pre",...x(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(p,{of:l,name:"ContentItemMono"}),`
+`,n.jsx(t.h1,{id:"contentitemmono",children:"ContentItemMono"}),`
+`,n.jsxs(t.ol,{children:[`
+`,n.jsx(t.li,{children:n.jsx(t.a,{href:"#import",children:"Import"})}),`
+`,n.jsx(t.li,{children:n.jsx(t.a,{href:"#contentitemmono",children:"ContentItemMono"})}),`
+`,n.jsx(t.li,{children:n.jsx(t.a,{href:"#contentitemmono-with-icon",children:"ContentItemMono with icon"})}),`
+`,n.jsx(t.li,{children:n.jsx(t.a,{href:"#contentitemmono-with-stick",children:"ContentItemMono with stick"})}),`
+`,n.jsx(t.li,{children:n.jsx(t.a,{href:"#contentitemmono-list",children:"ContentItemMono List"})}),`
+`]}),`
+`,n.jsx(t.h2,{id:"import",children:"Import"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { ContentItemMono } from "@axa-fr/design-system-look-and-feel-react";
+`})}),`
+`,n.jsx(t.h2,{id:"uses",children:"Uses"}),`
+`,n.jsx(t.h3,{id:"contentitemmono-1",children:"ContentItemMono"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`export const ContentListMonoComponent = () => (
+  <ContentItemMono
+    secondaryText="Texte secondaire"
+    tertiaryText="Texte tertiaire"
+  >
+    Texte principal
+  </ContentItemMono>
+);
+`})}),`
+`,n.jsx(o,{of:r}),`
+`,n.jsx(i,{of:r}),`
+`,n.jsx(t.h3,{id:"contentitemmono-with-icon",children:"ContentItemMono with icon"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`export const ContentListMonoComponent = () => (
+  <ContentItemMono
+    secondaryText="Texte secondaire"
+    tertiaryText="Texte tertiaire"
+    leftElement={<Svg src={arrowForward} />}
+    isLeftElementCentered
+  >
+    Texte principal
+  </ContentItemMono>
+);
+`})}),`
+`,n.jsx(o,{of:m}),`
+`,n.jsx(i,{of:m}),`
+`,n.jsx(t.h3,{id:"contentitemmono-with-stick",children:"ContentItemMono with stick"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`export const ContentListMonoComponent = () => (
+  <ContentItemMono secondaryText="Texte secondaire" hasStick>
+    Texte principal
+  </ContentItemMono>
+);
+`})}),`
+`,n.jsx(o,{of:s}),`
+`,n.jsx(i,{of:s}),`
+`,n.jsx(t.h3,{id:"contentitemmono-list",children:"ContentItemMono List"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`export const ContentItemMonoListComponent = () => (
+  <List>
+    <ContentItemMono
+      secondaryText="Texte secondaire"
+      tertiaryText="Texte tertiaire"
+    >
+      Texte principal
+    </ContentItemMono>
+    <ContentItemMono
+      secondaryText="Texte secondaire"
+      tertiaryText="Texte tertiaire"
+      leftElement={<Svg src={arrowForward} />}
+      isLeftElementCentered
+    >
+      Texte principal
+    </ContentItemMono>
+    <ContentItemMono secondaryText="Texte secondaire" hasStick>
+      Texte principal
+    </ContentItemMono>
+  </List>
+);
+`})}),`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(i,{of:c})]})}function on(e={}){const{wrapper:t}={...x(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(a,{...e})}):a(e)}export{on as default};
