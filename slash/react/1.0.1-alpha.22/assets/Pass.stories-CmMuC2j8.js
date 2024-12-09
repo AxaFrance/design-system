7c6b09039edd95b659d56ae71c8701bda4d4bfab
MartinWeb <MartinWeb@users.noreply.github.com>
2024-12-09 15:09:03 +0000

    deploy: d37aac66900fe9c73c9d10e5c2c712fc1865771d

---
+++ slash/react/1.0.1-alpha.22/assets/Pass.stories-CmMuC2j8.js
@@ -0,0 +1,32 @@
+import{j as l}from"./jsx-runtime-Cw0GR0a5.js";import{f as n}from"./index-C_FWhylE.js";import{P as r}from"./Pass-CDmUB5MI.js";const d={title:"Components/Form/Input/Pass",component:r},e={render:t=>l.jsx(r,{...t}),name:"Pass",args:{classModifier:"bad",name:"password",id:"uniqueid",type:"password",value:"",readOnly:!1,disabled:!1,className:"",autoFocus:!0,onChange:n(),onToggleType:n()},argTypes:{onChange:{action:"onChange"},onToggleType:{action:"onToggleType"},classModifier:{options:["bad","okay","good","verygood","excellent"],control:{type:"select"},defaultValue:"bad"}}};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => <Pass {...args} />,
+  name: "Pass",
+  args: {
+    classModifier: "bad",
+    name: "password",
+    id: "uniqueid",
+    type: "password",
+    value: "",
+    readOnly: false,
+    disabled: false,
+    className: "",
+    autoFocus: true,
+    onChange: fn(),
+    onToggleType: fn()
+  },
+  argTypes: {
+    onChange: {
+      action: "onChange"
+    },
+    onToggleType: {
+      action: "onToggleType"
+    },
+    classModifier: {
+      options: ["bad", "okay", "good", "verygood", "excellent"],
+      control: {
+        type: "select"
+      },
+      defaultValue: "bad"
+    }
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const i=["PassStory"],u=Object.freeze(Object.defineProperty({__proto__:null,PassStory:e,__namedExportsOrder:i,default:d},Symbol.toStringTag,{value:"Module"}));export{u as P,e as a};
