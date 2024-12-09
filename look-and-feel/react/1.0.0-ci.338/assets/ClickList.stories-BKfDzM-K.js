183fa80b0cf8fa1cd4c3907d40bbfe57c1b01fa9
MartinWeb <MartinWeb@users.noreply.github.com>
2024-12-09 15:08:31 +0000

    deploy: d37aac66900fe9c73c9d10e5c2c712fc1865771d

---
+++ look-and-feel/react/1.0.0-ci.338/assets/ClickList.stories-BKfDzM-K.js
@@ -0,0 +1,116 @@
+import{j as e}from"./jsx-runtime-QvZ8i92b.js";import{f as s,g as a}from"./Title-5HvLqByk.js";import{S as c}from"./Svg-C4poEASt.js";const l=(i,_)=>({children:x,...b})=>e.jsx(i,{..._,...b,children:x}),C="data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20width='48'%20height='48'%20viewBox='0%20-960%20960%20960'%3e%3cpath%20d='m734-120-42-42%2074-74H504v-60h262l-74-74%2042-42%20146%20146-146%20146Zm106-352h-60v-308h-60v90H240v-90h-60v600h264v60H180q-26%200-43-17t-17-43v-600q0-26%2017-43t43-17h202q7-35%2034.5-57.5T480-920q36%200%2063.5%2022.5T578-840h202q26%200%2043%2017t17%2043v308ZM480-780q17%200%2028.5-11.5T520-820q0-17-11.5-28.5T480-860q-17%200-28.5%2011.5T440-820q0%2017%2011.5%2028.5T480-780Z'/%3e%3c/svg%3e",j={title:"Components/List/ClickList",component:s},n={name:"ClickList with icon",render:i=>e.jsx(s,{...i}),args:{items:[{id:"1",icon:e.jsx(c,{src:a,"aria-hidden":!0}),children:"Texte principal",classModifier:"small"},{id:"2",icon:e.jsx(c,{src:a,"aria-hidden":!0}),children:"Texte principal"},{id:"3",icon:e.jsx(c,{src:a,"aria-hidden":!0}),children:"Texte principal",classModifier:"large"},{id:"4",icon:e.jsx(c,{src:C,"aria-hidden":!0}),children:"Texte principal",isDisabled:!0}],classModifier:""},argTypes:{classModifier:{options:["","large"],control:{type:"select"},defaultValue:""}}},r={name:"ClickList without icon",render:i=>e.jsx(s,{...i}),args:{items:[{id:"1",children:e.jsxs(e.Fragment,{children:[e.jsx("h3",{className:"af-click-item__title",children:"Fiche Orias précontractuelle"}),e.jsx("p",{className:"af-click-item__subtitle",children:"30 novembre 2021"}),e.jsx("p",{className:"af-click-item__secondary",children:"Signé électroniquement"})]})},{id:"2",children:e.jsxs(e.Fragment,{children:[e.jsx("h3",{className:"af-click-item__title",children:"Fiche Orias précontractuelle"}),e.jsx("p",{className:"af-click-item__subtitle",children:"30 novembre 2021"}),e.jsx("p",{className:"af-click-item__secondary",children:"Signé électroniquement"})]}),isDisabled:!0}],classModifier:""},argTypes:{classModifier:{options:["","large"],control:{type:"select"},defaultValue:""}}},t={name:"ClickList link without icon",render:i=>e.jsx(s,{...i}),args:{items:[{id:"1",children:e.jsxs(e.Fragment,{children:[e.jsx("h3",{className:"af-click-item__title",children:"Fiche Orias précontractuelle"}),e.jsx("p",{className:"af-click-item__subtitle",children:"30 novembre 2021"}),e.jsx("p",{className:"af-click-item__secondary",children:"Signé électroniquement"})]}),parentClickComponent:l("a",{href:"https://github.com/AxaFrance/design-system",target:"_blank",rel:"noreferrer"})},{id:"2",children:e.jsxs(e.Fragment,{children:[e.jsx("h3",{className:"af-click-item__title",children:"Fiche Orias précontractuelle"}),e.jsx("p",{className:"af-click-item__subtitle",children:"30 novembre 2021"}),e.jsx("p",{className:"af-click-item__secondary",children:"Signé électroniquement"})]}),parentClickComponent:l("a",{href:"https://github.com/AxaFrance/design-system",target:"_blank",rel:"noreferrer"}),isDisabled:!0}],classModifier:""},argTypes:{classModifier:{options:["","large"],control:{type:"select"},defaultValue:""}}};var o,m,d;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: "ClickList with icon",
+  render: args => <ClickList {...args} />,
+  args: {
+    items: [{
+      id: "1",
+      icon: <Svg src={error} aria-hidden />,
+      children: "Texte principal",
+      classModifier: "small"
+    }, {
+      id: "2",
+      icon: <Svg src={error} aria-hidden />,
+      children: "Texte principal"
+    }, {
+      id: "3",
+      icon: <Svg src={error} aria-hidden />,
+      children: "Texte principal",
+      classModifier: "large"
+    }, {
+      id: "4",
+      icon: <Svg src={contentPasteGo} aria-hidden />,
+      children: "Texte principal",
+      isDisabled: true
+    }],
+    classModifier: ""
+  },
+  argTypes: {
+    classModifier: {
+      options: ["", "large"],
+      control: {
+        type: "select"
+      },
+      defaultValue: ""
+    }
+  }
+}`,...(d=(m=n.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var p,h,u;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: "ClickList without icon",
+  render: args => <ClickList {...args} />,
+  args: {
+    items: [{
+      id: "1",
+      children: <>
+            <h3 className="af-click-item__title">
+              Fiche Orias précontractuelle
+            </h3>
+            <p className="af-click-item__subtitle">30 novembre 2021</p>
+            <p className="af-click-item__secondary">Signé électroniquement</p>
+          </>
+    }, {
+      id: "2",
+      children: <>
+            <h3 className="af-click-item__title">
+              Fiche Orias précontractuelle
+            </h3>
+            <p className="af-click-item__subtitle">30 novembre 2021</p>
+            <p className="af-click-item__secondary">Signé électroniquement</p>
+          </>,
+      isDisabled: true
+    }],
+    classModifier: ""
+  },
+  argTypes: {
+    classModifier: {
+      options: ["", "large"],
+      control: {
+        type: "select"
+      },
+      defaultValue: ""
+    }
+  }
+}`,...(u=(h=r.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var g,f,k;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  name: "ClickList link without icon",
+  render: args => <ClickList {...args} />,
+  args: {
+    items: [{
+      id: "1",
+      children: <>
+            <h3 className="af-click-item__title">
+              Fiche Orias précontractuelle
+            </h3>
+            <p className="af-click-item__subtitle">30 novembre 2021</p>
+            <p className="af-click-item__secondary">Signé électroniquement</p>
+          </>,
+      parentClickComponent: createClickItemParent("a", {
+        href: "https://github.com/AxaFrance/design-system",
+        target: "_blank",
+        rel: "noreferrer"
+      })
+    }, {
+      id: "2",
+      children: <>
+            <h3 className="af-click-item__title">
+              Fiche Orias précontractuelle
+            </h3>
+            <p className="af-click-item__subtitle">30 novembre 2021</p>
+            <p className="af-click-item__secondary">Signé électroniquement</p>
+          </>,
+      parentClickComponent: createClickItemParent("a", {
+        href: "https://github.com/AxaFrance/design-system",
+        target: "_blank",
+        rel: "noreferrer"
+      }),
+      isDisabled: true
+    }],
+    classModifier: ""
+  },
+  argTypes: {
+    classModifier: {
+      options: ["", "large"],
+      control: {
+        type: "select"
+      },
+      defaultValue: ""
+    }
+  }
+}`,...(k=(f=t.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};const v=["ClickListWithIcon","ClickListWithoutIcon","ClickListLinkWithoutIcon"],S=Object.freeze(Object.defineProperty({__proto__:null,ClickListLinkWithoutIcon:t,ClickListWithIcon:n,ClickListWithoutIcon:r,__namedExportsOrder:v,default:j},Symbol.toStringTag,{value:"Module"}));export{S as C,n as a,r as b,t as c};
---
+++ look-and-feel/react/1.0.0-ci.338/assets/ContentItemDuo-D5mF_oe_.js
@@ -0,0 +1,101 @@
+import{j as n}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as u}from"./index-FeUjBnvO.js";import{ae as m,af as o,ag as i}from"./index-Cb73D1Ps.js";import{C as p,D as r,V as s,a as l,b as a}from"./ContentItemDuo.stories-DafxmcJF.js";import"./index-uubelm5h.js";import"./iframe-D-IgPWKx.js";import"../sb-preview/runtime.js";import"./index-CfOt2XX2.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";import"./Title-5HvLqByk.js";/* empty css               */import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";import"./useIsSmallScreen-BMz0fP8s.js";import"./Tag-PCUeqJ0K.js";import"./Alert-yYw8AEPN.js";import"./error-BHKM2E7q.js";import"./Svg-C4poEASt.js";import"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-B4MiBKMF.js";import"./check_box_outline_blank-BvMHvB9Z.js";import"./CheckboxSelect-Dq6IwaT4.js";import"./Loader-DTzMALSS.js";import"./Radio-CYDN5Meu.js";import"./radio_button_unchecked-hw5p0x-H.js";import"./InputError-BsZAaKA6.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./NavBar-DFgCLS-S.js";import"./PreviousLink-D5jlWA8o.js";import"./arrow_back-DnyUknY1.js";import"./Link-BQTbBxg2.js";import"./close-DqeJ9fK5.js";import"./Modal-CD3ho01h.js";import"./Pagination-R-Zl63vI.js";import"./Skeleton-_BHz5ywW.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";function c(t){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",ol:"ol",pre:"pre",...u(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:p,name:"ContentItemDuo"}),`
+`,n.jsx(e.h1,{id:"contentitemduo",children:"ContentItemDuo"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"#import",children:"Import"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"#default-contentitemduo",children:"Default ContentItemDuo"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"#vertical-contentitemduo",children:"Vertical ContentItemDuo"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"#default-contentitemduo-list",children:"Default ContentItemDuo List"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"#vertical-contentitemduo-list",children:"Vertical ContentItemDuo List"})}),`
+`]}),`
+`,n.jsx(e.h2,{id:"import",children:"Import"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { ContentItemDuo } from "@axa-fr/design-system-look-and-feel-react";
+`})}),`
+`,n.jsx(e.h2,{id:"uses",children:"Uses"}),`
+`,n.jsx(e.h3,{id:"default-contentitemduo",children:"Default ContentItemDuo"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`export const DefaultContentItemDuoComponent = () => (
+  <ContentItemMono
+    label="Libellé"
+    value="Réponse"
+    isShowingDoneIcon
+    buttonText="En savoir plus"
+    onButtonClick
+  />
+);
+`})}),`
+`,n.jsx(o,{of:r}),`
+`,n.jsx(i,{of:r}),`
+`,n.jsx(e.h3,{id:"vertical-contentitemduo",children:"Vertical ContentItemDuo"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`export const VerticalContentItemDuoComponent = () => (
+  <ContentItemMono
+    isVertical
+    label="Le choc de véhicules terrestres à moteur, (voiture, trottinette à moteur…) ou la chute d’appareils aériens (avions, hélicoptères…) avec un propriétaire du véhicule adverse identifié ?"
+    value="Les incendies, explosions, implosions, fumée et foudre"
+    isShowingDoneIcon
+    buttonText="En savoir plus"
+    onButtonClick
+  />
+);
+`})}),`
+`,n.jsx(o,{of:s}),`
+`,n.jsx(i,{of:s}),`
+`,n.jsx(e.h3,{id:"default-contentitemduo-list",children:"Default ContentItemDuo List"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`export const DefaultContentItemDuoListComponent = () => (
+  <List classModifier="extra-padding">
+    <ContentItemMono
+      label="Libellé"
+      value="Réponse"
+      isShowingDoneIcon
+      buttonText="En savoir plus"
+      onButtonClick
+    />
+    <ContentItemMono
+      label="Libellé"
+      value="Réponse"
+      isShowingDoneIcon
+      buttonText="En savoir plus"
+      onButtonClick
+    />
+    <ContentItemMono
+      label="Libellé"
+      value="Réponse"
+      isShowingDoneIcon
+      buttonText="En savoir plus"
+      onButtonClick
+    />
+  </List>
+);
+`})}),`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(i,{of:l}),`
+`,n.jsx(e.h3,{id:"vertical-contentitemduo-list",children:"Vertical ContentItemDuo List"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`export const VerticalContentItemDuoListComponent = () => (
+  <List classModifier="extra-padding">
+    <ContentItemMono
+      isVertical
+      label="Le choc de véhicules terrestres à moteur, (voiture, trottinette à moteur…) ou la chute d’appareils aériens (avions, hélicoptères…) avec un propriétaire du véhicule adverse identifié ?"
+      value="Les incendies, explosions, implosions, fumée et foudre"
+      isShowingDoneIcon
+      buttonText="En savoir plus"
+      onButtonClick
+    />
+    <ContentItemMono
+      isVertical
+      label="Le choc de véhicules terrestres à moteur, (voiture, trottinette à moteur…) ou la chute d’appareils aériens (avions, hélicoptères…) avec un propriétaire du véhicule adverse identifié ?"
+      value="Les incendies, explosions, implosions, fumée et foudre"
+      isShowingDoneIcon
+      buttonText="En savoir plus"
+      onButtonClick
+    />
+    <ContentItemMono
+      isVertical
+      label="Le choc de véhicules terrestres à moteur, (voiture, trottinette à moteur…) ou la chute d’appareils aériens (avions, hélicoptères…) avec un propriétaire du véhicule adverse identifié ?"
+      value="Les incendies, explosions, implosions, fumée et foudre"
+      isShowingDoneIcon
+      buttonText="En savoir plus"
+      onButtonClick
+    />
+  </List>
+);
+`})}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(i,{of:a})]})}function on(t={}){const{wrapper:e}={...u(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(c,{...t})}):c(t)}export{on as default};
