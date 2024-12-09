cd29564f74df0ef98262c07077a4dbd5cb2e45cd
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2024-12-09 08:10:23 +0000

    deploy: 5c9c99b2567f6347a42cfaef3f094515e795a9d3

---
+++ look-and-feel/react/1.0.0-ci.335/assets/FIleUpload.stories-DX8AHTND.js
@@ -0,0 +1,53 @@
+import{j as s}from"./jsx-runtime-QvZ8i92b.js";import{F as m}from"./Title-EKhrPoNf.js";import"./index-uubelm5h.js";/* empty css               */import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";import"./useIsSmallScreen-BMz0fP8s.js";import"./Tag-PCUeqJ0K.js";import"./Alert-yYw8AEPN.js";import"./error-BHKM2E7q.js";import"./Svg-C4poEASt.js";import"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-DPn8M_Uc.js";import"./check_box_outline_blank-caNJpyLE.js";import"./CheckboxSelect-FCKGgETg.js";import"./Loader-DTzMALSS.js";import"./Radio-CYDN5Meu.js";import"./radio_button_unchecked-hw5p0x-H.js";import"./InputError-BsZAaKA6.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-CfOt2XX2.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./NavBar-DFgCLS-S.js";import"./PreviousLink-D5jlWA8o.js";import"./arrow_back-DnyUknY1.js";import"./Link-BQTbBxg2.js";import"./close-DqeJ9fK5.js";import"./Modal-CD3ho01h.js";import"./Pagination-R-Zl63vI.js";import"./Skeleton-_BHz5ywW.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";const K={title:"Components/Form/Input/FileUpload"},e={name:"FileUpload",render:({onChange:r,onView:t,onDelete:p,...a})=>s.jsx(m,{onChange:r,onView:t,onDelete:p,...a}),args:{id:"file-input",className:"",label:"Label",files:[{id:"1",name:" IMG_8904587880.jpg",size:109936,isLoading:!1},{id:"2",name:" IMG_465510680.jpg",size:5010,isLoading:!0},{id:"3",name:" IMG_8904587880.jpg",size:23e6,isLoading:!0}],globalError:"Erreur",errors:[{id:"3",message:"Votre fichier fait plus de 19 Mo."}],accept:"image/jpeg, image/png, application/*",buttonLabel:"Importer fichier",instructions:"2 fichiers max. / pdf, png, jpg, jpeg, gif / 19 Mo par fichier",dropzoneDescription:"Glissez/déposez vos fichiers",required:!1,isMobile:!1,multiple:!1},argTypes:{onChange:{action:"onChange"},onView:{action:"onView"},onDelete:{action:"onDelete"}}};var i,n,o;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  name: "FileUpload",
+  render: ({
+    onChange,
+    onView,
+    onDelete,
+    ...args
+  }) => <FileUpload onChange={onChange} onView={onView} onDelete={onDelete} {...args} />,
+  args: {
+    id: "file-input",
+    className: "",
+    label: "Label",
+    files: [{
+      id: "1",
+      name: " IMG_8904587880.jpg",
+      size: 109936,
+      isLoading: false
+    }, {
+      id: "2",
+      name: " IMG_465510680.jpg",
+      size: 5010,
+      isLoading: true
+    }, {
+      id: "3",
+      name: " IMG_8904587880.jpg",
+      size: 23000000,
+      isLoading: true
+    }],
+    globalError: "Erreur",
+    errors: [{
+      id: "3",
+      message: "Votre fichier fait plus de 19 Mo."
+    }],
+    accept: "image/jpeg, image/png, application/*",
+    buttonLabel: "Importer fichier",
+    instructions: "2 fichiers max. / pdf, png, jpg, jpeg, gif / 19 Mo par fichier",
+    dropzoneDescription: "Glissez/déposez vos fichiers",
+    required: false,
+    isMobile: false,
+    multiple: false
+  },
+  argTypes: {
+    onChange: {
+      action: "onChange"
+    },
+    onView: {
+      action: "onView"
+    },
+    onDelete: {
+      action: "onDelete"
+    }
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const P=["FileUploadStory"];export{e as FileUploadStory,P as __namedExportsOrder,K as default};
---
+++ look-and-feel/react/1.0.0-ci.335/assets/List-B3RQKq2V.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-QvZ8i92b.js";import{useMDXComponents as m}from"./index-FeUjBnvO.js";import{ae as e,af as s,ag as p}from"./index-D8rK9ShE.js";import{L as a,D as r}from"./List.stories-DtxNimzT.js";import"./index-uubelm5h.js";import"./iframe-CrMwjsPT.js";import"../sb-preview/runtime.js";import"./index-CfOt2XX2.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-DmeKSGxc.js";import"./index-DrFu-skq.js";import"./Svg-C4poEASt.js";import"./Title-EKhrPoNf.js";/* empty css               */import"./getComponentClassName-BDlozEvX.js";import"./index-vAxXS4R5.js";import"./useIsSmallScreen-BMz0fP8s.js";import"./Tag-PCUeqJ0K.js";import"./Alert-yYw8AEPN.js";import"./error-BHKM2E7q.js";import"./Button-GV2C7V7f.js";import"./Card-FCi1q2IJ.js";import"./Checkbox-DPn8M_Uc.js";import"./check_box_outline_blank-caNJpyLE.js";import"./CheckboxSelect-FCKGgETg.js";import"./Loader-DTzMALSS.js";import"./Radio-CYDN5Meu.js";import"./radio_button_unchecked-hw5p0x-H.js";import"./InputError-BsZAaKA6.js";import"./RadioSelect-CY_JYun0.js";import"./Select-DPf08Y0e.js";import"./IconBg-cuWLDUjJ.js";import"./Footer-Bbkzdz5M.js";import"./NavBar-DFgCLS-S.js";import"./PreviousLink-D5jlWA8o.js";import"./arrow_back-DnyUknY1.js";import"./Link-BQTbBxg2.js";import"./close-DqeJ9fK5.js";import"./Modal-CD3ho01h.js";import"./Pagination-R-Zl63vI.js";import"./Skeleton-_BHz5ywW.js";import"./Stepper-Br4-c7GM.js";import"./Tabs-uQNR_BBt.js";function n(i){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a,name:"List"}),`
+`,t.jsx(o.h1,{id:"list",children:"List"}),`
+`,t.jsx(o.h2,{id:"how-to-use",children:"How to use"}),`
+`,t.jsx(o.p,{children:"To use the List component import it like that:"}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-tsx",children:`import {
+  ClickItem,
+  ContentItemMono,
+  List,
+  Svg,
+} from "@axa-fr/design-system-look-and-feel-react";
+import trashIcon from "@material-symbols/svg-400/outlined/delete.svg";
+import publishedWithChangesIcon from "@material-symbols/svg-400/outlined/published_with_changes-fill.svg";
+
+const MyComponent = () => (
+  <List classModifier="first-separator-full-width">
+    <ContentItemMono secondaryText="nom.prénom@mail.fr">
+      Prénom NOM
+    </ContentItemMono>
+    <ClickItem icon={<Svg src={publishedWithChangesIcon} />}>
+      Modifier le profil
+    </ClickItem>
+    <ClickItem icon={<Svg src={trashIcon} />}>Supprimer le profil</ClickItem>
+  </List>
+);
+`})}),`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(p,{of:r})]})}function tt(i={}){const{wrapper:o}={...m(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(n,{...i})}):n(i)}export{tt as default};
