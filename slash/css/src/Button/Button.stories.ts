2400047282cb86e833322775ff5a18708831235c
pplancq <paul.plancq@outlook.fr>
2025-01-17 11:07:05 +0100

    feat(slash)!: restyle button with new-look Slash DS without Gosh variant
    BREAKING CHANGE: **button**, removed universes variant; use custom class if needed
    BREAKING CHANGE: **button**, removed hasIconLeft/hasIconRight; CSS auto-detects use of SVG or glyphicon
    BREAKING CHANGE: **button**, variant reverse is now renamed to secondary
    BREAKING CHANGE: **button**, variant success is now renamed to validated
    BREAKING CHANGE: **button**, removed disabled variant; now use button's disabled prop
    BREAKING CHANGE: **button**, removed circle variant

--- slash/css/src/Button/Button.stories.ts
+++ slash/css/src/Button/Button.stories.ts
@@ -2,48 +2,101 @@ import type { Meta, StoryObj } from "@storybook/html";
 import "./Button.scss";
 
 const meta: Meta = {
-  title: "Components/Button",
+  title: "Components/✅ Button",
+  parameters: {
+    layout: "centered",
+  },
 };
 
 export default meta;
 
-const MODIFIERS = [
-  "success",
-  "info",
-  "danger",
-  "reverse",
-  "disabled",
-  "small",
-  "hasiconLeft",
-  "hasiconRight",
-];
+const demoSvg = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+  </svg>`;
 
-export const Primary: StoryObj = {
+const demoGlyphicon = `
+  <i class="glyphicon glyphicon-pencil"></i>`;
+
+const VARIANT_CLASSES = ["primary", "secondary", "validated", "danger"];
+
+const SIZE_CLASSES = ["default", "small"];
+
+export const Button: StoryObj = {
   render: (args) => {
     const btn = document.createElement("button");
-    btn.innerHTML = args.icon
-      ? `<span class="af-btn__text">${args.label}</span> <span class="glyphicon glyphicon-${args.icon}" /> `
-      : args.label;
 
-    btn.className = [
-      "af-btn",
-      args.classModifier ? `af-btn--${args.classModifier}` : "",
-    ].join(" ");
+    btn.classList.add("af-btn");
+
+    if (args.disabled) {
+      btn.disabled = true;
+    }
+
+    if (args.variant !== "primary") {
+      btn.classList.add(`af-btn--${args.variant}`);
+    }
+
+    if (args.size !== "default") {
+      btn.classList.add(`af-btn--${args.size}`);
+    }
+
+    const getIcon = (type: string) => {
+      if (type === "svg") return demoSvg;
+      if (type === "glyphicon") return demoGlyphicon;
+      return "";
+    };
+    const leftIcon = getIcon(args.leftIcon);
+    const rightIcon = getIcon(args.rightIcon);
+
+    btn.innerHTML = `${leftIcon}
+  ${args.label}${rightIcon}
+`;
 
     return btn;
   },
   args: {
     label: "Button",
-    classModifier: "",
-    icon: null,
+    disabled: false,
+    variant: "primary",
+    size: "default",
+    leftIcon: "",
+    rightIcon: "",
   },
   argTypes: {
-    classModifier: {
-      options: MODIFIERS,
-      control: { type: "select" },
+    variant: {
+      options: VARIANT_CLASSES,
+      control: {
+        type: "select",
+      },
     },
-    icon: {
-      control: { type: "text" },
+    size: {
+      options: SIZE_CLASSES,
+      control: {
+        type: "select",
+      },
+    },
+    leftIcon: {
+      options: ["", "svg", "glyphicon"],
+      control: {
+        type: "select",
+        labels: {
+          "": "(none)",
+          svg: "SVG edit",
+          glyphicon: "Glyphicon edit",
+        },
+      },
+    },
+    rightIcon: {
+      options: ["", "svg", "glyphicon"],
+      control: {
+        type: "select",
+        labels: {
+          "": "(none)",
+          svg: "SVG edit",
+          glyphicon: "Glyphicon edit",
+        },
+      },
     },
   },
 };
