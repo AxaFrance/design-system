8eaaeba057038efd24f7d2bb5e6e4f9dc42e66af
JLou <jeanlou.pierme@gmail.com>
2025-01-20 14:53:09 +0100

    wip react 19

--- samples/vite/src/App.tsx
+++ samples/vite/src/App.tsx
@@ -1,10 +1,9 @@
 import { Header } from "@axa-fr/design-system-look-and-feel-react";
-import { Suspense } from "react";
-import { Navigate, NavLink, Route, Routes } from "react-router-dom";
-import Agent from "./Agent";
-import Client from "./Client";
+import { Navigate, NavLink, Route, Routes } from "react-router";
 
+import Agent from "./Agent";
 import "./App.css";
+import Client from "./Client";
 import "./Reset.css";
 const App = () => (
   <>
@@ -14,13 +13,11 @@ const App = () => (
       <NavLink to="/client">CLIENT</NavLink>,
       <NavLink to="/agent">AGENT</NavLink>,
     </Header>
-    <Suspense>
-      <Routes>
-        <Route path="/" element={<Navigate to={"/client"} />} />
-        <Route path="/client" element={<Client />} />
-        <Route path="/agent" element={<Agent />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route index element={<Navigate to={"/client"} />} />
+      <Route path="/client" element={<Client />} />
+      <Route path="/agent" element={<Agent />} />
+    </Routes>
   </>
 );
 
