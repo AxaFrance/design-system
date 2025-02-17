ff383c98e3acbe470af03269d547c2405e4bc663
Jonathan VACHERAT <jonathan.vacherat@axa.fr>
2025-02-17 11:19:50 +0100

    refactor(look&feel): select native

--- client/look-and-feel/react/src/Form/Select/Select.tsx
+++ client/look-and-feel/react/src/Form/Select/Select.tsx
@@ -1,60 +1,30 @@
 import "@axa-fr/design-system-look-and-feel-css/dist/Form/Select/Select.scss";
-import ReactSelect, {
-  ActionMeta,
-  ContainerProps,
-  GroupBase,
-  OptionProps,
-  SelectInstance,
-  SingleValue,
-  SingleValueProps,
-} from "react-select";
 import { ComponentPropsWithRef, forwardRef, useId } from "react";
 import classNames from "classnames";
-import { DropdownIndicator } from "./DropdownIndicator";
-import { CustomOption } from "./CustomOption";
-import { defaultAriaLiveMessages } from "./arialiveMessagesDefault";
 import { InputError } from "../InputError";
 
-type Option = { label: string; value: string | number };
-
-type Props = Omit<
-  ComponentPropsWithRef<ReactSelect>,
-  "placeholder" | "noOptionsMessage"
-> & {
+type Props = ComponentPropsWithRef<"select"> & {
   id?: string;
   label?: string;
-  options: { label: string; value?: string | number | readonly string[] }[];
-  value: Option | null;
   errorLabel?: string;
   placeholder?: string;
-  noOptionsMessage?: string;
-  onChange: (
-    newValue: SingleValue<Option>,
-    actionMeta: ActionMeta<string>,
-  ) => void;
-  disabled?: boolean;
-  required?: boolean;
 };
 
-const Select = forwardRef<
-  SelectInstance<unknown, boolean, GroupBase<unknown>>,
-  Props
->(
+const Select = forwardRef<HTMLSelectElement, Props>(
   (
-    {
-      id,
-      required,
-      disabled,
-      label,
-      errorLabel,
-      noOptionsMessage,
-      ...otherProps
-    },
+    { id, required, label, errorLabel, placeholder, children, ...otherProps },
     inputRef,
   ) => {
     const idError = useId();
     let inputId = useId();
     inputId = id || inputId;
+
+    const isShowingPlaceholder = otherProps.value === "";
+    const classname = classNames(
+      "af-form__select-input",
+      errorLabel && "af-form__select-input--error",
+      isShowingPlaceholder && "af-form__select-input-placeholder",
+    );
 
     return (
       <div>
@@ -64,61 +34,19 @@ const Select = forwardRef<
             {required && <span aria-hidden="true"> *</span>}
           </label>
         )}
-        <ReactSelect
-          inputId={inputId}
-          aria-errormessage={idError}
-          aria-invalid={Boolean(errorLabel)}
-          ariaLiveMessages={defaultAriaLiveMessages}
-          screenReaderStatus={({ count }: { count: number }) =>
-            `${count} résultat${count > 1 ? "s" : ""} disponible${count > 1 ? "s" : ""}`
-          }
-          unstyled
-          isDisabled={disabled}
-          noOptionsMessage={() => noOptionsMessage || "Aucun résultat"}
-          components={{
-            DropdownIndicator,
-            Option: CustomOption,
-          }}
-          classNames={{
-            control: () => "af-form__input-select",
-            menu: () => "af-form__input-select-menu",
-            menuList: () => "af-form__select-menu-list",
-            container: ({
-              isFocused,
-              isDisabled,
-              selectProps: { menuIsOpen },
-            }: ContainerProps<unknown, boolean, GroupBase<unknown>>) =>
-              classNames(
-                "af-form__input-select-container",
-                isFocused && "af-form__input-select-container-focused",
-                isDisabled && "af-form__input-select-container-disabled",
-                errorLabel &&
-                  !isFocused &&
-                  !menuIsOpen &&
-                  "af-form__input-select-container-error",
-              ),
-            option: ({
-              isSelected,
-              isFocused,
-            }: OptionProps<unknown, boolean, GroupBase<unknown>>) =>
-              classNames(
-                "af-form__input-select-menu-options",
-                isSelected && "af-form__input-select-menu-options-selected",
-                isFocused && "af-form__input-select-menu-options-focused",
-              ),
-            singleValue: ({
-              isDisabled,
-            }: SingleValueProps<unknown, boolean, GroupBase<unknown>>) =>
-              classNames(
-                "af-form__select-single-value",
-                isDisabled && "af-form__select-single-value-disabled",
-              ),
-            dropdownIndicator: () => "af-form__select-dropdown-indicator",
-          }}
-          className="af-form__input-select"
+        <select
+          className={classname}
           {...otherProps}
           ref={inputRef}
-        />
+          id={inputId}
+        >
+          {Boolean(placeholder) && (
+            <option disabled value="">
+              {placeholder}
+            </option>
+          )}
+          {children}
+        </select>
         {errorLabel && <InputError id={idError} message={errorLabel} />}
       </div>
     );
