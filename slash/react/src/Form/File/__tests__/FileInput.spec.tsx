e76afb253f81595ea6e15e684d52d94b124e6b19
BEN MUSTAPHA KHALED <B662PQ@login.axa>
2025-02-28 11:06:38 +0100

    feat(slash): ajout d'une propriété fileaction au composant fileinput

--- slash/react/src/Form/File/__tests__/FileInput.spec.tsx
+++ slash/react/src/Form/File/__tests__/FileInput.spec.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MessageTypes } from "../../core";
 import { FileInput } from "../FileInput";
+import { FilePreview } from "../File";
 
 describe("<FileInput>", () => {
   it("renders FileInput correctly", () => {
@@ -15,5 +17,81 @@ describe("<FileInput>", () => {
       />,
     );
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+const values = [
+  {
+    id: "000003",
+    file: {
+      name: "error-file.jpg",
+      size: 100,
+      preview: "https://via.placeholder.com/150Cx50",
+      type: "image",
+    } as FilePreview,
+  },
+];
+
+describe("<FileInput>", () => {
+  it("renders FileInput correctly and handles delete file action", async () => {
+    const handleChange = vi.fn();
+
+    render(
+      <FileInput
+        label="File"
+        id="id"
+        name="file"
+        values={values}
+        onChange={handleChange}
+        accept="image/jpeg, image/png, application/*"
+        messageType={MessageTypes.error}
+      />,
+    );
+    userEvent.click(screen.getByRole("button", { name: "Supprimer" }));
+    await waitFor(() => {
+      expect(handleChange).toHaveBeenCalledWith(
+        expect.objectContaining({
+          fileAction: "delete",
+          id: "id",
+          name: "file",
+          values: [],
+        }),
+      );
+    });
+  });
+});
+
+describe("<FileInput>", () => {
+  it("renders FileInput correctly and handles add file action", async () => {
+    const handleChange = vi.fn();
+    const user = userEvent.setup();
+    render(
+      <FileInput
+        label="File"
+        id="id"
+        data-testid="file"
+        name="file"
+        type="file"
+        fileLabel="file"
+        values={values}
+        onChange={handleChange}
+        accept="image/jpeg, image/png, application/*"
+        messageType={MessageTypes.error}
+      />,
+    );
+    const mockFile = new File(["hello"], "hello.png", { type: "image/png" });
+    const input = screen.getByLabelText("File");
+
+    await user.upload(input, mockFile).finally(() => SubmitEvent);
+    await waitFor(() => {
+      expect(handleChange).toHaveBeenCalledWith(
+        expect.objectContaining({
+          fileAction: "add",
+          id: "id",
+          name: "file",
+          errors: [],
+        }),
+      );
+    });
   });
 });
