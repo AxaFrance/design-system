477749a9f540d17c7ab971374ed6bcbfae58baf8
pplancq <paul.plancq@outlook.fr>
2025-03-03 09:56:48 +0100

    chore(design-system): update Storybook configuration and disable storybook Telemetry

--- apps/look-and-feel-stories-css/.storybook/main.ts
+++ apps/look-and-feel-stories-css/.storybook/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import type { StorybookConfig } from "@storybook/html-vite";
 
 import { join, dirname } from "path";
@@ -6,9 +7,10 @@ import { join, dirname } from "path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -21,5 +23,10 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/html-vite"),
     options: {},
   },
+  core: {
+    disableTelemetry: true,
+  },
 };
+
+// eslint-disable-next-line import/no-default-export
 export default config;
