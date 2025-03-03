6d3a4f0e622938b6643f2166f899a783bb3acb58
pplancq <paul.plancq@outlook.fr>
2025-03-03 09:56:48 +0100

    chore(design-system): update Storybook configuration and disable storybook Telemetry

--- apps/slash-stories/.storybook/main.ts
+++ apps/slash-stories/.storybook/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import type { StorybookConfig } from "@storybook/react-vite";
 import { dirname, join } from "path";
 
@@ -8,6 +9,7 @@ import { dirname, join } from "path";
 function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
 const config: StorybookConfig = {
   stories: [
     "../src/Home.mdx",
@@ -26,5 +28,10 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  core: {
+    disableTelemetry: true,
+  },
 };
+
+// eslint-disable-next-line import/no-default-export
 export default config;
