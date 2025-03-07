58808ecce42b007ae710e9a76dbf1e0cbee22466
Samuel Gomez <samuel.gomez@axa.fr>
2025-03-07 10:41:21 +0100

    refactor(apollo): rename files components

--- client/apollo/react/src/Button/Button.test.tsx
+++ client/apollo/react/src/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import { Button, buttonVariants, type ButtonVariants } from ".";
+import { Button, buttonVariants, type ButtonVariants } from "./Button";
 
 describe("Button component", () => {
   it.each(Object.keys(buttonVariants))(
