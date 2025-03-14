538fc258a08fdf464a49ad7f4f93bacc633717f1
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2025-03-14 16:53:05 +0100

    feat(slash): add navBarProps to MenuTitleWrapper

--- slash/react/src/Layout/Header/MenuTitleWrapper/MenuTitleWrapper.tsx
+++ slash/react/src/Layout/Header/MenuTitleWrapper/MenuTitleWrapper.tsx
@@ -1,12 +1,13 @@
-import { useCallback, useState } from "react";
-import { NavBar } from "../NavBar";
+import { useCallback, useState, type ComponentProps } from "react";
 import { HeaderTitle } from "../HeaderTitle/HeaderTitle";
+import { NavBar } from "../NavBar";
 
 type TMenuTitleWrapperProps = {
   children: React.ReactNode;
   menuVisible: boolean;
   subtitle: string;
   title: string;
+  navBarProps?: Partial<ComponentProps<typeof NavBar>>;
 };
 
 const MenuTitleWrapper = ({
@@ -14,6 +15,7 @@ const MenuTitleWrapper = ({
   menuVisible,
   subtitle,
   title,
+  navBarProps,
 }: TMenuTitleWrapperProps) => {
   const [isMenuVisible, setIsMenuVisible] = useState(menuVisible);
 
@@ -25,7 +27,7 @@ const MenuTitleWrapper = ({
 
   return (
     <>
-      <NavBar isVisible={isMenuVisible} onClick={handleClick}>
+      <NavBar isVisible={isMenuVisible} onClick={handleClick} {...navBarProps}>
         {children}
       </NavBar>
       <HeaderTitle title={title} subtitle={subtitle} toggleMenu={handleClick} />
