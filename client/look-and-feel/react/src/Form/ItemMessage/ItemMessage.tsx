b411011b6f7f6c9dc8165f636f0cc0a6e7ade615
pplancq <paul.plancq@outlook.fr>
2025-03-21 15:36:27 +0100

    chore(look&feel): fix lint for ci publish client

--- client/look-and-feel/react/src/Form/ItemMessage/ItemMessage.tsx
+++ client/look-and-feel/react/src/Form/ItemMessage/ItemMessage.tsx
@@ -1,1 +1,2 @@
+/* eslint-disable import/no-extraneous-dependencies */
 export { ItemMessage } from "@axa-fr/design-system-apollo-react/lf";
--- client/look-and-feel/react/src/Grid/DebugGrid.tsx
+++ client/look-and-feel/react/src/Grid/DebugGrid.tsx
@@ -1,1 +1,2 @@
+/* eslint-disable import/no-extraneous-dependencies */
 export { DebugGrid } from "@axa-fr/design-system-apollo-react/lf";
