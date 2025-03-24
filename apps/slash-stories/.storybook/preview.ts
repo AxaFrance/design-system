daa3924139652b2fec3917a181b678ec0bbea234
Martin Stievenart <stievenart.m@gmail.com>
2025-03-24 15:46:03 +0100

    feat(slash): add css tokens

--- apps/slash-stories/.storybook/preview.ts
+++ apps/slash-stories/.storybook/preview.ts
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import "@axa-fr/design-system-slash-css/dist/common/icons.scss";
-import "@axa-fr/design-system-slash-css/dist/common/tokens.scss";
+import "@axa-fr/design-system-slash-css/dist/common/tokens.css";
 import "@fontsource/source-sans-pro";
 import type { Preview } from "@storybook/react";
 
