ecc8742e4bc02d05f459818b25ba0681dfd18876
FROGER Baptiste <b405nk@login.axa>
2025-03-27 11:47:04 +0100

    feat(apollo): link

---
+++ client/apollo/react/src/Link/LinkApollo.tsx
@@ -0,0 +1,3 @@
+import "@axa-fr/design-system-apollo-css/dist/Link/LinkApollo.scss";
+
+export { Link } from "./LinkCommon";
---
+++ client/apollo/react/src/Link/LinkCommon.tsx
@@ -0,0 +1,52 @@
+import openInNew from "@material-symbols/svg-400/outlined/open_in_new.svg";
+import {
+  ComponentPropsWithoutRef,
+  PropsWithChildren,
+  useMemo,
+  type ReactNode,
+} from "react";
+import { Svg } from "../Svg/Svg";
+import { getComponentClassName } from "../utilities/helpers/getComponentClassName";
+
+type LinkProps = {
+  openInNewTab?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  className?: string;
+  classModifier?: string;
+} & ComponentPropsWithoutRef<"a">;
+
+export const Link = ({
+  href,
+  openInNewTab = false,
+  leftIcon,
+  rightIcon,
+  children,
+  className,
+  classModifier = "",
+  ...props
+}: PropsWithChildren<LinkProps>) => {
+  const newTabProps = openInNewTab && {
+    target: "_blank",
+    rel: "noopener noreferrer",
+  };
+
+  const componentClassName = useMemo(
+    () =>
+      getComponentClassName(
+        "af-link",
+        className,
+        `${classModifier}${!className && openInNewTab ? " openInNewTab" : ""}`,
+      ),
+    [classModifier, className, openInNewTab],
+  );
+
+  return (
+    <a className={componentClassName} href={href} {...newTabProps} {...props}>
+      {leftIcon}
+      {children}
+      {(openInNewTab || Boolean(rightIcon)) &&
+        (rightIcon ?? <Svg src={openInNew} />)}
+    </a>
+  );
+};
