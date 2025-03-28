f837a1a49f161b3c4c7821aaf8a889b41b26df16
MELLARD Quentin <mell.quentin@gmail.com>
2025-03-28 15:28:29 +0100

    feat(slash): ajout content dans les titres
    fixes #1023

--- slash/react/src/Title/__tests__/Title.test.tsx
+++ slash/react/src/Title/__tests__/Title.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { axe } from "jest-axe";
 import { Title } from "../Title";
 
@@ -83,5 +83,51 @@ describe("Title", () => {
 
     // Assert
     expect(await axe(container)).toHaveNoViolations();
+  });
+
+  it("should be wrapped in a container", async () => {
+    // Act
+    render(<Title heading="h3">A title</Title>);
+
+    // Assert
+    const container = screen.getByRole("heading", {
+      name: /A title/,
+      level: 3,
+    }).parentElement as HTMLElement;
+    expect(container.getAttribute("class")).toEqual("af-title--container");
+  });
+
+  it("content left should be child of heading", () => {
+    // Act
+    render(
+      <Title heading="h3" contentLeft={<p>Some content</p>}>
+        A title
+      </Title>,
+    );
+
+    // Assert
+    const heading = screen.getByRole("heading", {
+      level: 3,
+    });
+
+    expect(within(heading).getByText("Some content")).toBeInTheDocument();
+  });
+
+  it("content right should be child of content-right div", () => {
+    // Act
+    render(
+      <Title heading="h3" contentRight={<p>Some content</p>}>
+        A title
+      </Title>,
+    );
+
+    // Assert
+    const container = screen.getByRole("heading", {
+      level: 3,
+    }).parentElement!;
+
+    expect(
+      within(container.childNodes[1] as HTMLElement).getByText("Some content"),
+    ).toBeInTheDocument();
   });
 });
