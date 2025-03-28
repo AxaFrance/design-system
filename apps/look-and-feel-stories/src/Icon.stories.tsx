cbb09384d7163540dae9d17d2c975a6158fd2976
Pierre-Joseph BEAUGENDRE <pierrejoseph.beaugendre@axa.fr>
2025-03-28 16:05:15 +0100

    fix(apollo,look&feel): wrong import on lf

--- apps/look-and-feel-stories/src/Icon.stories.tsx
+++ apps/look-and-feel-stories/src/Icon.stories.tsx
@@ -2,7 +2,7 @@ import {
   Icon,
   iconSizeVariants,
   iconVariants,
-} from "@axa-fr/design-system-apollo-react";
+} from "@axa-fr/design-system-apollo-react/lf";
 import type { Meta, StoryObj } from "@storybook/react";
 import bank from "@material-symbols/svg-700/rounded/account_balance_wallet-fill.svg";
 
