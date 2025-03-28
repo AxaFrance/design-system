51111d2269de283da1253da7a0d9eda0c65151e1
Julien Gaudet <julien.gaudet@axa.fr>
2025-03-28 14:10:28 +0100

    feat(slash): add a role img for alert illustration icon

--- slash/react/src/Alert/Alert.tsx
+++ slash/react/src/Alert/Alert.tsx
@@ -36,6 +36,7 @@ export const Alert = ({
       <div className="af-alert__title">
         <div className="af-alert__title-icon">
           <i
+            role="presentation"
             className={
               icon || TypeIcons[classModifier as keyof typeof TypeIcons]
             }
