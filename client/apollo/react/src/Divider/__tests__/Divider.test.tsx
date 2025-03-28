633d3579bc4eeba43ef46df40179eeff75300209
Jovan BIENVENU <B519MK@login.axa>
2025-03-28 15:18:30 +0100

    feat(apollo): add Divider

--- client/apollo/react/src/Divider/__tests__/Divider.test.tsx
+++ client/apollo/react/src/Divider/__tests__/Divider.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { Divider } from "../Divider";
+import { Divider } from "../DividerCommon";
 
 describe("Divider component", () => {
   it("should render with default class name", () => {
