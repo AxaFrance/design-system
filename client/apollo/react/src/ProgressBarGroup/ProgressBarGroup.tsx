f53d675b255acc010480419d33dc326f7ce29c6c
SOW Mouhamadou moustapha <b469bb@login.axa>
2025-04-03 15:36:07 +0200

    feat(apollo,look&feel): progressbar, progressbargroup, stepper

---
+++ client/apollo/react/src/ProgressBarGroup/ProgressBarGroup.tsx
@@ -0,0 +1,58 @@
+import classNames from "classnames";
+import { ComponentProps, ComponentType, useCallback, useId } from "react";
+import { ProgressBar } from "../ProgressBar/ProgressBarCommon";
+
+const INITIAL_STEPPER_PROGRESS = 10;
+const MAX_STEPPER_PROGRESS = 100;
+
+export type ProgressBarGroupProps = {
+  currentStepProgress?: number;
+  currentStep: number;
+  nbSteps?: 3 | 4 | 5 | 6 | 7 | 8;
+  label?: string;
+  className?: string;
+  ProgressBarComponent: ComponentType<ComponentProps<typeof ProgressBar>>;
+};
+
+export const ProgressBarGroup = ({
+  currentStepProgress = INITIAL_STEPPER_PROGRESS,
+  nbSteps = 4,
+  currentStep,
+  label,
+  className,
+  ProgressBarComponent,
+}: ProgressBarGroupProps) => {
+  const stepperId = useId();
+
+  const getCurrentProgress: (index: number) => number = useCallback(
+    (index) => {
+      if (index < currentStep) {
+        return MAX_STEPPER_PROGRESS;
+      }
+      if (index === currentStep) {
+        if (currentStepProgress === 0) {
+          return INITIAL_STEPPER_PROGRESS;
+        }
+        return currentStepProgress;
+      }
+      return 0;
+    },
+    [currentStep, currentStepProgress],
+  );
+
+  return (
+    <div
+      id={stepperId}
+      role="group"
+      aria-label={label}
+      className={classNames("af-progress-bar-group", className)}
+    >
+      {[...Array(nbSteps).keys()].map((index) => (
+        <ProgressBarComponent
+          key={`${stepperId}-${index}`}
+          value={getCurrentProgress(index)}
+        />
+      ))}
+    </div>
+  );
+};
