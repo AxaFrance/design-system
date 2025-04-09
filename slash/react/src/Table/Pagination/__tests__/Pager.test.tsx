94c2d64389f7e6710cad37bd81d4ae898de5d713
Habib KECHRID <kechrid.habib@outlook.com>
2025-04-09 14:43:09 +0200

    fix(slash): added test for aria-label default value

--- slash/react/src/Table/Pagination/__tests__/Pager.test.tsx
+++ slash/react/src/Table/Pagination/__tests__/Pager.test.tsx
@@ -20,3 +20,24 @@ it("should render Pager correctly with a select having the right aria label", ()
   const link = screen.getByRole("combobox");
   expect(link).toHaveAttribute("aria-label", "Affichage");
 });
+
+it("should render Pager correctly with a select having a default aria label", () => {
+  render(
+    <>
+      <Table>{/* Table content */}</Table>
+      <Paging
+        currentPage={3}
+        numberItems={10}
+        displayLabel="Affichage"
+        numberPages={9}
+        mode="default"
+      />
+    </>,
+  );
+
+  const link = screen.getByRole("combobox");
+  expect(link).toHaveAttribute(
+    "aria-label",
+    "Modifier le nombre d'éléments à afficher dans le tableau",
+  );
+});
