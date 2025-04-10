bca712e63cf32bf0793275bffdcb771d630fbd31
Samuel Gomez <samuel.gomez@axa.fr>
2025-04-10 10:52:41 +0200

    feat(apollo,look&feel)!: message (alert)

---
+++ apps/apollo-stories/src/components/Message/render.tsx
@@ -0,0 +1,51 @@
+import { type Args } from "storybook/internal/types";
+import {
+  Button,
+  Link,
+  Message,
+  messageVariants,
+} from "@axa-fr/design-system-apollo-react";
+
+export const renderMessageAll = () => (
+  <div className="af-message-demo">
+    {Object.values(messageVariants).map((variant) => (
+      <Message
+        key={variant}
+        variant={variant}
+        title={`Title for ${variant} with Button`}
+        action={<Button variant="ghost">Call to action</Button>}
+      >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt non,
+        praesentium inventore dolor in saepe harum ad ducimus sapiente tenetur,
+        deleniti fugit provident necessitatibus? Vero reiciendis doloremque eos
+        nemo cum.
+      </Message>
+    ))}
+    {Object.values(messageVariants).map((variant) => (
+      <Message
+        key={variant}
+        variant={variant}
+        title={`Title for ${variant} with Link`}
+        action={
+          <Link href="https://www.axa.fr" openInNewTab>
+            Axa.fr
+          </Link>
+        }
+      >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt non,
+        praesentium inventore dolor in saepe harum ad ducimus sapiente tenetur,
+        deleniti fugit provident necessitatibus? Vero reiciendis doloremque eos
+        nemo cum.
+      </Message>
+    ))}
+  </div>
+);
+
+export const renderMessage = ({ variant, onClick, ...args }: Args) => (
+  <Message variant={variant} onClick={onClick} {...args} title="Title message">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt non,
+    praesentium inventore dolor in saepe harum ad ducimus sapiente tenetur,
+    deleniti fugit provident necessitatibus? Vero reiciendis doloremque eos nemo
+    cum.
+  </Message>
+);
