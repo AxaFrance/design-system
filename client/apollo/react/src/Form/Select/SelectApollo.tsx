1594ee0cdd64afd8c819034278d3b71974d4f8d2
S865997 <tej.boolakee.accenture@axa.fr>
2025-04-11 12:18:12 +0200

    feat(apollo,look&feel): composant select retours pr

--- client/apollo/react/src/Form/Select/SelectApollo.tsx
+++ client/apollo/react/src/Form/Select/SelectApollo.tsx
@@ -1,3 +1,18 @@
 import "@axa-fr/design-system-apollo-css/dist/Form/Select/SelectApollo.scss";
+import type { ComponentProps } from "react";
+import { ItemLabel } from "../ItemLabel/ItemLabelApollo";
+import { ItemMessage } from "../ItemMessage/ItemMessageApollo";
+import { Select as SelectCommon } from "./SelectCommon";
 
-export { Select } from "./SelectCommon";
+export const TextInput = (
+  props: Omit<
+    ComponentProps<typeof SelectCommon>,
+    "ItemLabelComponent" | "ItemMessageComponent"
+  >,
+) => (
+  <SelectCommon
+    {...props}
+    ItemLabelComponent={ItemLabel}
+    ItemMessageComponent={ItemMessage}
+  />
+);
