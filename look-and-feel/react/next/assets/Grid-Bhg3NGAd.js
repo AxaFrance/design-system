eb2e9c2c33e42e67bb7be5bc19b9348b2bfbf545
JLou <JLou@users.noreply.github.com>
2025-04-11 12:46:52 +0000

    deploy: 6a4525c97d107e4b23da756ad55bc2cfee292810

---
+++ look-and-feel/react/next/assets/Grid-Bhg3NGAd.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-BJaai1kI.js";import{M as f,C as i,a as m}from"./index-CVg-PBzV.js";import{Form as e}from"./GridExampleForm.stories-DgTYyvBY.js";import{Page as p}from"./GridExamplePage.stories-jTa9vOqB.js";import{G as x,W as n}from"./GridWireframe.stories-CvzsQc6q.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B-Ah4GOn.js";import"./index-BsNqBstw.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";/* empty css                      */import"./BasePictureLF-DkRpuPn0.js";import"./constants-BUkAsgEf.js";import"./DebugGrid-C6wm-xHi.js";import"./TextInputLF-C1wQqEQE.js";import"./ItemLabelLF-B3qeHIPJ.js";import"./ItemMessageCommon-CpWSDhRP.js";import"./CheckboxSelect-DHyh-n_a.js";import"./Title-BIRSRgVS.js";import"./IconBg-OWl9mlT3.js";import"./home-BuLaEOyh.js";import"./MyHeader-BzrRWTLu.js";import"./Footer-BuodtEyP.js";import"./useIsSmallScreen-B3lIGOTq.js";import"./person-fill-CNn1Gt-j.js";import"./logo-axa-Cqr9fAkX.js";import"./NavBar-bO0XVBjK.js";import"./PreviousLink-CcS15EN3.js";import"./arrow_back-DnyUknY1.js";import"./ClickItem-CBVCCSce.js";import"./ClickItem.helpers-CDlaBFDl.js";import"./List-BuDwXwV1.js";import"./Card-Duuuo1TZ.js";import"./ContentItemMono-DU5RoLRP.js";import"./call-fill-Nx5D3UHq.js";import"./article-fill-B_-J39Ry.js";import"./ContentItemDuo-COExjZcq.js";function s(t){const o={a:"a",h2:"h2",p:"p",...a(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(f,{of:x}),`
+`,r.jsx(o.h2,{id:"grid-system",children:"Grid System"}),`
+`,r.jsxs(o.p,{children:["More info here : ",r.jsx(o.a,{href:"https://axafrance.github.io/design-system/apollo/react/latest?path=/docs/guidelines-grid-system--grid",rel:"nofollow",children:"Grid documentation"})]}),`
+`,r.jsx(o.h2,{id:"wireframe",children:"Wireframe"}),`
+`,r.jsx(i,{of:n}),`
+`,r.jsx(m,{of:n}),`
+`,r.jsx(o.h2,{id:"example-page",children:"Example page"}),`
+`,r.jsx(i,{of:p}),`
+`,r.jsx(m,{of:p}),`
+`,r.jsx(o.h2,{id:"example-form",children:"Example form"}),`
+`,r.jsx(i,{of:e}),`
+`,r.jsx(m,{of:e})]})}function Y(t={}){const{wrapper:o}={...a(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(s,{...t})}):s(t)}export{Y as default};
---
+++ look-and-feel/react/next/assets/index-ByTDyliE.js
@@ -0,0 +1,1 @@
+import"./preview-AMP2w5lw.js";import"./DocsRenderer-CFRXHY34-3cQRKeMY.js";import"./index-CVg-PBzV.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
