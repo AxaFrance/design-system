5c707df13fe0ec48a5be2c96eafe53345a74e232
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-04-14 14:13:24 +0000

    deploy: 77883b982d9e7e9e21c949d270109fd03897f93d

--- look-and-feel/css/next/assets/InputError.stories-Ct72vRFk.js
+++ look-and-feel/css/next/assets/InputError.stories-Ct72vRFk.js
@@ -1,4 +1,4 @@
-/* empty css                   */const c={title:"Components/Form/InputError"},e={name:"Input Error",render:a=>{const r=document.createElement("div");return r.className="af-input-error",r.innerHTML=`
+const o={title:"Components/Form/InputError"},e={name:"Input Error",render:a=>{const r=document.createElement("div");return r.className="af-input-error",r.innerHTML=`
         <svg class="af-input-error__icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ErrorOutlineIcon"><path d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2M12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8"></path></svg>
         ${a.message&&`<span class="af-input-error__message">${a.message}</span>`}
     `,r},args:{message:"Error Message"}};var s,n,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
@@ -15,4 +15,4 @@
   args: {
     message: "Error Message"
   }
-}`,...(t=(n=e.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,c as default};
+}`,...(t=(n=e.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,o as default};
