132d71fffaf562b1ecb6e3346eed05e484e6b1df
JLou <jeanlou.pierme@gmail.com>
2025-04-14 11:25:28 +0200

    refactor(slash): remove legacy field usage
    - Removed legacy field usage from the codebase
    - Updated stories of inputs
    - Added role group and radiogroup to CheckboxInput and RadioInput respectively
    - HelpMessage is linked to inputs
    
    fixes #850 #993 #1106 #1115

--- slash/react/src/Form/Choice/ChoiceInput.tsx
+++ slash/react/src/Form/Choice/ChoiceInput.tsx
@@ -1,10 +1,9 @@
-import { forwardRef, useId, type ComponentProps } from "react";
-import { LegacyField, useInputClassModifier, useOptionsWithId } from "../core";
+import { forwardRef, type ComponentProps } from "react";
+import { Field, type ConsumerFieldProps } from "../core";
 import { Choice } from "./Choice";
-import { useAriaInvalid } from "../core/useAriaInvalid";
 
 type Props = ComponentProps<typeof Choice> &
-  Omit<ComponentProps<typeof LegacyField>, "children">;
+  Omit<ConsumerFieldProps, "children">;
 
 const defaultOptions = [
   { label: "Oui", value: true },
@@ -12,75 +11,30 @@ const defaultOptions = [
 ];
 
 const ChoiceInput = forwardRef<HTMLInputElement, Props>(
-  (
-    {
-      id,
-      name,
-      messageType,
-      message,
-      className,
-      classModifier,
-      isVisible,
-      classNameContainerLabel,
-      classNameContainerInput,
-      label,
-      forceDisplayMessage,
-      options = defaultOptions,
-      disabled,
-      required,
-      ...otherProps
-    },
-    inputRef,
-  ) => {
-    const errorUseId = useId();
-    const newOptions = useOptionsWithId(
-      options.map((o) => ({
-        ...o,
-        value: `${o.value}`,
-      })),
-    );
-
-    const { inputClassModifier, inputFieldClassModifier } =
-      useInputClassModifier(
-        classModifier ?? "",
-        disabled ?? false,
-        false,
-        required,
-      );
-
-    const firstId = newOptions?.[0]?.id ?? "";
-    const choiceOptions = newOptions.map((o) => ({
-      ...o,
-      value: o.value === "true",
-    }));
-    const isInvalid = useAriaInvalid(message, forceDisplayMessage, messageType);
+  ({ label, options = defaultOptions, ...otherProps }, inputRef) => {
     return (
-      <LegacyField
+      <Field
         label={label}
-        id={firstId}
-        message={message}
-        messageType={messageType}
-        isVisible={isVisible}
-        forceDisplayMessage={forceDisplayMessage}
-        className={className}
-        classModifier={inputFieldClassModifier}
-        classNameContainerLabel={classNameContainerLabel}
-        classNameContainerInput={classNameContainerInput}
-        errorId={errorUseId}
-      >
-        <Choice
-          {...otherProps}
-          id={id}
-          name={name}
-          ref={inputRef}
-          classModifier={inputClassModifier}
-          options={options ? choiceOptions : undefined}
-          required={required}
-          disabled={disabled}
-          aria-describedby={errorUseId}
-          aria-invalid={isInvalid}
-        />
-      </LegacyField>
+        roleContainer="radiogroup"
+        {...otherProps}
+        renderInput={({
+          classModifier,
+          id,
+          errorId,
+          ariaInvalid,
+          ...props
+        }) => (
+          <Choice
+            id={id}
+            ref={inputRef}
+            classModifier={classModifier}
+            aria-describedby={errorId}
+            aria-invalid={ariaInvalid}
+            options={options}
+            {...props}
+          />
+        )}
+      />
     );
   },
 );
