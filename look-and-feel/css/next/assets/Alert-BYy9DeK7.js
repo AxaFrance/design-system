7726714987c756ebbeb78f7bb0526540af57206b
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-04-14 08:53:01 +0000

    deploy: c8dd50f38c536927dba1a9248f9209db5ffbe615

---
+++ look-and-feel/css/next/assets/Alert-BYy9DeK7.js
@@ -0,0 +1,4 @@
+import{j as e,M as r}from"./index-4VzRWw_Z.js";import{useMDXComponents as s}from"./index-D2HRBQ8N.js";import"./iframe-8l6aZ7bk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function o(t){const n={a:"a",h2:"h2",p:"p",strong:"strong",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Components/Alert"}),`
+`,e.jsx(n.h2,{id:"documentation",children:"Documentation"}),`
+`,e.jsxs(n.p,{children:["Alert component is ",e.jsx(n.strong,{children:"deprecated"})," and will be removed in the next major version. Please use the Message component instead."]}),`
+`,e.jsxs(n.p,{children:["More info here : ",e.jsx(n.a,{href:"https://axafrance.github.io/design-system/apollo/?path=/story/components-message--playground",rel:"nofollow",children:"Message documentation"})]})]})}function d(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{d as default};
---
+++ look-and-feel/css/next/assets/Grid-_Kfb6mix.js
@@ -0,0 +1,15 @@
+import{j as e,M as l,C as t,a as i}from"./index-4VzRWw_Z.js";import{useMDXComponents as p}from"./index-D2HRBQ8N.js";import{Form as n}from"./GridExampleForm.stories-RBBU3o0J.js";import{Page as s}from"./GridExamplePage.stories-f58rkSpX.js";import{G as x,W as m}from"./GridWireframe.stories-Bk7QljDk.js";import"./iframe-8l6aZ7bk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";/* empty css                  *//* empty css                      */import"./HeaderRender-BSq0LDi7.js";import"./utils-DJsnTcQJ.js";import"./renderDebugGrid-CIc3yifD.js";import"./ButtonRender-CZGcb1u2.js";import"./CheckBoxRender-BnPgTt_X.js";import"./FooterRender-CXnN22dm.js";import"./RadioRender-BNO6JHal.js";import"./TitleRender-tSWyE4Rs.js";import"./ContentItemDuoRender-DVKJePZp.js";function a(o){const r={a:"a",h2:"h2",p:"p",...p(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{of:x,parameters:{layout:"fullscreen",options:{storySort:{order:["Wireframe","Example Page","Example Form"]}}}}),`
+`,e.jsx(r.h2,{id:"grid-system",children:"Grid System"}),`
+`,e.jsxs(r.p,{children:["More info here : ",e.jsx(r.a,{href:"https://axafrance.github.io/design-system/apollo/react/latest?path=/docs/guidelines-grid-system--grid",rel:"nofollow",children:"Grid documentation"})]}),`
+`,e.jsx(r.h2,{id:"example-with-wireframe",children:"Example with wireframe"}),`
+`,e.jsx(r.p,{children:"This wireframe will show you the basic usage :"}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(r.h2,{id:"example-page",children:"Example page"}),`
+`,e.jsx(r.p,{children:"An example with lists components :"}),`
+`,e.jsx(t,{of:s}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(r.h2,{id:"example-form",children:"Example form"}),`
+`,e.jsx(r.p,{children:"An example with form components :"}),`
+`,e.jsx(t,{of:n}),`
+`,e.jsx(i,{of:n})]})}function P(o={}){const{wrapper:r}={...p(),...o.components};return r?e.jsx(r,{...o,children:e.jsx(a,{...o})}):a(o)}export{P as default};
