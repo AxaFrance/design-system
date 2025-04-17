e55daa6baf129d42f5a3e627e19028ab9c5b65cf
JLou <JLou@users.noreply.github.com>
2025-04-17 09:34:56 +0000

    deploy: 34167427953220c49eef8e1f24030751b56980c4

---
+++ apollo/react/next/assets/Link-jajPUhTT.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-BJaai1kI.js";import"./index-iR6bFKUE.js";import{L as s,D as t}from"./Link.stories-BUOlZdN_.js";import{M as m,C as a,a as p}from"./index-CUqCkIrO.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-BzCzsXGR.js";import"./iframe-D1yA2mY-.js";import"./DocsRenderer-CFRXHY34-DS5CE2NU.js";import"./react-18-7rs9glc5.js";import"./index-BsNqBstw.js";import"./MessageApollo-glY2offe.js";import"./LinkCommon-Bvf6w3xi.js";import"./Svg-BhXIr2Or.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function e(r){const o={code:"code",h2:"h2",h3:"h3",pre:"pre",...i(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:s,name:"Link"}),`
+`,n.jsx(o.h2,{id:"link",children:"Link"}),`
+`,n.jsx(o.h3,{id:"import",children:"Import"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-tsx",children:`import { Link } from "@axa-fr/design-system-apollo-react";
+`})}),`
+`,n.jsx(o.h3,{id:"use",children:"Use"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-tsx",children:`export const LinkComponent = () => (\r
+  <Link openInNewTab href="https://fakelink.com">\r
+    Plus de détails\r
+  </Link>\r
+);
+`})}),`
+`,n.jsx(o.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(a,{of:t}),`
+`,n.jsx(p,{of:t})]})}function w(r={}){const{wrapper:o}={...i(),...r.components};return o?n.jsx(o,{...r,children:n.jsx(e,{...r})}):e(r)}export{w as default};
---
+++ apollo/react/next/assets/index-iR6bFKUE.js
@@ -0,0 +1,1 @@
+import"./preview-BzCzsXGR.js";import"./DocsRenderer-CFRXHY34-DS5CE2NU.js";import"./index-CUqCkIrO.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
