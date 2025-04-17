37d671a596cf556a9f538457595a7acdaf1a8dcb
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-04-17 09:37:28 +0000

    deploy: 6d72bd4d2ca25abe601f23c4005893565ed41683

---
+++ look-and-feel/react/next/assets/index-Cq2w-tmm.js
@@ -0,0 +1,1 @@
+import"./preview-DV8Er49G.js";import"./DocsRenderer-CFRXHY34-BcDuePLP.js";import"./index-Jze3YmTl.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
--- look-and-feel/react/next/assets/index-Ctk-A1Dm.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Ce4yFlSe.js";import"./DocsRenderer-CFRXHY34-IGvbHdpU.js";import"./index-CQIFHqBM.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
