1dac2241759d56eda76f35a6378e8a8a0fad61ec
Paul PLANCQ <paul.plancq@outlook.fr>
2025-04-17 13:35:59 +0200

    chore(look&feel): remove storybook css

--- apps/look-and-feel-stories-css/.storybook/manager.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { addons } from "@storybook/manager-api";
-import { LookAndFeelCssTheme } from "./LFCssTheme";
-
-addons.setConfig({
-  theme: LookAndFeelCssTheme,
-});
