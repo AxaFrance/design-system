6f6ef35dfdf29cea1f8f4a9bc6b272d06528515f
S865997 <tej.boolakee.accenture@axa.fr>
2025-04-23 15:21:46 +0200

    feat(apollo, look&feel): retours pt select

--- apps/apollo-stories/src/components/Select/Select.stories.tsx
+++ apps/apollo-stories/src/components/Select/Select.stories.tsx
@@ -3,19 +3,23 @@ import { Meta, StoryObj } from "@storybook/react";
 import { ComponentProps } from "react";
 
 const meta: Meta<typeof Select> = {
-  component: Select,
-  title: "Components/Form/Select/Select",
-  args: {
-    value: "Lorem ipsum",
-    label: "Label",
-    disabled: false,
-    required: false,
-    placeholder: "Placeholder",
+    component: Select,
+    title: "Components/Form/Select/Select",
+    args: {
+        value: "Lorem ipsum",
+        description: "Description",
+        label: "Label",
+        disabled: false,
+        required: true,
+        placeholder: "Placeholder",
+        buttonLabel: "En savoir plus",
+        errorLabel: "",
+        helper: "Information complémentaires",
     error: "",
-  },
-  argTypes: {
-    onChange: { action: "onChange" },
-  },
+    },
+    argTypes: {
+        onChange: { action: "onChange" },
+    },
 };
 
 export default meta;
@@ -24,40 +28,40 @@ type StoryProps = ComponentProps<typeof Select>;
 type Story = StoryObj<StoryProps>;
 
 const render = ({ onChange, ...args }: StoryProps) => (
-  <div style={{ width: 300 }}>
-    <Select onChange={onChange} {...args}>
-      <option value="fun">For fun</option>
-      <option value="work">For work</option>
-      <option value="drink">For drink</option>
-    </Select>
-  </div>
+    <div style={{ width: 300 }}>
+        <Select onChange={onChange} {...args}>
+            <option value="fun">For fun</option>
+            <option value="work">For work</option>
+            <option value="drink">For drink</option>
+        </Select>
+    </div>
 );
 
 export const SelectStory: Story = {
-  name: "Select",
-  render,
+    name: "Select",
+    render,
 };
 
 export const SelectErrorStory: Story = {
-  name: "Select with Error",
-  render,
-  args: {
-    error: "Titre du Message",
-  },
-  argTypes: {
-    "aria-errormessage": {
-      control: { type: "select" },
+    name: "Select with Error",
+    render,
+    args: {
+        error: "Titre du Message",
     },
-  },
+    argTypes: {
+        "aria-errormessage": {
+            control: { type: "select" },
+        },
+    },
 };
 
 export const SelectDisabledStory: Story = {
-  name: "Select disabled",
-  render,
-  args: {
-    label: "Label",
-    description: "Description",
-    disabled: true,
-    helper: "Information complémentaire",
-  },
+    name: "Select disabled",
+    render,
+    args: {
+        label: "Label",
+        description: "Description",
+        disabled: true,
+        helper: "Information complémentaire",
+    },
 };
