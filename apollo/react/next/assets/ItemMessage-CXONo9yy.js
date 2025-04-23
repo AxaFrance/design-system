1064f38bccb267459220e90ad69e9db479f4f572
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-04-23 12:22:40 +0000

    deploy: eecff4dd5e57889093a5ac6966015a2c111a0181

---
+++ apollo/react/next/assets/ItemMessage-CXONo9yy.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-BJaai1kI.js";import"./index-BRHh4gmV.js";import{I as c,E as r,S as o}from"./ItemMessage.stories-DnMEpFSN.js";import{M as p,C as n,a as i}from"./index-C_mg3_Fr.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-CrCON-Ey.js";import"./iframe-BC5Ga1Ii.js";import"./DocsRenderer-CFRXHY34-DdWOmnT5.js";import"./react-18-7rs9glc5.js";import"./index-BsNqBstw.js";import"./TimelineVerticalApollo-DMIqffHS.js";import"./ItemMessageCommon-BDyjlk75.js";import"./check_circle-fill-BFt-p0hv.js";import"./Svg-DgVyijRv.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function a(t){const s={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:c,name:"ItemMessage"}),`
+`,e.jsx(s.h1,{id:"itemmessage",children:"ItemMessage"}),`
+`,e.jsx(s.p,{children:"To use the spinner import it like that:"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-tsx",children:`import { ItemMessage } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <ItemMessage type="success" message="My message" />;
+`})}),`
+`,e.jsx(s.h2,{id:"type",children:"Type"}),`
+`,e.jsxs(s.p,{children:["Use the ",e.jsx(s.code,{children:"type"})," prop to change the type of the item message. The available variants are ",e.jsx(s.code,{children:"error"})," (default) or ",e.jsx(s.code,{children:"success"}),"."]}),`
+`,e.jsx(s.h2,{id:"error-message",children:"Error Message"}),`
+`,e.jsx(n,{of:r}),`
+`,e.jsx(i,{of:r}),`
+`,e.jsx(s.h2,{id:"success-message",children:"Success Message"}),`
+`,e.jsx(n,{of:o}),`
+`,e.jsx(i,{of:o})]})}function _(t={}){const{wrapper:s}={...m(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(a,{...t})}):a(t)}export{_ as default};
--- apollo/react/next/assets/index-weoSlB3y.js
+++
@@ -1,1 +0,0 @@
-import"./preview-D3w5jEXI.js";import"./DocsRenderer-CFRXHY34-CjQcsIMe.js";import"./index-BEY8VOnn.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
