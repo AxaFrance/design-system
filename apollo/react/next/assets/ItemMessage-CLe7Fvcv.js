e3e626b2b9a52406afde4f3222c9856ef98c8db8
JLou <JLou@users.noreply.github.com>
2025-04-24 09:36:19 +0000

    deploy: c87e4b5a509fc52cd7df81045a4be3928dcb4577

---
+++ apollo/react/next/assets/ItemMessage-CLe7Fvcv.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-BJaai1kI.js";import"./index-4AXj2Y5H.js";import{I as c,E as r,S as o}from"./ItemMessage.stories-lNhfw5Wh.js";import{M as p,C as n,a as i}from"./index-Boh9_t5H.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-CZTm5lZP.js";import"./iframe-DplO3V8E.js";import"./DocsRenderer-CFRXHY34-BSspQNMB.js";import"./react-18-7rs9glc5.js";import"./index-BsNqBstw.js";import"./TimelineVerticalApollo-CgK76z1b.js";import"./ItemMessageCommon-tW9wGOBH.js";import"./check_circle-fill-BFt-p0hv.js";import"./Svg-DDjfGxib.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function a(t){const s={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:c,name:"ItemMessage"}),`
+`,e.jsx(s.h1,{id:"itemmessage",children:"ItemMessage"}),`
+`,e.jsx(s.p,{children:"To use the spinner import it like that:"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-tsx",children:`import { ItemMessage } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <ItemMessage type="success" message="My message" />;
+`})}),`
+`,e.jsx(s.h2,{id:"type",children:"Type"}),`
+`,e.jsxs(s.p,{children:["Use the ",e.jsx(s.code,{children:"type"})," prop to change the type of the item message. The available variants are ",e.jsx(s.code,{children:"error"})," (default) or ",e.jsx(s.code,{children:"success"}),"."]}),`
+`,e.jsx(s.h2,{id:"error-message",children:"Error Message"}),`
+`,e.jsx(n,{of:r}),`
+`,e.jsx(i,{of:r}),`
+`,e.jsx(s.h2,{id:"success-message",children:"Success Message"}),`
+`,e.jsx(n,{of:o}),`
+`,e.jsx(i,{of:o})]})}function _(t={}){const{wrapper:s}={...m(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(a,{...t})}):a(t)}export{_ as default};
---
+++ apollo/react/next/assets/index-4AXj2Y5H.js
@@ -0,0 +1,1 @@
+import"./preview-CZTm5lZP.js";import"./DocsRenderer-CFRXHY34-BSspQNMB.js";import"./index-Boh9_t5H.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
