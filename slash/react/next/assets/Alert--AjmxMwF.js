b3959336f523dda25974d8093d90293ed412b680
pplancq <pplancq@users.noreply.github.com>
2025-04-25 09:41:08 +0000

    deploy: 6627396393585308a06c5c59fa6fb63f2f4004d7

---
+++ slash/react/next/assets/Alert--AjmxMwF.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-G7nTXt62.js";import{M as o,C as d,a as l}from"./index-DHseYOsm.js";import{A as c,D as s}from"./Alert.stories-CG475f_m.js";import"./index-DbaA8R3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CQOXoTWJ.js";import"./index-BdSKqQiB.js";import"./index-BOKuxaB8.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Loader-C1uJHYLx.js";/* empty css              */import"./index-28pPPJTf.js";function t(n){const e={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...i(),...n.components};return r.jsxs(r.Fragment,{children:[r.jsx(o,{of:c}),`
+`,r.jsx(e.h2,{id:"alert",children:"Alert"}),`
+`,r.jsx(e.h3,{id:"import",children:"Import"}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-tsx",children:`import { Alert } from "@axa-fr/design-system-slash-react/";
+`})}),`
+`,r.jsx(e.h3,{id:"use",children:"Use"}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-tsx",children:`export const AlertComponent = () => (
+  <Alert classModifier="danger" title="Warning: information is missing" />
+);
+`})}),`
+`,r.jsx(e.h3,{id:"modifiers",children:"Modifiers"}),`
+`,r.jsxs(e.p,{children:["There are 4 modifiers available for the Alert component, ",r.jsx(e.code,{children:"error"})," modifier is the default one :"]}),`
+`,r.jsxs("table",{style:{width:"100%"},children:[r.jsx("thead",{children:r.jsx("tr",{children:r.jsx("th",{children:"Modifiers"})})}),r.jsxs("tbody",{children:[r.jsx("tr",{children:r.jsx("td",{children:"danger"})}),r.jsx("tr",{children:r.jsx("td",{children:"info"})}),r.jsx("tr",{children:r.jsx("td",{children:"error"})}),r.jsx("tr",{children:r.jsx("td",{children:"success"})})]})]}),`
+`,r.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,r.jsx(d,{of:s}),`
+`,r.jsx(l,{of:s}),`
+`,r.jsx(e.p,{children:"./Alert.stories.js./Alert.stories.js"})]})}function b(n={}){const{wrapper:e}={...i(),...n.components};return e?r.jsx(e,{...n,children:r.jsx(t,{...n})}):t(n)}export{b as default};
---
+++ slash/react/next/assets/Pass.stories-hpthCaVW.js
@@ -0,0 +1,32 @@
+import{j as l}from"./jsx-runtime-D_zvdyIk.js";import{P as r}from"./Loader-C1uJHYLx.js";/* empty css              */import"./index-DbaA8R3S.js";import"./index-BdSKqQiB.js";import{f as o}from"./index-28pPPJTf.js";const d={title:"Components/Form/Input/Pass",component:r},e={render:t=>l.jsx(r,{...t}),name:"Pass",args:{classModifier:"bad",name:"password",id:"uniqueid",type:"password",value:"",readOnly:!1,disabled:!1,className:"",autoFocus:!0,onChange:o(),onToggleType:o()},argTypes:{onChange:{action:"onChange"},onToggleType:{action:"onToggleType"},classModifier:{options:["bad","okay","good","verygood","excellent"],control:{type:"select"},defaultValue:"bad"}}};var n,a,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => <Pass {...args} />,
+  name: "Pass",
+  args: {
+    classModifier: "bad",
+    name: "password",
+    id: "uniqueid",
+    type: "password",
+    value: "",
+    readOnly: false,
+    disabled: false,
+    className: "",
+    autoFocus: true,
+    onChange: fn(),
+    onToggleType: fn()
+  },
+  argTypes: {
+    onChange: {
+      action: "onChange"
+    },
+    onToggleType: {
+      action: "onToggleType"
+    },
+    classModifier: {
+      options: ["bad", "okay", "good", "verygood", "excellent"],
+      control: {
+        type: "select"
+      },
+      defaultValue: "bad"
+    }
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const i=["PassStory"],f=Object.freeze(Object.defineProperty({__proto__:null,PassStory:e,__namedExportsOrder:i,default:d},Symbol.toStringTag,{value:"Module"}));export{f as P,e as a};
