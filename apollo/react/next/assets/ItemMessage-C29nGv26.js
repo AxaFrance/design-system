7d5ef3cd258b5449b2807fb97f0a67cfd8ea5ea5
Mohamed-EL-HANOUICHI <Mohamed-EL-HANOUICHI@users.noreply.github.com>
2025-04-25 12:12:44 +0000

    deploy: e57a47e514f7caab6f18019a097bc5dd881fb578

---
+++ apollo/react/next/assets/ItemMessage-C29nGv26.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-BJaai1kI.js";import"./index-C9mAJ3Qa.js";import{I as c,E as r,S as o}from"./ItemMessage.stories-BsWkMxjC.js";import{M as p,C as n,a}from"./index-BOVNz8Sb.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-CSJfGkOS.js";import"./iframe-DzM0tpCN.js";import"./DocsRenderer-CFRXHY34-d1uRykqH.js";import"./react-18-D2JjEoAS.js";import"./index-BsNqBstw.js";import"./CheckboxCardApollo-DfufkJOR.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function i(t){const s={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:c,name:"ItemMessage"}),`
+`,e.jsx(s.h1,{id:"itemmessage",children:"ItemMessage"}),`
+`,e.jsx(s.p,{children:"To use the spinner import it like that:"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-tsx",children:`import { ItemMessage } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <ItemMessage type="success" message="My message" />;
+`})}),`
+`,e.jsx(s.h2,{id:"type",children:"Type"}),`
+`,e.jsxs(s.p,{children:["Use the ",e.jsx(s.code,{children:"type"})," prop to change the type of the item message. The available variants are ",e.jsx(s.code,{children:"error"})," (default) or ",e.jsx(s.code,{children:"success"}),"."]}),`
+`,e.jsx(s.h2,{id:"error-message",children:"Error Message"}),`
+`,e.jsx(n,{of:r}),`
+`,e.jsx(a,{of:r}),`
+`,e.jsx(s.h2,{id:"success-message",children:"Success Message"}),`
+`,e.jsx(n,{of:o}),`
+`,e.jsx(a,{of:o})]})}function D(t={}){const{wrapper:s}={...m(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(i,{...t})}):i(t)}export{D as default};
--- apollo/react/next/assets/index-D5zO70ok.js
+++
@@ -1,1 +0,0 @@
-import"./preview-2mJujL6v.js";import"./DocsRenderer-CFRXHY34-Ce9QTlqj.js";import"./index-CHZd1MRS.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
