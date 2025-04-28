c12ea5b848e606235c85e0076a5d8cf349f0b2b9
neehal.moodhoo.accenture <neehal.moodhoo.accenture@axa.fr>
2025-04-28 07:35:32 +0200

    feat(apollo,look&feel): ajout des stories et du css du composant item file

--- client/apollo/react/src/Form/ItemFile/ItemFileApollo.tsx
+++ client/apollo/react/src/Form/ItemFile/ItemFileApollo.tsx
@@ -1,3 +1,15 @@
 import "@axa-fr/design-system-apollo-css/dist/Form/ItemFile/ItemFileApollo.scss";
+import { ItemFile as ItemFileCommon } from "./ItemFileCommon";
+import { ItemMessage } from "../ItemMessage/ItemMessageApollo";
+import { Icon } from "../../Icon/IconApollo";
+import { ClickIcon } from "../../ClickIcon/ClickIconApollo";
+import { Spinner } from "../../Spinner/SpinnerApollo";
 
-export { ItemFile } from "./ItemFileCommon";
+export const ItemFile = () => (
+  <ItemFileCommon
+    ItemMessageComponent={ItemMessage}
+    ItemIconComponent={Icon}
+    ItemClickIconComponent={ClickIcon}
+    ItemSpinnerComponent={Spinner}
+  />
+);
