c16e0c33796dae6072eb92b4d823cab295a541c6
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2025-04-29 09:41:35 +0200

    refactor(apollo): reorganize imports in alphabetic order

--- client/apollo/react/src/index.ts
+++ client/apollo/react/src/index.ts
@@ -9,14 +9,25 @@ export {
   buttonVariants,
   type ButtonVariants,
 } from "./Button/ButtonApollo";
+export {
+  CardMessage,
+  cardMessageVariants,
+  type CardMessageVariants,
+} from "./CardMessage/CardMessageApollo";
 export { ClickIcon } from "./ClickIcon/ClickIconApollo";
+export { ContentItemMono } from "./ContentItemMono/ContentItemMonoApollo";
 export { Divider } from "./Divider/DividerApollo";
+export { Checkbox } from "./Form/Checkbox/Checkbox/CheckboxApollo";
+export { CheckboxCard } from "./Form/Checkbox/CheckboxCard/CheckboxCardApollo";
+export { CheckboxText } from "./Form/Checkbox/CheckboxText/CheckboxTextApollo";
+export { DateInput } from "./Form/DateInput/DateInputApollo";
 export { ItemLabel } from "./Form/ItemLabel/ItemLabelApollo";
 export { ItemMessage } from "./Form/ItemMessage/ItemMessageApollo";
 export { Select } from "./Form/Select/SelectApollo";
 export { TextArea } from "./Form/TextArea/TextAreaApollo";
 export { TextInput } from "./Form/TextInput/TextInputApollo";
 export { DebugGrid } from "./Grid/DebugGridApollo";
+export { Heading, type HeadingLevel } from "./Heading/HeadingApollo";
 export {
   Icon,
   iconSizeVariants,
@@ -24,39 +35,27 @@ export {
   type IconSizeVariants,
   type IconVariants,
 } from "./Icon/IconApollo";
-export { Link, linkVariants, type LinkVariants } from "./Link/LinkApollo";
-export {
-  Message,
-  messageVariants,
-  type MessageVariants,
-} from "./Message/MessageApollo";
-export {
-  Spinner,
-  spinnerVariants,
-  type SpinnerVariants,
-} from "./Spinner/SpinnerApollo";
-export { Svg } from "./Svg/Svg";
-export { Tag, tagVariants, type TagVariants } from "./Tag/TagApollo";
-export { Toggle } from "./Toggle/ToggleApollo";
-
-export {
-  CardMessage,
-  cardMessageVariants,
-  type CardMessageVariants,
-} from "./CardMessage/CardMessageApollo";
-export { ContentItemMono } from "./ContentItemMono/ContentItemMonoApollo";
-export { Checkbox } from "./Form/Checkbox/Checkbox/CheckboxApollo";
-export { CheckboxCard } from "./Form/Checkbox/CheckboxCard/CheckboxCardApollo";
-export { CheckboxText } from "./Form/Checkbox/CheckboxText/CheckboxTextApollo";
-export { DateInput } from "./Form/DateInput/DateInputApollo";
-export { Heading, type HeadingLevel } from "./Heading/HeadingApollo";
 export {
   ItemTabBar,
   itemTabBarVariants,
   type ItemTabBarVariants,
 } from "./ItemTabBar/ItemTabBarApollo";
+export { Link, linkVariants, type LinkVariants } from "./Link/LinkApollo";
 export { ContentItemDuo } from "./List/ContentItemDuo/ContentItemDuoApollo";
+export {
+  Message,
+  messageVariants,
+  type MessageVariants,
+} from "./Message/MessageApollo";
 export { ProgressBar } from "./ProgressBar/ProgressBarApollo";
 export { ProgressBarGroup } from "./ProgressBarGroup/ProgressBarGroupApollo";
+export {
+  Spinner,
+  spinnerVariants,
+  type SpinnerVariants,
+} from "./Spinner/SpinnerApollo";
 export { Stepper } from "./Stepper/StepperApollo";
+export { Svg } from "./Svg/Svg";
+export { Tag, tagVariants, type TagVariants } from "./Tag/TagApollo";
 export { TimelineVertical } from "./TimelineVertical/TimelineVerticalApollo";
+export { Toggle } from "./Toggle/ToggleApollo";
--- client/apollo/react/src/indexLF.ts
+++ client/apollo/react/src/indexLF.ts
@@ -4,14 +4,25 @@ import "@fontsource/source-sans-pro";
 
 export { BasePicture } from "./BasePicture/BasePictureLF";
 export { Button, buttonVariants, type ButtonVariants } from "./Button/ButtonLF";
+export {
+  CardMessage,
+  cardMessageVariants,
+  type CardMessageVariants,
+} from "./CardMessage/CardMessageLF";
 export { ClickIcon } from "./ClickIcon/ClickIconLF";
+export { ContentItemMono } from "./ContentItemMono/ContentItemMonoLF";
 export { Divider } from "./Divider/DividerLF";
+export { Checkbox } from "./Form/Checkbox/Checkbox/CheckboxLf";
+export { CheckboxCard } from "./Form/Checkbox/CheckboxCard/CheckboxCardLF";
+export { CheckboxText } from "./Form/Checkbox/CheckboxText/CheckboxTextLF";
+export { DateInput } from "./Form/DateInput/DateInputLF";
 export { ItemLabel } from "./Form/ItemLabel/ItemLabelLF";
 export { ItemMessage } from "./Form/ItemMessage/ItemMessageLF";
 export { Select } from "./Form/Select/SelectLF";
 export { TextArea } from "./Form/TextArea/TextAreaLF";
 export { TextInput } from "./Form/TextInput/TextInputLF";
 export { DebugGrid } from "./Grid/DebugGridLF";
+export { Heading, type HeadingLevel } from "./Heading/HeadingLF";
 export {
   Icon,
   iconSizeVariants,
@@ -19,39 +30,27 @@ export {
   type IconSizeVariants,
   type IconVariants,
 } from "./Icon/IconLF";
-export { Link, linkVariants, type LinkVariants } from "./Link/LinkLF";
-export {
-  Message,
-  messageVariants,
-  type MessageVariants,
-} from "./Message/MessageLF";
-export {
-  Spinner,
-  spinnerVariants,
-  type SpinnerVariants,
-} from "./Spinner/SpinnerLF";
-export { Svg } from "./Svg/Svg";
-export { Tag, tagVariants, type TagVariants } from "./Tag/TagLF";
-export { Toggle } from "./Toggle/ToggleLF";
-
-export {
-  CardMessage,
-  cardMessageVariants,
-  type CardMessageVariants,
-} from "./CardMessage/CardMessageLF";
-export { ContentItemMono } from "./ContentItemMono/ContentItemMonoLF";
-export { Checkbox } from "./Form/Checkbox/Checkbox/CheckboxLf";
-export { CheckboxCard } from "./Form/Checkbox/CheckboxCard/CheckboxCardLF";
-export { CheckboxText } from "./Form/Checkbox/CheckboxText/CheckboxTextLF";
-export { DateInput } from "./Form/DateInput/DateInputLF";
-export { Heading, type HeadingLevel } from "./Heading/HeadingLF";
 export {
   ItemTabBar,
   itemTabBarVariants,
   type ItemTabBarVariants,
 } from "./ItemTabBar/ItemTabBarLF";
+export { Link, linkVariants, type LinkVariants } from "./Link/LinkLF";
 export { ContentItemDuo } from "./List/ContentItemDuo/ContentItemDuoLF";
+export {
+  Message,
+  messageVariants,
+  type MessageVariants,
+} from "./Message/MessageLF";
 export { ProgressBar } from "./ProgressBar/ProgressBarLF";
 export { ProgressBarGroup } from "./ProgressBarGroup/ProgressBarGroupLF";
+export {
+  Spinner,
+  spinnerVariants,
+  type SpinnerVariants,
+} from "./Spinner/SpinnerLF";
 export { Stepper } from "./Stepper/StepperLF";
+export { Svg } from "./Svg/Svg";
+export { Tag, tagVariants, type TagVariants } from "./Tag/TagLF";
 export { TimelineVertical } from "./TimelineVertical/TimelineVerticalLF";
+export { Toggle } from "./Toggle/ToggleLF";
