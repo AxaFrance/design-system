a9b793ddab7fa38809692bd799353a8c0fdccfd7
buddyvegas <buddyvegas@users.noreply.github.com>
2025-04-30 15:12:16 +0000

    deploy: 6fe21b2f537d150bccbce416504acbfa7bbea671

---
+++ look-and-feel/react/next/assets/Grid-BoGLoDGY.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-BJaai1kI.js";import{M as f,C as i,a as m}from"./index-D16hNNGg.js";import{Form as e}from"./GridExampleForm.stories-zjVf1oin.js";import{Page as n}from"./GridExamplePage.stories-CC_qdd6g.js";import{G as x,W as p}from"./GridWireframe.stories-DeHMNNa7.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CuuMGkgT.js";import"./index-BsNqBstw.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";/* empty css                      */import"./CheckboxTextLF-m184Kumh.js";import"./constants-q-WgEg4M.js";import"./DebugGridLF-CO1HolH-.js";import"./Title-BWYlPnUY.js";import"./IconBg-faKAVS83.js";import"./home-BuLaEOyh.js";import"./MyHeader-M1MeGY-q.js";import"./Footer-C4kcxL4g.js";import"./useIsSmallScreen-B3lIGOTq.js";import"./person-fill-BFXmhM6H.js";import"./logo-axa-Cqr9fAkX.js";import"./NavBar-C4GZIVhf.js";import"./PreviousLink-DlGAz0zO.js";import"./arrow_back-DnyUknY1.js";import"./ClickItem-BG0qJfgD.js";import"./ClickItem.helpers-CDlaBFDl.js";import"./List-BZPurVeP.js";import"./Card-BTualPmp.js";import"./ContentItemMono-DljrFYox.js";import"./call-fill-Nx5D3UHq.js";import"./article-fill-B_-J39Ry.js";import"./ContentItemDuoLF-CUbEItIm.js";function s(t){const o={a:"a",h2:"h2",p:"p",...a(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(f,{of:x}),`
+`,r.jsx(o.h2,{id:"grid-system",children:"Grid System"}),`
+`,r.jsxs(o.p,{children:["More info here : ",r.jsx(o.a,{href:"https://axafrance.github.io/design-system/apollo/react/latest?path=/docs/guidelines-grid-system--grid",rel:"nofollow",children:"Grid documentation"})]}),`
+`,r.jsx(o.h2,{id:"wireframe",children:"Wireframe"}),`
+`,r.jsx(i,{of:p}),`
+`,r.jsx(m,{of:p}),`
+`,r.jsx(o.h2,{id:"example-page",children:"Example page"}),`
+`,r.jsx(i,{of:n}),`
+`,r.jsx(m,{of:n}),`
+`,r.jsx(o.h2,{id:"example-form",children:"Example form"}),`
+`,r.jsx(i,{of:e}),`
+`,r.jsx(m,{of:e})]})}function Q(t={}){const{wrapper:o}={...a(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(s,{...t})}):s(t)}export{Q as default};
---
+++ look-and-feel/react/next/assets/index-Biv_hKJK.js
@@ -0,0 +1,1 @@
+import"./preview-MiPpbrVy.js";import"./DocsRenderer-CFRXHY34-z985T0yi.js";import"./index-D16hNNGg.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
