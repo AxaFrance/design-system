1929a847411cd53a72955c636ab59ea3920ff475
drtapha <drtapha@users.noreply.github.com>
2025-05-06 12:44:15 +0000

    deploy: bb60ed51c718612e02f067d65b078aa15ae7c480

---
+++ apollo/react/next/assets/index-BArtnYu4.js
@@ -0,0 +1,1 @@
+import"./preview-BA2YnF_m.js";import"./DocsRenderer-CFRXHY34-B0WpLJpo.js";import"./index-DhYO-Jbl.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
--- apollo/react/next/assets/index-GBRIOK7R.js
+++
@@ -1,1 +0,0 @@
-import"./preview-BGrxtYl-.js";import"./DocsRenderer-CFRXHY34-uto_jFRP.js";import"./index-DsNUDS2P.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
