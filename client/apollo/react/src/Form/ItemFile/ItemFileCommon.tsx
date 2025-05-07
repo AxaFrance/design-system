b0e10bc35d0bfa64ea8127384053fe6bce91ad01
neehal.moodhoo.accenture <neehal.moodhoo.accenture@axa.fr>
2025-05-07 09:31:52 +0200

    feat(apollo,look&feel): ajout des stories

--- client/apollo/react/src/Form/ItemFile/ItemFileCommon.tsx
+++ client/apollo/react/src/Form/ItemFile/ItemFileCommon.tsx
@@ -2,13 +2,12 @@ import validationIcon from "@material-symbols/svg-400/outlined/check_circle-fill
 import errorIcon from "@material-symbols/svg-400/outlined/error-fill.svg";
 import deleteIcon from "@material-symbols/svg-400/outlined/delete.svg";
 import visibilityIcon from "@material-symbols/svg-400/outlined/visibility-fill.svg";
+
 import {
   type ComponentProps,
   type ComponentPropsWithoutRef,
   type ComponentType,
-  type PropsWithChildren,
   useId,
-  useMemo,
 } from "react";
 
 import { ItemMessage } from "../ItemMessage/ItemMessageCommon";
@@ -16,76 +15,67 @@ import { Spinner, spinnerVariants } from "../../Spinner/SpinnerCommon";
 import { ClickIcon } from "../../ClickIcon/ClickIconCommon";
 import { Icon } from "../../Icon/IconCommon";
 
-export const itemFileVariants = {
+export const itemFileStates = {
   success: "success",
   error: "error",
   loading: "loading",
 } as const;
 
-type ItemFileVariants = keyof typeof itemFileVariants;
+type ItemFileState = keyof typeof itemFileStates;
 
 type ItemFileProps = {
-  success: boolean;
-  error: string;
-  loading: boolean;
+  state: ItemFileState;
+  title: string;
+  subTitle: string;
+  errorMessage?: string;
   ItemMessageComponent: ComponentType<ComponentProps<typeof ItemMessage>>;
   ItemIconComponent: ComponentType<ComponentProps<typeof Icon>>;
   ItemClickIconComponent: ComponentType<ComponentProps<typeof ClickIcon>>;
   ItemSpinnerComponent: ComponentType<ComponentProps<typeof Spinner>>;
-  variant?: ItemFileVariants;
-  title: string;
-  subTitle: string;
 } & ComponentPropsWithoutRef<"div">;
 
-const getIconFromType = (variant: ItemFileVariants) =>
-  ({
-    [itemFileVariants.success]: validationIcon,
-    [itemFileVariants.error]: errorIcon,
-    [itemFileVariants.loading]: errorIcon,
-  })[variant];
-
 export const ItemFile = ({
-  variant = itemFileVariants.success,
-  className,
-  success,
-  error,
-  loading,
+  state = "success",
   title,
   subTitle,
+  errorMessage,
   ItemSpinnerComponent,
   ItemClickIconComponent,
   ItemIconComponent,
   ItemMessageComponent,
-}: PropsWithChildren<ItemFileProps>) => {
-  const icon = useMemo(() => getIconFromType(variant), [variant]);
+}: ItemFileProps) => {
   const idError = useId();
 
+  const getIconByState = () => {
+    if (state === "loading") {
+      return <ItemSpinnerComponent size={24} variant={spinnerVariants.blue} />;
+    }
+
+    const iconSrc = state === "success" ? validationIcon : errorIcon;
+
+    return (
+      <ItemIconComponent
+        variant="primary"
+        className="item-file-icon"
+        size="S"
+        src={iconSrc}
+      />
+    );
+  };
+
   return (
-    <div
-      className={["af-item-file", `af-item-file--${variant}`, className]
-        .filter(Boolean)
-        .join(" ")}
-      role="alert"
-    >
+    <div className="af-item-file" role="alert">
       <div className="af-icon">
-        {loading ? (
-          <ItemIconComponent
-            variant="primary"
-            className="item-file-icon"
-            size="S"
-            src={icon}
-          />
-        ) : (
-          <ItemSpinnerComponent size={24} variant={spinnerVariants.blue} />
-        )}
+        {getIconByState()}
+
         <div>
-          <div className="af-item-file-title">{title} IMG_879687880.jpg</div>
-          <div className="af-item-file-subtitle">{subTitle} 0.12 Mo</div>
+          <div className="af-item-file-title">{title}</div>
+          <div className="af-item-file-subtitle">{subTitle}</div>
         </div>
       </div>
 
       <div className="af-click-icon__content">
-        {success ? (
+        {state === "success" ? (
           <>
             <ItemClickIconComponent src={visibilityIcon} />
             <ItemClickIconComponent src={deleteIcon} />
@@ -94,7 +84,10 @@ export const ItemFile = ({
           <ItemClickIconComponent src={deleteIcon} />
         )}
       </div>
-      <ItemMessageComponent id={idError} message={error} />
+
+      {state === "error" && (
+        <ItemMessageComponent id={idError} message={errorMessage} />
+      )}
     </div>
   );
 };
