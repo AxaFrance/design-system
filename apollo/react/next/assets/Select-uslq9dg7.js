21eb842a2d83875202dd4c38f3f501328fd8d4de
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2025-05-12 06:46:31 +0000

    deploy: d1cb5bceb698014fb6c920b7ae9c2816665c80a0

---
+++ apollo/react/next/assets/Select-uslq9dg7.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-BJaai1kI.js";import"./index-Ry9IlVBB.js";import{S as d,a as s,b as i,c}from"./Select.stories-DoeWf7D6.js";import{M as p,C as r,a as n}from"./index-CcrodAmO.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-9yx6YNRc.js";import"./iframe-BSge8Y0t.js";import"./DocsRenderer-CFRXHY34-BxS0f_3f.js";import"./react-18-D2JjEoAS.js";import"./index-BsNqBstw.js";import"./ToggleCommon-BLluYP1P.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function l(o){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...a(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:d,name:"Select"}),`
+`,e.jsx(t.h1,{id:"select",children:"Select"}),`
+`,e.jsx(t.p,{children:"To use Select import it like this:"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`import { Select } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <Select label="label">
+`})}),`
+`,e.jsx(t.h2,{id:"type",children:"Type"}),`
+`,e.jsxs(t.p,{children:["Use the ",e.jsx(t.code,{children:"type"})," prop to change the type of the select. The available variants are ",e.jsx(t.code,{children:"default"}),", ",e.jsx(t.code,{children:"error"})," and ",e.jsx(t.code,{children:"disabled"}),"."]}),`
+`,e.jsx(t.h2,{id:"select-1",children:"Select"}),`
+`,e.jsx(r,{of:s}),`
+`,e.jsx(n,{of:s}),`
+`,e.jsx(t.h2,{id:"error-message",children:"Error Message"}),`
+`,e.jsx(r,{of:i}),`
+`,e.jsx(n,{of:i}),`
+`,e.jsx(t.h2,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(n,{of:c})]})}function T(o={}){const{wrapper:t}={...a(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(l,{...o})}):l(o)}export{T as default};
--- apollo/react/next/assets/index-psKzPOsT.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CUxDRJu-.js";import"./DocsRenderer-CFRXHY34-CNWipD7b.js";import"./index-C2l_NnyC.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
