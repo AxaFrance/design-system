7ce40d553a41bd7ade229f56644e66a1b8f80b78
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-05-16 12:47:40 +0000

    deploy: 939f5273abf3d89350c00e513cb4c154d9ce452a

---
+++ apollo/react/next/assets/CheckboxText.stories-BPeItaq6.js
@@ -0,0 +1,41 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{b as n}from"./ToggleCommon-C-f3rYGm.js";import"./index-DuZrfDUH.js";const c={title:"Components/Form/Checkbox/CheckboxText",component:n,argTypes:{}},e={name:"CheckboxText",render:({...a})=>s.jsx(n,{...a}),args:{label:"J'accepte de fournir à AXA mes coordonnées ainsi que les données  relatives à mon projet et ma situation. Ces dernières seront transmises à mon conseiller AXA qui pourra  me contacter pour m'accompagner.",name:"option1",value:"option1"},argTypes:{label:{control:{type:"text"}},name:{control:{type:"text"}},value:{control:{type:"text"}},errorMessage:{control:{type:"text"}},checked:{control:{type:"boolean"}},onChange:{action:"onChange"}}};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: "CheckboxText",
+  render: ({
+    ...args
+  }) => <CheckboxText {...args} />,
+  args: {
+    label: "J'accepte de fournir à AXA mes coordonnées ainsi que les données  relatives à mon projet et ma situation. Ces dernières seront transmises à mon conseiller AXA qui pourra  me contacter pour m'accompagner.",
+    name: "option1",
+    value: "option1"
+  },
+  argTypes: {
+    label: {
+      control: {
+        type: "text"
+      }
+    },
+    name: {
+      control: {
+        type: "text"
+      }
+    },
+    value: {
+      control: {
+        type: "text"
+      }
+    },
+    errorMessage: {
+      control: {
+        type: "text"
+      }
+    },
+    checked: {
+      control: {
+        type: "boolean"
+      }
+    },
+    onChange: {
+      action: "onChange"
+    }
+  }
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const p=["CheckboxTextStory"],x=Object.freeze(Object.defineProperty({__proto__:null,CheckboxTextStory:e,__namedExportsOrder:p,default:c},Symbol.toStringTag,{value:"Module"}));export{x as C,e as a};
--- apollo/react/next/assets/index-var-ZzQh.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Dbhyn2sy.js";import"./DocsRenderer-CFRXHY34-DZ7NMSjF.js";import"./index-D71-mJ7t.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
