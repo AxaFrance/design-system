9dbf5140d34266550458654158f0038e589f6199
Paul PLANCQ <paul.plancq@outlook.fr>
2025-06-11 16:21:16 +0200

    feat(apollo,look&feel): hide debug grid in production environment

--- apps/apollo-stories/src/fondations/Grid/DebugGrid.stories.tsx
+++ apps/apollo-stories/src/fondations/Grid/DebugGrid.stories.tsx
@@ -12,7 +12,7 @@ export default meta;
 export const Default: StoryObj = {
   render: () => (
     <div style={{ height: "100svh", display: "block" }}>
-      <DebugGrid isCheckedByDefault />
+      <DebugGrid<{ forceVisible?: boolean }> isCheckedByDefaul forceVisible />
     </div>
   ),
 };
