195ef15917a77286cfa09dd5f9406afff255b98e
Paul PLANCQ <paul.plancq@outlook.fr>
2025-06-11 16:21:16 +0200

    feat(apollo,look&feel): hide debug grid in production environment

--- client/apollo/react/src/Grid/DebugGridApollo.tsx
+++ client/apollo/react/src/Grid/DebugGridApollo.tsx
@@ -1,18 +1,29 @@
-import "@axa-fr/design-system-apollo-css/dist/Grid/DebugGrid.scss";
 import { useState } from "react";
 import { CardCheckbox } from "../Form/Checkbox/CardCheckbox/CardCheckboxApollo";
 import { DebugGridCommon } from "./DebugGridCommon";
 
-export const DebugGrid = ({
-  cols = 12,
-  isCheckedByDefault = false,
-}: {
+import "@axa-fr/design-system-apollo-css/dist/Grid/DebugGrid.scss";
+
+type DebugGridProps<P = object> = P & {
   cols?: number;
   isCheckedByDefault?: boolean;
-}) => {
+};
+
+export const DebugGrid = <P = object,>({
+  cols = 12,
+  isCheckedByDefault = false,
+  ...props
+}: DebugGridProps<P>) => {
   const [checked, setChecked] = useState(isCheckedByDefault);
 
   const handleChecked = () => setChecked(!checked);
+
+  const forceVisible =
+    (props as { forceVisible?: boolean })?.forceVisible || false;
+  if (process.env.NODE_ENV === "production" && !forceVisible) {
+    return null;
+  }
+
   return (
     <>
       <CardCheckbox
