68199c93e025a5daaa86f957ff91528c0aa51171
Abderrahman FAWZY DEV <S878630@azure-paas.com>
2025-06-12 09:29:08 +0200

    revert "feat(look&feel): suppression du code du composant modal et réexportation d'apollo"
    This reverts commit e5280d211d37cf3d28ec36524a7716532c288ad6.

---
+++ client/look-and-feel/react/src/Modal/components/ModalCoreHeader.tsx
@@ -0,0 +1,62 @@
+import closeSmall from "@material-symbols/svg-400/outlined/close.svg";
+import type { HTMLAttributes, JSX, MouseEvent, KeyboardEvent } from "react";
+import { Button } from "../../Button/Button";
+import { Svg } from "../../Svg";
+import type { TitleLevel } from "../../Title";
+
+export type ModalCoreHeaderProps = HTMLAttributes<HTMLDivElement> & {
+  className?: string;
+  title: string;
+  subtitle?: string;
+  iconTitle?: string;
+  levelTitle?: TitleLevel;
+  onCancel: (event: MouseEvent | KeyboardEvent) => void;
+  closeButtonAriaLabel?: string;
+};
+
+export const ModalCoreHeader = ({
+  className,
+  title,
+  subtitle,
+  iconTitle,
+  levelTitle = 2,
+  onCancel,
+  closeButtonAriaLabel = "Fermer la boite de dialogue",
+  ...props
+}: ModalCoreHeaderProps) => {
+  const HLevel = `h${levelTitle}` as keyof JSX.IntrinsicElements;
+  return (
+    <header
+      className={["af-modal__header", className].filter(Boolean).join(" ")}
+      {...props}
+    >
+      <Button
+        variant="ghost"
+        iconLeft={
+          <Svg
+            src={closeSmall}
+            width={32}
+            height={32}
+            role="graphics-document"
+          />
+        }
+        aria-label={closeButtonAriaLabel}
+        onClick={onCancel}
+      />
+      <div className="af-modal__header-title">
+        {iconTitle && (
+          <Svg
+            src={iconTitle}
+            width={32}
+            height={32}
+            role="graphics-document"
+          />
+        )}
+        <HLevel className="af-modal__header-title-heading">{title}</HLevel>
+        {subtitle && (
+          <span className="af-modal__header-title-subtitle">{subtitle}</span>
+        )}
+      </div>
+    </header>
+  );
+};
