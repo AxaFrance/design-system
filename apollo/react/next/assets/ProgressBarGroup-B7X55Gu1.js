9c5197e561df30fd040cc896322edfb2469784a2
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-06-16 12:25:55 +0000

    deploy: 250a60484832915a18bec0fa456c6d7d26937ade

---
+++ apollo/react/next/assets/ProgressBarGroup-B7X55Gu1.js
@@ -0,0 +1,10 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-BJaai1kI.js";import"./index-B9fM6Jdh.js";import{P as a,a as e}from"./ProgressBarGroup.stories-vUEInBif.js";import{M as i,C as m,a as p}from"./index-Bh7z8ODD.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-B2nBuLE0.js";import"./iframe-DSks1T2x.js";import"./DocsRenderer-CFRXHY34-CA4uBIzj.js";import"./react-18-D2JjEoAS.js";import"./index-BsNqBstw.js";import"./ToggleCommon-C0WSr7hZ.js";import"./ProgressBarGroupApollo-BU7hQ_iL.js";import"./ProgressBarCommon-BgCE42rb.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function n(t){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(i,{of:a,name:"ProgressBar Client"}),`
+`,r.jsx(o.h1,{id:"progressbar",children:"ProgressBar"}),`
+`,r.jsx(o.p,{children:"To use the header import it like that:"}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-tsx",children:`import { ProgressBarGroup } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <ProgressBarGroup />;
+`})}),`
+`,r.jsx(o.h2,{id:"playground",children:"Playground"}),`
+`,r.jsx(m,{of:e}),`
+`,r.jsx(p,{of:e})]})}function _(t={}){const{wrapper:o}={...s(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(n,{...t})}):n(t)}export{_ as default};
