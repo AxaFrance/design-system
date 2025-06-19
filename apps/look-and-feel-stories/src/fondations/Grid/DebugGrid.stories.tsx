64a383f3585124a834264aa374fab55483716370
Paul PLANCQ <paul.plancq@outlook.fr>
2025-06-19 11:41:12 +0200

    refactor(apollo,look&feel): streamline DebugGrid components by consolidating props

--- apps/look-and-feel-stories/src/fondations/Grid/DebugGrid.stories.tsx
+++ apps/look-and-feel-stories/src/fondations/Grid/DebugGrid.stories.tsx
@@ -12,7 +12,7 @@ export default meta;
 export const Default: StoryObj = {
   render: () => (
     <div style={{ height: "100svh", display: "block" }}>
-      <DebugGrid<{ forceVisible?: boolean }> isCheckedByDefaul forceVisible />
+      <DebugGrid<{ forceVisible?: boolean }> isCheckedByDefault forceVisible />
     </div>
   ),
 };
