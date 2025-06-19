1dc5745045dc36d3e8d7d63cad34cf27f3838672
Paul PLANCQ <paul.plancq@outlook.fr>
2025-06-19 11:41:12 +0200

    refactor(apollo,look&feel): streamline DebugGrid components by consolidating props

--- client/apollo/react/src/Grid/DebugGridCommon.tsx
+++ client/apollo/react/src/Grid/DebugGridCommon.tsx
@@ -1,13 +1,48 @@
 import "@axa-fr/design-system-apollo-css/dist/Grid/DebugGrid.scss";
+import type { ComponentProps, ComponentType } from "react";
+import type { CardCheckboxCommon } from "../Form/Checkbox/CardCheckbox/CardCheckboxCommon";
 
-export const DebugGridCommon = ({ cols = 12 }: { cols?: number }) => {
+export type DebugGridCommonProps<P = object> = P & {
+  cols?: number;
+  isCheckedByDefault?: boolean;
+  CardCheckbox: ComponentType<
+    Partial<ComponentProps<typeof CardCheckboxCommon>>
+  >;
+};
+
+export const DebugGridCommon = <P = object,>({
+  cols = 12,
+  isCheckedByDefault = false,
+  CardCheckbox,
+  ...props
+}: DebugGridCommonProps<P>) => {
+  // The forceVisible prop is a hidden prop to force the component's visibility when building the storybook
+  const forceVisible =
+    (props as { forceVisible?: boolean })?.forceVisible || false;
+  if (process.env.NODE_ENV === "production" && !forceVisible) {
+    return null;
+  }
+
   return (
-    <div className="debug-grid">
-      <div className="grid">
-        {[...Array(cols).keys()].map((col: number) => (
-          <div key={col} className="cols" />
-        ))}
+    <>
+      <CardCheckbox
+        type="horizontal"
+        options={[
+          {
+            name: "debuggrid",
+            label: "Grid",
+            defaultChecked: isCheckedByDefault,
+          },
+        ]}
+      />
+
+      <div className="debug-grid">
+        <div className="grid">
+          {[...Array(cols).keys()].map((col: number) => (
+            <div key={col} className="cols" />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
