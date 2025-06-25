cc9ecbf60f530664b961085daae3d06d9e3cd91b
FROGER Baptiste <b405nk@login.axa>
2025-06-25 11:42:12 +0200

    feat(apollo,look&feel): pull request suggested changes - add storybook cases

--- apps/look-and-feel-stories/src/InputPhone/InputPhone.stories.tsx
+++ apps/look-and-feel-stories/src/InputPhone/InputPhone.stories.tsx
@@ -35,7 +35,7 @@ export default meta;
 type Story = StoryObj<typeof InputPhone>;
 
 export const InputPhonePlaceholderStory: Story = {
-  name: "Text placeholder",
+  name: "InputPhone without select",
   render: ({ onChange, ...args }) => {
     const [value, setValue] = useState("");
     return (
@@ -46,5 +46,23 @@ export const InputPhonePlaceholderStory: Story = {
         {...args}
       />
     );
+  },
+};
+
+export const InputPhoneWithSelectStory: Story = {
+  name: "InputPhone with select",
+  render: ({ onChange, ...args }) => {
+    const [value, setValue] = useState("");
+    return (
+      <InputPhone
+        onChange={onChange}
+        value={value}
+        onChangeInput={setValue}
+        {...args}
+      />
+    );
+  },
+  args: {
+    showSelect: true,
   },
 };
