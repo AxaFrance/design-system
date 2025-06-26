72ef26d8e6c37b942a2743be8c97a1a6fc17404e
pplancq <pplancq@users.noreply.github.com>
2025-06-26 07:42:14 +0000

    deploy: b835b1cd8565cc6ad2382ecab7acac0123681c2e

---
+++ look-and-feel/react/next/assets/Card-CZef0Pin.js
@@ -0,0 +1,21 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-BJaai1kI.js";import{M as c,C as o,a as t}from"./index-BnZAGJRV.js";import{C as m,D as a,B as s}from"./Card.stories-KaXmlCo9.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Dsnx-yqb.js";import"./index-BsNqBstw.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./DropdownCommon-D-mREVsa.js";import"./constants-DXq0Q4w3.js";import"./Card-CStnVMIG.js";import"./getComponentClassName-HlajKSGI.js";function d(e){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...i(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(c,{of:m,name:"Card"}),`
+`,n.jsx(r.h1,{id:"card",children:"Card"}),`
+`,n.jsx(r.h3,{id:"import",children:"Import"}),`
+`,n.jsx(r.pre,{children:n.jsx(r.code,{className:"language-tsx",children:`import { Card } from "@axa-fr/design-system-look-and-feel-react";
+`})}),`
+`,n.jsx(r.h3,{id:"use",children:"Use"}),`
+`,n.jsx(r.pre,{children:n.jsx(r.code,{className:"language-tsx",children:`export const CardComponent = () => (
+  <Card>
+    <h1 className="af-card__title">My card title</h1>
+    <p>My card content</p>
+  </Card>
+);
+`})}),`
+`,n.jsx(r.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(r.h3,{id:"default-card",children:"Default Card"}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(t,{of:a}),`
+`,n.jsx(r.h3,{id:"button-card",children:"Button Card"}),`
+`,n.jsxs(r.p,{children:["To enable Card as a button you just need to add the ",n.jsx(r.code,{children:"onClick"})," prop to the ",n.jsx(r.code,{children:"Card"})," component."]}),`
+`,n.jsx(o,{of:s}),`
+`,n.jsx(t,{of:s})]})}function B(e={}){const{wrapper:r}={...i(),...e.components};return r?n.jsx(r,{...e,children:n.jsx(d,{...e})}):d(e)}export{B as default};
---
+++ look-and-feel/react/next/assets/Pagination-CAtkZU8G.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-BJaai1kI.js";import"./index-JACO0c2a.js";import{P as i,a as e}from"./Pagination.stories-B64GRRjA.js";import{M as m,C as s,a as p}from"./index-BnZAGJRV.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-CJM8XDk1.js";import"./iframe-Dsnx-yqb.js";import"./DocsRenderer-CFRXHY34-Bc7UMMye.js";import"./react-18-D2JjEoAS.js";import"./index-BsNqBstw.js";import"./DropdownCommon-D-mREVsa.js";import"./constants-DXq0Q4w3.js";import"./useIsSmallScreen-B3lIGOTq.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function a(t){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:i,name:"Pagination"}),`
+`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
+`,n.jsx(o.p,{children:"To use the Pagination import it like that:"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-tsx",children:`import { Pagination } from "@axa-fr/design-system-look-and-feel-react";
+
+const MyComponent = () => (
+  <Pagination
+    numberPage={numberPage}
+    onChangePage={onChangePage}
+    defaultPage={defaultPage}
+  />
+);
+`})}),`
+`,n.jsx(o.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(s,{of:e}),`
+`,n.jsx(p,{of:e})]})}function _(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(a,{...t})}):a(t)}export{_ as default};
