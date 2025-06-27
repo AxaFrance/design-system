7d8911ab51e7cd21fe88b19ed0b81a200e5c156b
fffan64 <fffan64@users.noreply.github.com>
2025-06-27 07:04:11 +0000

    deploy: 05d70e1c36bc24a85a37ca9f8432a1bbaa6c3b90

---
+++ look-and-feel/react/next/assets/Dropdown-xSsfhkTB.js
@@ -0,0 +1,18 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-BJaai1kI.js";import"./index-DD_jrysS.js";import{D as c,a as t,b as d,c as i}from"./Dropdown.stories-Beu0mU9d.js";import{M as l,C as n,a as s}from"./index-DO3qTVNF.js";import"./index-DuZrfDUH.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-CYaa2m13.js";import"./iframe-BFRRH8au.js";import"./DocsRenderer-CFRXHY34-C28wBgHU.js";import"./react-18-D2JjEoAS.js";import"./index-BsNqBstw.js";import"./DropdownCommon-aggMtOTW.js";import"./constants-1ZfGuOtP.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function p(e){const r={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...a(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(l,{of:c,name:"Dropdown"}),`
+`,o.jsx(r.h1,{id:"dropdown",children:"Dropdown"}),`
+`,o.jsx(r.p,{children:"To use Dropdown import it like this:"}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-tsx",children:`import { Dropdown } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <Dropdown label="label">
+`})}),`
+`,o.jsx(r.h2,{id:"type",children:"Type"}),`
+`,o.jsxs(r.p,{children:["Use the ",o.jsx(r.code,{children:"type"})," prop to change the type of the select. The available variants are ",o.jsx(r.code,{children:"default"}),", ",o.jsx(r.code,{children:"error"})," and ",o.jsx(r.code,{children:"disabled"}),"."]}),`
+`,o.jsx(r.h2,{id:"dropdown-1",children:"Dropdown"}),`
+`,o.jsx(n,{of:t}),`
+`,o.jsx(s,{of:t}),`
+`,o.jsx(r.h2,{id:"error-message",children:"Error Message"}),`
+`,o.jsx(n,{of:d}),`
+`,o.jsx(s,{of:d}),`
+`,o.jsx(r.h2,{id:"disabled",children:"Disabled"}),`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(s,{of:i})]})}function T(e={}){const{wrapper:r}={...a(),...e.components};return r?o.jsx(r,{...e,children:o.jsx(p,{...e})}):p(e)}export{T as default};
--- look-and-feel/react/next/assets/index-JACO0c2a.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CJM8XDk1.js";import"./DocsRenderer-CFRXHY34-Bc7UMMye.js";import"./index-BnZAGJRV.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
