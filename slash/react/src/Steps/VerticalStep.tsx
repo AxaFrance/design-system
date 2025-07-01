44dfc2dba1d27a70e9f1abb9086135d1da0deb25
Martin Stievenart <stievenart.m@gmail.com>
2025-07-01 10:42:17 +0200

    feat(slash): add VerticalStep component

---
+++ slash/react/src/Steps/VerticalStep.tsx
@@ -0,0 +1,92 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
+import edit from "@material-symbols/svg-400/sharp/edit-fill.svg";
+import check from "@material-symbols/svg-400/sharp/check.svg";
+import lock from "@material-symbols/svg-400/sharp/lock-fill.svg";
+import { Title } from "../Title/Title";
+import { Svg } from "../Svg";
+import type { VerticalStepMode } from "./types";
+
+import "@axa-fr/design-system-slash-css/dist/Steps/VerticalStep.css";
+import { Button } from "../Button/Button";
+
+type Props = {
+  title: string;
+  id: string;
+  stepMode: VerticalStepMode;
+  onEdit: React.MouseEventHandler<HTMLButtonElement>;
+  form: ReactNode;
+  restitution: ReactNode;
+  editButtonLabel?: string;
+  editButtonAriaLabel?: string;
+  showRestitution?: boolean;
+};
+
+const defaultClassName = "af-vertical-step";
+
+export const VerticalStep = ({
+  title,
+  id,
+  stepMode,
+  editButtonLabel = "Modifier",
+  editButtonAriaLabel = `Modifier l'étape ${title}`,
+  onEdit,
+  form,
+  restitution,
+  showRestitution = true,
+}: Props) => {
+  const isStepInEdition = stepMode === "edited";
+  const isStepValidated = stepMode === "validated";
+  const isStepLocked = stepMode === "locked";
+
+  return (
+    <div
+      className={classNames(defaultClassName, {
+        [`${defaultClassName}--edition`]: isStepInEdition,
+      })}
+    >
+      <div
+        className={classNames("af-vertical-step-icon", {
+          [`${defaultClassName}-icon--validated`]: isStepValidated,
+          [`${defaultClassName}-icon--locked`]: isStepLocked,
+          [`${defaultClassName}-icon--edited`]: isStepInEdition,
+        })}
+      >
+        {isStepValidated ? <Svg role="presentation" src={check} /> : null}
+        {isStepLocked ? <Svg role="presentation" src={lock} /> : null}
+        {isStepInEdition ? <Svg role="presentation" src={edit} /> : null}
+      </div>
+      <Title
+        className={classNames("af-title", `${defaultClassName}-title`)}
+        id={id}
+      >
+        {title}
+        {isStepValidated ? (
+          <Button
+            aria-label={editButtonAriaLabel}
+            onClick={onEdit}
+            className="af-vertical-step-title-button"
+          >
+            <Svg role="presentation" src={edit} />
+            {editButtonLabel}
+          </Button>
+        ) : null}
+      </Title>
+
+      {isStepInEdition ? <section>{form}</section> : null}
+      {isStepValidated && showRestitution ? (
+        <section>{restitution}</section>
+      ) : null}
+
+      {isStepInEdition ? (
+        <div
+          className={classNames(
+            `${defaultClassName}-icon ${defaultClassName}-icon--lastIcon`,
+          )}
+        >
+          <Svg role="presentation" src={check} />
+        </div>
+      ) : null}
+    </div>
+  );
+};
--- slash/react/src/Steps/types.ts
+++ slash/react/src/Steps/types.ts
@@ -1,6 +1,10 @@
+import { ReactNode } from "react";
+
 export type CustomClickEvent = {
   href: string;
   number?: ReactNode;
   id: string;
   title: string;
 };
+
+export type VerticalStepMode = "edited" | "locked" | "validated";
