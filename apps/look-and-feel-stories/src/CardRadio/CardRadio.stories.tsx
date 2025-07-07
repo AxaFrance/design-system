f4288894f4026e45c504e3a733d353040c95bd58
Samuel Gomez <samuel.gomez@axa.fr>
2025-07-07 11:28:53 +0200

    feat(apollo,look&feel): focus cardcheckbox cardradio

--- apps/look-and-feel-stories/src/CardRadio/CardRadio.stories.tsx
+++ apps/look-and-feel-stories/src/CardRadio/CardRadio.stories.tsx
@@ -117,3 +117,43 @@ export const CardRadioWithLabel: StoryObj<StoryProps> = {
     },
   },
 };
+
+export const CardRadioWithError: StoryObj<StoryProps> = {
+  name: "CardRadioWithError",
+  render: ({ includeIcons = true, options = defaultOptions, ...args }) => {
+    const mappedOptions = options.map((option) => ({
+      ...option,
+      ...(includeIcons && { icon: home }),
+    }));
+    return <CardRadio {...args} options={mappedOptions} />;
+  },
+  args: {
+    type: "vertical",
+    isRequired: true,
+    name: "cities",
+    includeIcons: true,
+    options: defaultOptions,
+    labelGroup: "Choose your city",
+    descriptionGroup: "Select the city where you want to live",
+    error: "There is an error",
+  },
+  argTypes: {
+    type: {
+      control: { type: "inline-radio" },
+      options: ["vertical", "horizontal"],
+    },
+    includeIcons: {
+      control: { type: "boolean" },
+      description: "Toggle icon visibility",
+    },
+    options: {
+      control: { type: "object" },
+      description: "Radio card options",
+    },
+    onChange: { action: "onChange" },
+    value: {
+      control: { type: "select" },
+      options: defaultOptions.map((option) => option.value),
+    },
+  },
+};
