2c0d732e72fec3390e012553f8be975f3319dbcf
JLou <jeanlou.pierme@gmail.com>
2025-07-08 15:33:07 +0200

    test(slash): add Modal tests

--- slash/react/vitest.setup.ts
+++ slash/react/vitest.setup.ts
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { toHaveNoViolations } from "jest-axe";
-import { afterEach, expect, vi } from "vitest";
+import { afterEach, expect, vi, vitest } from "vitest";
 
 expect.extend(toHaveNoViolations);
 
@@ -13,3 +13,21 @@ global.URL.createObjectURL = vi.fn(() => "mockedObjectURL");
 vi.mock("@tanem/svg-injector", () => ({
   SVGInjector: vi.fn(),
 }));
+
+HTMLDialogElement.prototype.show = vitest.fn(function mock(
+  this: HTMLDialogElement,
+) {
+  this.open = true;
+});
+
+HTMLDialogElement.prototype.showModal = vitest.fn(function mock(
+  this: HTMLDialogElement,
+) {
+  this.open = true;
+});
+
+HTMLDialogElement.prototype.close = vitest.fn(function mock(
+  this: HTMLDialogElement,
+) {
+  this.open = false;
+});
