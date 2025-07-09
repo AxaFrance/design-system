e0d84927175b73f4d66af3f66ecb555205fe41ae
sergeomoffo <sergeomoffo@users.noreply.github.com>
2025-07-09 14:05:14 +0000

    deploy: e53ccf91497734a00e2893d6339fb83d5fb5fafc

---
+++ apollo/react/next/assets/Toggle-DVukrpsn.js
@@ -0,0 +1,17 @@
+import{j as e}from"./index-C1mcBe1y.js";import{useMDXComponents as s}from"./index-G7nTXt62.js";import"./index-W3AC2_fq.js";import{T as i,P as t}from"./Toggle.stories-H8IVEKrQ.js";import{M as a,C as c,a as l}from"./index-D1cAWqY-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-C4sGivDV.js";import"./iframe-NsKrpkFo.js";import"./DocsRenderer-CFRXHY34-BgRv0rjj.js";import"./react-18-Cg43Tkb0.js";import"./DropdownCommon-6uE27r1f.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function r(n){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:i,name:"Toggle"}),`
+`,e.jsx(o.h1,{id:"toggle",children:"Toggle"}),`
+`,e.jsx(o.p,{children:"To use the toggle import it like that:"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-tsx",children:`import { Toggle } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <>
+    <Toggle checked={checked} disabled={disabled} onChange={onChange} />
+    <Toggle ref={checkboxRef} />
+    <Toggle checked disabled />
+  </>
+);
+`})}),`
+`,e.jsx(o.h2,{id:"playground",children:"Playground"}),`
+`,e.jsx(c,{of:t}),`
+`,e.jsx(l,{of:t}),`
+`,e.jsxs(o.p,{children:["The toggle accepts all input props except for the ",e.jsx(o.code,{children:"type"})," and ",e.jsx(o.code,{children:"style"}),"."]})]})}function D(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(r,{...n})}):r(n)}export{D as default};
--- apollo/react/next/assets/index-RwLHx5dX.js
+++
@@ -1,1 +0,0 @@
-import"./preview-DOcTbcta.js";import"./DocsRenderer-CFRXHY34-CtXZjC0R.js";import"./index-nkoXttGR.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
