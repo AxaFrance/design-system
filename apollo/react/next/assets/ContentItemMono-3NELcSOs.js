424a997bf7f1df41ff2737d852e93508918194de
abderrahman-fawzy-acn-axa <abderrahman-fawzy-acn-axa@users.noreply.github.com>
2025-07-21 13:06:28 +0000

    deploy: e5d6f26faa236bbdead854c3ef8886bf4e276a6e

---
+++ apollo/react/next/assets/ContentItemMono-3NELcSOs.js
@@ -0,0 +1,10 @@
+import{j as o}from"./index-C1mcBe1y.js";import{useMDXComponents as m}from"./index-G7nTXt62.js";import"./index-87WcOEKu.js";import{C as s,P as e}from"./ContentItemMono.stories-B3zuO02y.js";import{M as i,C as a,a as p}from"./index-B0La5TYA.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-CXYctBQU.js";import"./iframe-BscaA0yW.js";import"./DocsRenderer-CFRXHY34-DxStH6Ft.js";import"./react-18-Cg43Tkb0.js";import"./DropdownCommon-QSNm7ICU.js";import"./BasePictureCommon-k4JAJx1Z.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function r(n){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:s,name:"Content Item Mono"}),`
+`,o.jsx(t.h1,{id:"contentitemmono",children:"ContentItemMono"}),`
+`,o.jsx(t.p,{children:"To use the header import it like that:"}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-tsx",children:`import { ContentItemMono } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <ContentItemMono />;
+`})}),`
+`,o.jsx(t.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(a,{of:e}),`
+`,o.jsx(p,{of:e})]})}function _(n={}){const{wrapper:t}={...m(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(r,{...n})}):r(n)}export{_ as default};
--- apollo/react/next/assets/index-rbOUs2hQ.js
+++
@@ -1,1 +0,0 @@
-import"./preview-BsF6CZMm.js";import"./DocsRenderer-CFRXHY34-_V3jiXRG.js";import"./index-A4SI64-T.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
