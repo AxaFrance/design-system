65ac19363c6b664f4cc4c2b9ace0c9a06b201906
arnaudforaison <arnaudforaison@users.noreply.github.com>
2025-07-24 08:33:37 +0000

    deploy: 222f15b4dc73d0cb21e2e8b388105d6b77ff5704

---
+++ slash/react/next/assets/Footer-Bofnvr1k.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-G7nTXt62.js";import"./index-B4Mz2soX.js";import{F as i,a as e}from"./Footer.stories-D5FutkeP.js";import{M as a,C as m,a as p}from"./index-BeORAHYI.js";import"./index-DbaA8R3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-B9PsDOAd.js";import"./iframe-DdU0JqB6.js";import"./DocsRenderer-CFRXHY34-Dmo0bGoe.js";import"./react-18-Cg43Tkb0.js";import"./index-BOKuxaB8.js";/* empty css              */import"./Loader-CVE_xvy1.js";import"./index-CG6JzWRx.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function n(t){const r={code:"code",h2:"h2",h3:"h3",pre:"pre",...s(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:i}),`
+`,o.jsx(r.h2,{id:"default",children:"Default"}),`
+`,o.jsx(r.h3,{id:"import",children:"Import"}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-tsx",children:`import { Footer } from "@axa-fr/design-system-slash-react";
+`})}),`
+`,o.jsx(r.h3,{id:"use",children:"Use"}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-tsx",children:`export const MyComponent = () => (
+  <Footer icon={logo_asset} href="https://www.axa.fr/" version="1.0.0">
+    © 2021 AXA All right right reserved
+  </Footer>
+);
+`})}),`
+`,o.jsx(r.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(m,{of:e}),`
+`,o.jsx(p,{of:e})]})}function A(t={}){const{wrapper:r}={...s(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(n,{...t})}):n(t)}export{A as default};
---
+++ slash/react/next/assets/index-B4Mz2soX.js
@@ -0,0 +1,1 @@
+import"./preview-B9PsDOAd.js";import"./DocsRenderer-CFRXHY34-Dmo0bGoe.js";import"./index-BeORAHYI.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
