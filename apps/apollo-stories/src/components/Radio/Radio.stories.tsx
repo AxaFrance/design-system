bc218ae5f37d12f1e18e4514cfa1ede529ab808d
Paul PLANCQ <paul.plancq@outlook.fr>
2025-07-28 15:17:50 +0200

    refactor(look&feel,apollo): simplify Radio component stories and styles

--- apps/apollo-stories/src/components/Radio/Radio.stories.tsx
+++ apps/apollo-stories/src/components/Radio/Radio.stories.tsx
@@ -1,75 +1,33 @@
 import { Radio } from "@axa-fr/design-system-apollo-react";
-import { Meta, StoryObj } from "@storybook/react";
-import { ComponentProps, useEffect, useRef } from "react";
-
-const argsDefault = {
-  name: "option1",
-  value: "option1",
-};
-
-const argTypesDefault = {
-  name: {
-    control: { type: "text" },
-  },
-  value: {
-    control: { type: "text" },
-  },
-  checked: {
-    control: { type: "boolean" },
-  },
-  onChange: { action: "onChange" },
-  "aria-invalid": { type: "boolean" },
-} as const;
+import type { Meta, StoryObj } from "@storybook/react";
+import type { ComponentProps } from "react";
 
 const meta: Meta = {
   title: "Components/Form/Radio/Radio",
   component: Radio,
-  argTypes: argTypesDefault,
-  args: argsDefault,
+  argTypes: {
+    name: {
+      control: { type: "text" },
+    },
+    value: {
+      control: { type: "text" },
+    },
+    checked: {
+      control: { type: "boolean" },
+    },
+    isInvalid: { type: "boolean" },
+  },
+  args: {
+    name: "option1",
+    value: "option1",
+  },
 };
 
 export default meta;
 
-const renderFocus = ({ ...args }) => {
-  const ref = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    ref?.current?.focus?.();
-  }, []);
-
-  return <Radio {...args} ref={ref} />;
-};
-
 export const RadioStory: StoryObj<ComponentProps<typeof Radio>> = {
-  name: "Radio",
-};
-
-export const RadioStoryFocus: StoryObj<ComponentProps<typeof Radio>> = {
-  name: "Radio focus",
-  render: renderFocus,
-};
-
-export const RadioStoryChecked: StoryObj<ComponentProps<typeof Radio>> = {
-  name: "Radio Checked",
-  args: {
-    ...argsDefault,
-    checked: true,
-  },
-};
-
-export const RadioStoryError: StoryObj<ComponentProps<typeof Radio>> = {
-  name: "Radio Error",
-  args: {
-    ...argsDefault,
-    "aria-invalid": true,
-  },
-};
-
-export const RadioStoryErrorFocus: StoryObj<ComponentProps<typeof Radio>> = {
-  name: "Radio Error focus",
-  render: renderFocus,
-  args: {
-    ...argsDefault,
-    "aria-invalid": true,
-  },
+  name: "Playground",
+  render: ({ isInvalid, ...args }) => (
+    <Radio {...args} isInvalid={isInvalid ? true : undefined} />
+  ),
 };
--- client/apollo/react/src/Form/Radio/Radio/RadioLF.tsx
+++ client/apollo/react/src/Form/Radio/Radio/RadioLF.tsx
@@ -1,9 +1,3 @@
-import { forwardRef } from "react";
 import "@axa-fr/design-system-apollo-css/dist/Form/Radio/Radio/RadioLF.scss";
-import { RadioCommon, type RadioProps } from "./RadioCommon";
 
-export const Radio = forwardRef<HTMLInputElement, RadioProps>((props, ref) => (
-  <RadioCommon {...props} ref={ref} />
-));
-
-Radio.displayName = "Radio";
+export { Radio } from "./RadioCommon";
