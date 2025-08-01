561530007427cc0cee452568f89c1ea13c3a7a66
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-01 16:05:11 +0200

    feat(look&feel): add CardRadio and CardRadioItem components with styles for no breaking lf

---
+++ client/look-and-feel/react/src/Form/Radio/CardRadioItem.tsx
@@ -0,0 +1,32 @@
+import { type ComponentProps } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Icon, Radio } from "@axa-fr/design-system-apollo-react/lf";
+
+export type TCardRadioItemProps = Omit<ComponentProps<typeof Radio>, "size"> & {
+  label: string;
+  description?: string;
+  subtitle?: string;
+  icon?: ComponentProps<typeof Icon>["src"];
+  size: ComponentProps<typeof Icon>["size"];
+};
+
+export const CardRadioItem = ({
+  label,
+  description,
+  subtitle,
+  icon,
+  size,
+  ...inputProps
+}: TCardRadioItemProps) => (
+  <label className="af-card-radio-label">
+    <Radio {...inputProps} />
+    <div className="af-card-radio-content">
+      {icon && <Icon src={icon} size={size} role="presentation" />}
+      <div className="af-card-radio-content-description">
+        <span>{label}</span>
+        {description && <span>{description}</span>}
+        {subtitle && <span>{subtitle}</span>}
+      </div>
+    </div>
+  </label>
+);
---
+++ client/look-and-feel/react/src/Form/Radio/types.ts
@@ -0,0 +1,17 @@
+import type { ComponentProps, ComponentType } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Icon, Radio } from "@axa-fr/design-system-apollo-react/lf";
+import type { TCardRadioItemProps } from "./CardRadioItem";
+
+export type TCardRadioItemOption = Omit<
+  TCardRadioItemProps,
+  "RadioComponent" | "IconComponent" | "size" | "name"
+>;
+
+export type RadioComponent = {
+  RadioComponent: ComponentType<ComponentProps<typeof Radio>>;
+};
+
+export type IconComponent = {
+  IconComponent: ComponentType<ComponentProps<typeof Icon>>;
+};
