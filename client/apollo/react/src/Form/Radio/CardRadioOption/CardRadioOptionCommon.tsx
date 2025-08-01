ffdca6c49eba70dd8fc93734d5c6cf28b5172879
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-01 15:18:53 +0200

    feat(look&feel,apollo): enhance icon sizing and improve CardRadioOption styles

--- client/apollo/react/src/Form/Radio/CardRadioOption/CardRadioOptionCommon.tsx
+++ client/apollo/react/src/Form/Radio/CardRadioOption/CardRadioOptionCommon.tsx
@@ -5,8 +5,6 @@ import {
   type ReactNode,
 } from "react";
 import type { Icon as IconCommon } from "../../../Icon/IconCommon";
-import { BREAKPOINT } from "../../../utilities/constants";
-import { useIsSmallScreen } from "../../../utilities/hook/useIsSmallScreen";
 import type { Radio } from "../Radio/RadioCommon";
 
 export type CardRadioOptionProps = Omit<
@@ -43,37 +41,30 @@ export const CardRadioOptionCommon = forwardRef<
       ...inputProps
     },
     ref,
-  ) => {
-    const isMobile = useIsSmallScreen(BREAKPOINT.SM);
-    const size: ComponentProps<typeof IconCommon>["size"] = isMobile
-      ? "M"
-      : "L";
-
-    return (
-      <label
-        className={[
-          "af-card-radio-option",
-          isInvalid && "af-card-radio-option--invalid",
-          type === "horizontal" && "af-card-radio-option--horizontal",
-          className,
-        ]
-          .filter(Boolean)
-          .join(" ")}
-      >
-        {icon && <IconComponent src={icon} size={size} role="presentation" />}
-        <div className="af-card-radio-option__content">
-          <p className="af-card-radio-option__label">{label}</p>
-          {Boolean(description) && (
-            <p className="af-card-radio-option__description">{description}</p>
-          )}
-          {Boolean(subtitle) && (
-            <p className="af-card-radio-option__subtitle">{subtitle}</p>
-          )}
-        </div>
-        <RadioComponent {...inputProps} isInvalid={isInvalid} ref={ref} />
-      </label>
-    );
-  },
+  ) => (
+    <label
+      className={[
+        "af-card-radio-option",
+        isInvalid && "af-card-radio-option--invalid",
+        type === "horizontal" && "af-card-radio-option--horizontal",
+        className,
+      ]
+        .filter(Boolean)
+        .join(" ")}
+    >
+      {icon && <IconComponent src={icon} role="presentation" />}
+      <div className="af-card-radio-option__content">
+        <p className="af-card-radio-option__label">{label}</p>
+        {Boolean(description) && (
+          <p className="af-card-radio-option__description">{description}</p>
+        )}
+        {Boolean(subtitle) && (
+          <p className="af-card-radio-option__subtitle">{subtitle}</p>
+        )}
+      </div>
+      <RadioComponent {...inputProps} isInvalid={isInvalid} ref={ref} />
+    </label>
+  ),
 );
 
 CardRadioOptionCommon.displayName = "CardRadioOptionCommon";
