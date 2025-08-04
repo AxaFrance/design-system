d77065743061b5e61bece02b27bce836cfca743a
pplancq <pplancq@users.noreply.github.com>
2025-08-04 11:54:58 +0000

    deploy: c7d93a3f6696728eb02290eb5bf6e2ade6abc6d0

---
+++ apollo/react/next/assets/ItemFile-CAgZqn5G.js
@@ -0,0 +1,12 @@
+import{j as t}from"./index-C1mcBe1y.js";import{useMDXComponents as r}from"./index-G7nTXt62.js";import"./index-ChHp9g_X.js";import{I as s,D as n}from"./ItemFile.stories-sJcLY4kC.js";import{M as m,C as a,a as l}from"./index-CC0KdnAy.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-dnZq5Qlv.js";import"./iframe-DwYKn72U.js";import"./DocsRenderer-CFRXHY34-ho5QLvYk.js";import"./react-18-Cg43Tkb0.js";import"./DropdownCommon-DaQANgyy.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function i(o){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:s,name:"ItemFile"}),`
+`,t.jsx(e.h1,{id:"itemfile",children:"ItemFile"}),`
+`,t.jsx(e.p,{children:"To use the ItemFile import it like that:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`import { ItemFile } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <ItemFile title="Title" subTitle="Subtitle" state="success" />
+);
+`})}),`
+`,t.jsx(e.h2,{id:"default",children:"Default"}),`
+`,t.jsx(a,{of:n}),`
+`,t.jsx(l,{of:n})]})}function T(o={}){const{wrapper:e}={...r(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(i,{...o})}):i(o)}export{T as default};
--- apollo/react/next/assets/index-Bd0dzlHW.js
+++
@@ -1,1 +0,0 @@
-import"./preview-BPr_0hfe.js";import"./DocsRenderer-CFRXHY34-AjMSE4MT.js";import"./index-CcO5btoE.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
