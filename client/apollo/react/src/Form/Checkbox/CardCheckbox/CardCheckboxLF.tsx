e9bfa6afa34dcb306a0d1925ca76b5b2cee2dbf0
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-06 13:48:20 +0200

    refactor(look&feel,apollo): enhance CardCheckbox component with new options and playground

--- client/apollo/react/src/Form/Checkbox/CardCheckbox/CardCheckboxLF.tsx
+++ client/apollo/react/src/Form/Checkbox/CardCheckbox/CardCheckboxLF.tsx
@@ -1,17 +1,16 @@
-import "@axa-fr/design-system-apollo-css/dist/Form/Checkbox/CardCheckbox/CardCheckboxLF.scss";
-import { Icon } from "../../../Icon/IconLF";
 import { ItemMessage } from "../../ItemMessage/ItemMessageLF";
-import { Checkbox } from "../Checkbox/CheckboxLF";
+import { CardCheckboxOption } from "../CardCheckboxOption/CardCheckboxOptionLF";
 import {
   CardCheckboxCommon,
   type CardCheckboxProps,
 } from "./CardCheckboxCommon";
 
+import "@axa-fr/design-system-apollo-css/dist/Form/Checkbox/CardCheckbox/CardCheckboxLF.scss";
+
 export const CardCheckbox = (props: CardCheckboxProps) => (
   <CardCheckboxCommon
     {...props}
-    IconComponent={Icon}
-    CheckboxComponent={Checkbox}
+    CardCheckboxItemComponent={CardCheckboxOption}
     ItemMessageComponent={ItemMessage}
   />
 );
