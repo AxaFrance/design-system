99e59109800e367d09b26ca350852fbda40398e0
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2025-08-06 15:32:30 +0200

    feat(look&feel,apollo): add label prop for accessibility in ItemProgressBar

--- client/apollo/react/src/ItemProgressBar/ItemProgressBarCommon.tsx
+++ client/apollo/react/src/ItemProgressBar/ItemProgressBarCommon.tsx
@@ -1,15 +1,34 @@
-import type { ComponentProps } from "react";
+import { useId, type ComponentProps, type ReactNode } from "react";
 
-type ItemProgressBarProps = ComponentProps<"progress">;
+type ItemProgressBarProps = ComponentProps<"progress"> & {
+  label?: ReactNode;
+};
 
 export const ItemProgressBar = ({
+  id,
+  label,
   className,
   ...props
-}: ItemProgressBarProps) => (
-  <progress
-    className={["af-item-progress-bar", className].filter(Boolean).join(" ")}
-    {...props}
-  />
-);
+}: ItemProgressBarProps) => {
+  let inputId = useId();
+  inputId = id || inputId;
+
+  return (
+    <>
+      {Boolean(label) && (
+        <label className="af-item-progress-bar__label" htmlFor={inputId}>
+          {label}
+        </label>
+      )}
+      <progress
+        id={inputId}
+        className={["af-item-progress-bar", className]
+          .filter(Boolean)
+          .join(" ")}
+        {...props}
+      />
+    </>
+  );
+};
 
 ItemProgressBar.displayName = "ItemProgressBar";
