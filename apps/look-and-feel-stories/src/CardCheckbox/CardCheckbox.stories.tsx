925e16de8cdac3a1907d3e55359239c016e72b78
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-06 13:48:20 +0200

    refactor(look&feel,apollo): enhance CardCheckbox component with new options and playground

--- apps/look-and-feel-stories/src/CardCheckbox/CardCheckbox.stories.tsx
+++ apps/look-and-feel-stories/src/CardCheckbox/CardCheckbox.stories.tsx
@@ -1,137 +1,104 @@
 import { CardCheckbox } from "@axa-fr/design-system-apollo-react/lf";
-import homeIcons from "@material-symbols/svg-400/outlined/home.svg";
-import { Meta, StoryObj } from "@storybook/react";
-import { ComponentProps, useEffect, useRef } from "react";
-
-const argTypesDefault = {
-  type: {
-    control: { type: "inline-radio" },
-    options: ["vertical", "horizontal"],
-  },
-  options: {
-    control: { type: "object" },
-  },
-  onChange: { action: "onChange" },
-} as const;
+import homeIcon from "@material-symbols/svg-400/outlined/home.svg";
+import type { Meta, StoryObj } from "@storybook/react";
+import type { ComponentProps } from "react";
 
 const optionsDefault = [
   {
-    label: "Titre",
-    description: "Sous-titre 1",
-    subtitle: "Sous-titre 2",
-    name: "paris",
-    value: "paris",
-    icon: homeIcons,
+    label: "option 1",
+    description: "description 1",
+    subtitle: "subtitle 1",
+    value: "1",
+    icon: homeIcon,
   },
   {
-    label: "Titre",
-    description: "Sous-titre 1",
-    subtitle: "Sous-titre 2",
-    name: "bruxelles",
-    value: "bruxelles",
-    icon: homeIcons,
+    label: "option 2",
+    description: "description 2",
+    subtitle: "subtitle 2",
+    value: "2",
+    icon: homeIcon,
   },
   {
-    label: "Titre",
-    description: "Sous-titre 1",
-    subtitle: "Sous-titre 2",
-    name: "lille",
-    value: "lille",
-    icon: homeIcons,
+    label: "option 3",
+    description: "description 3",
+    subtitle: "subtitle 3",
+    value: "3",
+    icon: homeIcon,
   },
 ];
-
-const argsDefault = {
-  type: "vertical",
-  isRequired: true,
-  error: "",
-  options: optionsDefault,
-} as const;
 
 const meta: Meta = {
   title: "Components/Form/Checkbox/CardCheckbox",
   component: CardCheckbox,
-  argTypes: argTypesDefault,
-  args: argsDefault,
+  argTypes: {
+    type: {
+      control: { type: "inline-radio" },
+      options: ["vertical", "horizontal"],
+      table: {
+        defaultValue: { summary: "vertical" },
+        type: { summary: '"vertical" | "horizontal"' },
+      },
+    },
+    label: {
+      control: "text",
+    },
+    description: {
+      control: "text",
+    },
+    options: {
+      control: { type: "object" },
+      description: "Radio card options",
+    },
+    required: {
+      control: "boolean",
+    },
+    error: {
+      control: "text",
+    },
+    labelGroup: {
+      table: {
+        disable: true,
+      },
+    },
+    descriptionGroup: {
+      table: {
+        disable: true,
+      },
+    },
+    isRequired: {
+      table: {
+        disable: true,
+      },
+    },
+    value: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  args: {
+    type: "vertical",
+    label: "Label",
+    description: "Description",
+    name: "name",
+    options: optionsDefault,
+    required: false,
+    error: "",
+  },
 };
 
 export default meta;
 
-const renderFocus = ({
-  options,
-  ...args
-}: ComponentProps<typeof CardCheckbox>) => {
-  const ref = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    ref?.current?.focus?.();
-  }, []);
-
-  const mappedOptionsWithFocus = [
-    { ...options[0], ref },
-    options[1],
-    options[2],
-  ];
-
-  return <CardCheckbox {...args} options={mappedOptionsWithFocus} />;
-};
-
 export const CardCheckboxStory: StoryObj<ComponentProps<typeof CardCheckbox>> =
-  { name: "CardCheckbox" };
-
-export const CardCheckboxFocus: StoryObj<ComponentProps<typeof CardCheckbox>> =
-  { name: "CardCheckboxFocus", render: renderFocus };
-
-export const CardCheckboxStoryChecked: StoryObj<
-  ComponentProps<typeof CardCheckbox>
-> = {
-  name: "CardCheckbox checked",
-  args: {
-    ...argsDefault,
-    options: optionsDefault.map((option) => ({ ...option, checked: true })),
-  },
-};
-
-export const CardCheckboxWithLabel: StoryObj<
-  ComponentProps<typeof CardCheckbox>
-> = {
-  name: "CardCheckboxWithLabel",
-  args: {
-    ...argsDefault,
-    labelGroup: "Quelle ville ?",
-    descriptionGroup: "Choisissez une ville",
-  },
-};
-
-export const CardCheckboxWithError: StoryObj<
-  ComponentProps<typeof CardCheckbox>
-> = {
-  name: "CardCheckboxWithError",
-  args: {
-    ...argsDefault,
-    labelGroup: "Quelle ville ?",
-    descriptionGroup: "Choisissez une ville",
-    error: "There is an error",
-    options: optionsDefault.map((option) => ({
-      ...option,
-      "aria-invalid": true,
-    })),
-  },
-};
-
-export const CardCheckboxWithErrorFocus: StoryObj<
-  ComponentProps<typeof CardCheckbox>
-> = {
-  name: "CardCheckboxWithErrorFocus",
-  render: renderFocus,
-  args: {
-    ...argsDefault,
-    labelGroup: "Quelle ville ?",
-    descriptionGroup: "Choisissez une ville",
-    error: "There is an error",
-    options: optionsDefault.map((option) => ({
-      ...option,
-      "aria-invalid": true,
-    })),
-  },
-};
+  {
+    name: "Playground",
+    render: ({ description, error, name, type, ...args }) => (
+      <CardCheckbox
+        {...args}
+        description={description !== "" ? description : undefined}
+        error={error !== "" ? error : undefined}
+        name={name !== "" ? name : undefined}
+        type={type === "horizontal" ? type : undefined}
+      />
+    ),
+  };
