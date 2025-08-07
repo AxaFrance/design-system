434587a6bdb0f6c46194c28e89e7f34f09bfcb3f
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-07 14:24:46 +0200

    feat(look&feel): add CardCheckbox and CardCheckboxItem components with styles for no breaking lf

--- client/look-and-feel/react/src/Form/Checkbox/CardCheckbox.tsx
+++ client/look-and-feel/react/src/Form/Checkbox/CardCheckbox.tsx
@@ -1,2 +1,83 @@
-/* eslint-disable import/no-extraneous-dependencies */
-export { CardCheckbox } from "@axa-fr/design-system-apollo-react/lf";
+import { useId, type ChangeEventHandler } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ItemMessage } from "@axa-fr/design-system-apollo-react/lf";
+import {
+  BREAKPOINT,
+  getComponentClassName,
+  useIsSmallScreen,
+} from "../../utilities";
+import { CardCheckboxItem, type TCardCheckboxItem } from "./CardCheckboxItem";
+
+import "@axa-fr/design-system-look-and-feel-css/dist/Form/CardCheckbox/CardCheckbox.scss";
+
+export type CardCheckboxProps = Partial<TCardCheckboxItem> & {
+  type: "vertical" | "horizontal";
+  labelGroup?: string;
+  descriptionGroup?: string;
+  isRequired?: boolean;
+  options: TCardCheckboxItem[];
+  onChange?: ChangeEventHandler;
+  error?: string;
+};
+
+export const CardCheckbox = ({
+  className,
+  labelGroup,
+  descriptionGroup,
+  isRequired,
+  options,
+  onChange,
+  type = "vertical",
+  error,
+  ...inputProps
+}: CardCheckboxProps) => {
+  const componentClassName = getComponentClassName(
+    "af-card-checkbox__container",
+    className,
+  );
+  const checkboxGroupClassName = getComponentClassName(
+    "af-card-checkbox-group",
+    className,
+    type,
+  );
+  const errorId = useId();
+
+  const isMobile = useIsSmallScreen(BREAKPOINT.SM);
+  const size = isMobile ? "M" : "L";
+
+  return (
+    <fieldset className={componentClassName}>
+      {labelGroup ? (
+        <legend className="af-card-checkbox__legend">
+          <p>
+            {labelGroup}
+            {isRequired ? <span aria-hidden>&nbsp;*</span> : null}
+          </p>
+          {descriptionGroup ? (
+            <p className="af-card-checkbox__description">{descriptionGroup}</p>
+          ) : null}
+        </legend>
+      ) : null}
+      <div className="af-card-checkbox__choices">
+        <ul className={checkboxGroupClassName}>
+          {options.map(({ hasError, ...optionProps }) => (
+            <li key={crypto.randomUUID()}>
+              <CardCheckboxItem
+                {...inputProps}
+                size={size}
+                errorId={errorId}
+                onChange={onChange}
+                hasError={Boolean(error) || hasError}
+                {...optionProps}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <ItemMessage id={errorId} message={error} messageType="error" />
+      </div>
+    </fieldset>
+  );
+};
+
+CardCheckbox.displayName = "CardCheckbox";
