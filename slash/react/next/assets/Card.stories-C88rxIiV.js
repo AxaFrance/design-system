a83eae20842118c68c1d0961bfabd1f78c2b1129
JLou <JLou@users.noreply.github.com>
2025-08-11 12:50:14 +0000

    deploy: a5a4b28a4813b0bc498bb942636fc3bc303fc3cd

---
+++ slash/react/next/assets/Card.stories-C88rxIiV.js
@@ -0,0 +1,27 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";/* empty css              */import{t as v,u as I}from"./Loader-MKkxOcwp.js";import"./index-DbaA8R3S.js";import"./index-CG6JzWRx.js";import{f as y}from"./index-28pPPJTf.js";import{v as x}from"./villa-BhFUGjo6.js";const i="af-card",s=({children:r,className:H,iconSrc:c,orientation:d="vertical",error:B=!1,..._})=>o.jsxs("button",{type:"button",className:v(i,B&&`${i}--error`,d==="horizontal"&&`${i}--${d}`,H),..._,children:[c?o.jsx(I,{src:c}):null,r]}),w={component:s,title:"Components/Card",parameters:{layout:"centered"},args:{onClick:y(),disabled:!1},argTypes:{orientation:{table:{disable:!0}},iconSrc:{table:{disable:!0}}}},n={name:"Card Button with icon",render:({...r})=>o.jsx(s,{...r,iconSrc:x,children:"Contenu"})},a={name:"Card Button",render:({...r})=>o.jsx(s,{...r,children:"Contenu"})},e={name:"Card Button Horizontal with icon",render:({...r})=>o.jsx(s,{...r,iconSrc:x,orientation:"horizontal",children:"Contenu"})},t={name:"Card Button Horizontal",render:({...r})=>o.jsx(s,{...r,orientation:"horizontal",children:"Contenu"})};var l,u,m;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: "Card Button with icon",
+  render: ({
+    ...args
+  }) => <Card {...args} iconSrc={villaIcon}>
+      Contenu
+    </Card>
+}`,...(m=(u=n.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var p,C,h;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: "Card Button",
+  render: ({
+    ...args
+  }) => <Card {...args}>Contenu</Card>
+}`,...(h=(C=a.parameters)==null?void 0:C.docs)==null?void 0:h.source}}};var g,f,z;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  name: "Card Button Horizontal with icon",
+  render: ({
+    ...args
+  }) => <Card {...args} iconSrc={villaIcon} orientation="horizontal">
+      Contenu
+    </Card>
+}`,...(z=(f=e.parameters)==null?void 0:f.docs)==null?void 0:z.source}}};var S,b,j;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  name: "Card Button Horizontal",
+  render: ({
+    ...args
+  }) => <Card {...args} orientation="horizontal">
+      Contenu
+    </Card>
+}`,...(j=(b=t.parameters)==null?void 0:b.docs)==null?void 0:j.source}}};const D=["Default","VerticalWithoutIcon","Horizontal","HorizontalWithoutIcon"],$=Object.freeze(Object.defineProperty({__proto__:null,Default:n,Horizontal:e,HorizontalWithoutIcon:t,VerticalWithoutIcon:a,__namedExportsOrder:D,default:w},Symbol.toStringTag,{value:"Module"}));export{$ as C,n as D,e as H,a as V,t as a};
---
+++ slash/react/next/assets/Tabs-CF4yLsPK.js
@@ -0,0 +1,55 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-G7nTXt62.js";import"./index-DNBN1Q5Q.js";import{T as l,S as e,C as s}from"./Tabs.stories-DtwQuZCH.js";import{M as T,C as o,a as i}from"./index-DEj4JD8P.js";import"./index-DbaA8R3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-BvL8hSYQ.js";import"./iframe-C09A3D4A.js";import"./DocsRenderer-CFRXHY34-CNYLpgKJ.js";import"./react-18-Cg43Tkb0.js";import"./index-BOKuxaB8.js";/* empty css              */import"./Loader-MKkxOcwp.js";import"./index-CG6JzWRx.js";import"./index-28pPPJTf.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function r(a){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...c(),...a.components};return n.jsxs(n.Fragment,{children:[n.jsx(T,{of:l}),`
+`,n.jsx(t.h1,{id:"tabs",children:"Tabs"}),`
+`,n.jsx(t.p,{children:"To use tabs, import them as follows:"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { Tabs } from "@axa-fr/design-system-slash-react";
+`})}),`
+`,n.jsx(t.h2,{id:"single-tab",children:"Single Tab"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`const MyComponent = () => (
+  <Tabs>
+    <Tabs.Tab title="My Title">Content of my first tab</Tabs.Tab>
+  </Tabs>
+);
+`})}),`
+`,n.jsx(o,{of:e}),`
+`,n.jsx(i,{of:e}),`
+`,n.jsx(t.h2,{id:"complex-tabs",children:"Complex Tabs"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`const TabTitleIconLeft = (
+  <span>
+    <i className="glyphicon glyphicon-ok" /> Title with left icon
+  </span>
+);
+const TabTitleIconRight = (
+  <span>
+    Title with right icon <i className="glyphicon glyphicon-facetime-video" />
+  </span>
+);
+const TabTitleBadge = (
+  <span>
+    Title with badge <Badge classModifier="danger"> 42 </Badge>
+  </span>
+);
+const TabTitleIconBadge = (
+  <span>
+    Title with badge and left icon
+    <Badge classModifier="error"> Lorem ipsum </Badge>
+    <i className="glyphicon glyphicon-facetime-video" />
+  </span>
+);
+
+const MyComponent = () => (
+  <Tabs>
+    <Tabs.Tab title={TabTitleIconLeft} classModifier="has-icon-left">
+      Content of my first tab
+    </Tabs.Tab>
+    <Tabs.Tab title={TabTitleIconRight} classModifier="has-icon-right">
+      Content of my second tab
+    </Tabs.Tab>
+    <Tabs.Tab title={TabTitleBadge}>Content of my third tab </Tabs.Tab>
+    <Tabs.Tab title={TabTitleIconBadge} classModifier="has-icon-left">
+      Content of my fifth tab
+    </Tabs.Tab>
+  </Tabs>
+);
+`})}),`
+`,n.jsx(o,{of:s}),`
+`,n.jsx(i,{of:s})]})}function S(a={}){const{wrapper:t}={...c(),...a.components};return t?n.jsx(t,{...a,children:n.jsx(r,{...a})}):r(a)}export{S as default};
