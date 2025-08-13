6ee107d17fda4b12075c20ef1fb8a7d9606117d9
pplancq <pplancq@users.noreply.github.com>
2025-08-13 07:45:20 +0000

    deploy: 434587a6bdb0f6c46194c28e89e7f34f09bfcb3f

---
+++ apollo/react/next/assets/CardCheckbox-BQQiw_V5.js
@@ -0,0 +1,40 @@
+import{j as e}from"./index-C1mcBe1y.js";import{useMDXComponents as s}from"./index-G7nTXt62.js";import{M as i,C as a,a as c}from"./index-BbXG3EVb.js";import{C as l,a as t}from"./CardCheckbox.stories-BPEE8Jcz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./iframe-hCRcq6Qg.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./DropdownCommon-CY8DX785.js";import"./home-BuLaEOyh.js";function r(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:l,name:"CardCheckbox"}),`
+`,e.jsx(n.h1,{id:"cardcheckbox-components",children:"CardCheckbox components"}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to use"}),`
+`,e.jsxs(n.p,{children:["To use the ",e.jsx(n.code,{children:"CardCheckbox"}),"component import it like this:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { CardCheckbox } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <CardCheckbox
+    type="vertical"
+    name="city"
+    label="Choisissez des villes"
+    options={[
+      {
+        label: "Paris",
+        value: "paris",
+        icon: homeIcon,
+        description: "Capitale de la France",
+        subtitle: "Nord",
+      },
+      {
+        label: "Bruxelles",
+        value: "bruxelles",
+        icon: homeIcon,
+        description: "Capitale de la Belgique",
+      },
+      {
+        label: "Lille",
+        value: "lille",
+        icon: homeIcon,
+        checked: true,
+      },
+    ]}
+  />
+);
+`})}),`
+`,e.jsx(n.h2,{id:"playground",children:"Playground"}),`
+`,e.jsxs(n.p,{children:["The CardCheckbox component has 2 modes: ",e.jsx(n.code,{children:"vertical"})," (default) and ",e.jsx(n.code,{children:"horizontal"}),`.
+You can see them in action in the stories below.`]}),`
+`,e.jsx(a,{of:t}),`
+`,e.jsx(c,{of:t})]})}function g(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{g as default};
