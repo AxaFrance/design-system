cf868366329ba6c3b1715de2e1b059daa5749949
JLou <jeanlou.pierme@gmail.com>
2025-08-14 16:37:31 +0200

    fix(slash): component User's default text color is grey

--- apps/slash-stories/src/Layout/Header.stories.tsx
+++ apps/slash-stories/src/Layout/Header.stories.tsx
@@ -1,7 +1,7 @@
 import logo from "@axa-fr/design-system-slash-css/logo-axa.svg";
 import { Meta, StoryObj } from "@storybook/react";
 
-import { Header, Infos, Name } from "@axa-fr/design-system-slash-react";
+import { Header, Infos, Name, User } from "@axa-fr/design-system-slash-react";
 
 const meta: Meta<typeof Header> = {
   component: Header,
@@ -41,6 +41,38 @@ export const HeaderWithNameAndInfos: StoryObj<typeof Header> = {
             { word: "Wallet:", definition: "000123456789" },
             { word: "Status :", definition: "New business" },
           ]}
+        />
+      </>
+    ),
+  },
+};
+
+export const HeaderWithNameUserAndInfos: StoryObj<typeof Header> = {
+  args: {
+    children: (
+      <>
+        <Name
+          alt="Logo React"
+          img={logo}
+          onClick={() => {}}
+          subtitle="Baseline"
+          title="Nom de l'application"
+        />
+        <Infos
+          infos={[
+            { word: "Customer :", definition: "0123456789 - NOM" },
+            { word: "Wallet:", definition: "000123456789" },
+            { word: "Status :", definition: "New business" },
+          ]}
+        />
+        <User
+          name="Pierre Martin"
+          profile="profile"
+          title="Voir mon profil"
+          onClick={({ event }) => {
+            event.preventDefault();
+          }}
+          href="/#"
         />
       </>
     ),
