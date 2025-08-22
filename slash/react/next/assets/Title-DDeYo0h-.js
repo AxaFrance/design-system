98bb301ed97a0c495b13634c4e9b9093aa9fad0f
MartinWeb <MartinWeb@users.noreply.github.com>
2025-08-22 12:50:02 +0000

    deploy: cc0046398dbabcf1a818746619f6595a0b0b8615

---
+++ slash/react/next/assets/Title-DDeYo0h-.js
@@ -0,0 +1,32 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-G7nTXt62.js";import{M as r,C as a,a as l}from"./index-BX9wa-IN.js";import{T as c,a as o}from"./Title.stories-Cz3xLnZ1.js";import"./index-DbaA8R3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CT7BXZxk.js";import"./index-CG6JzWRx.js";import"./index-BOKuxaB8.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";/* empty css              */import"./CardData-DhNLGjyo.js";function i(n){const e={code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",ul:"ul",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:c}),`
+`,t.jsx(e.h2,{id:"title",children:"Title"}),`
+`,t.jsx(e.h3,{id:"use-of-title",children:"Use of Title"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`import { Title } from "@axa-fr/design-system-slash-react";
+
+export const MyComponent = () => (
+  <Title classModifier="af-title" className="">
+    Sample Title
+  </Title>
+);
+`})}),`
+`,t.jsx(e.p,{children:"The title can also contain any React component. Simply pass it into :"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:["the ",t.jsx(e.code,{children:"contentLeft"})," prop if you want it to be displayed between the title and the separator."]}),`
+`,t.jsxs(e.li,{children:["the ",t.jsx(e.code,{children:"contentRight"})," prop if you want it to be displayed after the separator."]}),`
+`]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`import { Title } from "@axa-fr/design-system-slash-react";
+
+export const MyComponent = () => (
+  <Title
+    classModifier="af-title"
+    className=""
+    contentLeft={<Button>Left button</Button>}
+    contentRight={<Button>Right button</Button>}
+  >
+    Title with content
+  </Title>
+);
+`})}),`
+`,t.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,t.jsx(a,{of:o}),`
+`,t.jsx(l,{of:o})]})}function b(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(i,{...n})}):i(n)}export{b as default};
---
+++ slash/react/next/assets/User.stories-BWPBQNb7.js
@@ -0,0 +1,7 @@
+/* empty css              */import"./CardData-DhNLGjyo.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DbaA8R3S.js";import"./index-CG6JzWRx.js";import{U as m}from"./User-ItvCLObR.js";import{f as p}from"./index-28pPPJTf.js";const c={args:{name:"Pierre Martin",profile:"profile",title:"Voir mon profil",onClick:p()},argTypes:{onClick:{action:"onClick"}},component:m,title:"Components/Header/User"},r={args:{href:"/#"}},e={name:"User Without link"};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    href: "/#"
+  }
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var a,i,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: "User Without link"
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const l=["UserWithLink","UserWithoutLink"],W=Object.freeze(Object.defineProperty({__proto__:null,UserWithLink:r,UserWithoutLink:e,__namedExportsOrder:l,default:c},Symbol.toStringTag,{value:"Module"}));export{W as U,r as a,e as b};
