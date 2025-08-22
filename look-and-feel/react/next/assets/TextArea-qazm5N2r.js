bb8f627bdbf024706a58fbece41400155cd4739d
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-08-22 12:38:17 +0000

    deploy: 493e37a577d405065d98fa28e9ff68a94c24083f

---
+++ look-and-feel/react/next/assets/TextArea-qazm5N2r.js
@@ -0,0 +1,10 @@
+import{j as o}from"./index-C1mcBe1y.js";import{useMDXComponents as s}from"./index-G7nTXt62.js";import"./index-C6YXWs-j.js";import{T as a,a as r}from"./TextArea.stories-MAPQu-y8.js";import{M as i,C as m,a as p}from"./index-UofKVJyM.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-C3ivA4Nu.js";import"./iframe-RIHdJhP9.js";import"./DocsRenderer-CFRXHY34-4kDP-UIf.js";import"./react-18-Cg43Tkb0.js";import"./FooterLF-Cnd-k_XT.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function n(e){const t={code:"code",h1:"h1",h2:"h2",pre:"pre",...s(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:a,name:"TextArea"}),`
+`,o.jsx(t.h1,{id:"textarea",children:"TextArea"}),`
+`,o.jsx(t.h2,{id:"how-to-use",children:"How to use"}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-tsx",children:`import { TextArea } from "@axa-fr/design-system-apollo-react/lf";
+
+const MyComponent = () => <TextArea />;
+`})}),`
+`,o.jsx(t.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(m,{of:r,sourceState:"shown"}),`
+`,o.jsx(p,{of:r})]})}function D(e={}){const{wrapper:t}={...s(),...e.components};return t?o.jsx(t,{...e,children:o.jsx(n,{...e})}):n(e)}export{D as default};
--- look-and-feel/react/next/assets/index-CeOM2NfX.js
+++
@@ -1,1 +0,0 @@
-import"./preview-yQ2iYKr6.js";import"./DocsRenderer-CFRXHY34-BD_qj0S8.js";import"./index-C37jYOw3.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
