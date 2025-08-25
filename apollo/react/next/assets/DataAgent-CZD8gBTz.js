5013cafe38ec3cd50534e0207b0f484f908ccbae
Nourelayne <Nourelayne@users.noreply.github.com>
2025-08-25 14:29:46 +0000

    deploy: ae4695add2df747dd0e0f5678d6b6382eb7f9639

---
+++ apollo/react/next/assets/DataAgent-CZD8gBTz.js
@@ -0,0 +1,66 @@
+import{j as n}from"./index-C1mcBe1y.js";import{useMDXComponents as i}from"./index-G7nTXt62.js";import"./index-CSAwXcYd.js";import{D as r,a}from"./DataAgent.stories-DCkKPab6.js";import{M as c,C as s,a as l}from"./index-BOFaJs9O.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-ES6OCNge.js";import"./iframe-PjtRoNuH.js";import"./DocsRenderer-CFRXHY34-hoxWFa_B.js";import"./react-18-Cg43Tkb0.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./FooterApollo-C1lvlZhO.js";import"./constants-DLOxiQAl.js";import"./account_balance-fill-Cw_j1XWX.js";import"./BasePictureCommon-Bq6nsnps.js";import"./ContentItemMonoApollo-D4SiMm2b.js";import"./DividerCommon-8j865qc_.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function o(e){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(c,{of:r,name:"DataAgent"}),`
+`,n.jsx(t.h1,{id:"dataagent",children:"DataAgent"}),`
+`,n.jsx(t.p,{children:"To use the DataAgent import it like that:"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { DataAgent } from "@axa-fr/design-system-apollo-react";
+import accountBalance from "@material-symbols/svg-400/rounded/account_balance-fill.svg";
+
+const MyComponent = () => (
+  <DataAgent
+    agentContractProps={{
+      subtitle: "Assurance Vie, Auto, Habitation, Complémentaire santé",
+      title: "Contrat rattaché",
+      type: "stick",
+    }}
+    agentProps={{
+      picture: "https://dummyimage.com/48/48/fff&text=A",
+      subtitle: "AXA Assurance & Banque",
+      title: "Michel Lhote",
+      type: "picture",
+    }}
+    clickContents={[
+      {
+        icon: accountBalance,
+        onClick: () => {},
+        title: "Envoyer un message",
+        variant: "small",
+      },
+      {
+        icon: accountBalance,
+        onClick: () => {},
+        title: "Envoyer un message à l’agent",
+        variant: "small",
+      },
+      {
+        icon: accountBalance,
+        onClick: () => {},
+        title: "Accéder à plus d’informations",
+        variant: "small",
+      },
+    ]}
+    contents={[
+      {
+        icon: accountBalance,
+        subtitle1: "01 23 34 45 67",
+        title: "",
+        type: "icon",
+      },
+      {
+        icon: accountBalance,
+        subtitle1: "01 23 34 45 67",
+        title: "",
+        type: "icon",
+      },
+      {
+        icon: accountBalance,
+        subtitle1: "01 23 34 45 67",
+        title: "",
+        type: "icon",
+      },
+    ]}
+    texteOrias="N° Orias (orias.fr) : 10057601 - 10057601"
+  />
+);
+`})}),`
+`,n.jsx(t.h2,{id:"default",children:"Default"}),`
+`,n.jsx(s,{of:a}),`
+`,n.jsx(l,{of:a})]})}function N(e={}){const{wrapper:t}={...i(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{N as default};
---
+++ apollo/react/next/assets/index-CSAwXcYd.js
@@ -0,0 +1,1 @@
+import"./preview-ES6OCNge.js";import"./DocsRenderer-CFRXHY34-hoxWFa_B.js";import"./index-BOFaJs9O.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
