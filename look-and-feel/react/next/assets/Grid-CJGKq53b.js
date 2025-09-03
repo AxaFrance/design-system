6ed22be347eaac7e5769037008f62f2e360c211a
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2025-09-03 13:17:12 +0000

    deploy: d39f9cd1725fbf3594e6afcb5ff6f33214469ddb

---
+++ look-and-feel/react/next/assets/Grid-CJGKq53b.js
@@ -0,0 +1,12 @@
+import{j as r}from"./index-C1mcBe1y.js";import{useMDXComponents as a}from"./index-G7nTXt62.js";import{M as f,C as i,a as m}from"./index-CwfQGcsQ.js";import{Form as e}from"./GridExampleForm.stories-B3yZIaIJ.js";import{Page as n}from"./GridExamplePage.stories-CeVcctye.js";import{G as x,W as p}from"./GridWireframe.stories-ChbZsJ6K.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./iframe-CWBkmzEu.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";/* empty css                      */import"./ToggleCommon-DUfTbsFe.js";import"./constants-IoXHNJcQ.js";import"./DebugGridLF-szWpnxKl.js";import"./Title-DH_LS40V.js";import"./IconBg-DX4KQOGu.js";import"./home-BuLaEOyh.js";import"./MyHeader-UoTCYHk1.js";import"./FooterCommon-BsEMH1Tw.js";import"./constants-DLOxiQAl.js";import"./person-fill-QYsbg8Nc.js";import"./NavBar-CdN4YrXw.js";import"./PreviousLink-XexooXn0.js";import"./arrow_back-DnyUknY1.js";import"./ClickItem-C6p4rz8a.js";import"./ContentItemMonoLF-Q_v-hOTc.js";import"./List-CvTqqjJ3.js";import"./Card-DaUtUjio.js";import"./article-fill-B_-J39Ry.js";import"./ContentItemDuoLF-sIKZ2yHv.js";function s(t){const o={a:"a",h2:"h2",p:"p",...a(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(f,{of:x}),`
+`,r.jsx(o.h2,{id:"grid-system",children:"Grid System"}),`
+`,r.jsxs(o.p,{children:["More info here : ",r.jsx(o.a,{href:"https://axafrance.github.io/design-system/apollo/react/latest?path=/docs/guidelines-grid-system--grid",rel:"nofollow",children:"Grid documentation"})]}),`
+`,r.jsx(o.h2,{id:"wireframe",children:"Wireframe"}),`
+`,r.jsx(i,{of:p}),`
+`,r.jsx(m,{of:p}),`
+`,r.jsx(o.h2,{id:"example-page",children:"Example page"}),`
+`,r.jsx(i,{of:n}),`
+`,r.jsx(m,{of:n}),`
+`,r.jsx(o.h2,{id:"example-form",children:"Example form"}),`
+`,r.jsx(i,{of:e}),`
+`,r.jsx(m,{of:e})]})}function K(t={}){const{wrapper:o}={...a(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(s,{...t})}):s(t)}export{K as default};
--- look-and-feel/react/next/assets/index-Jh3HG0D7.js
+++
@@ -1,1 +0,0 @@
-import"./preview-DZNZ_kxF.js";import"./DocsRenderer-CFRXHY34-DMC1CZAi.js";import"./index-DFxZZ8An.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
