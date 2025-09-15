ba5cf8fa42d911b08bdd700350caab07eb8fce4e
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-15 21:02:41 +0200

    refactor(apollo,look&feel): component ItemLabel type

--- client/apollo/react/src/Form/ItemLabel/ItemLabelApollo.tsx
+++ client/apollo/react/src/Form/ItemLabel/ItemLabelApollo.tsx
@@ -1,8 +1,7 @@
 import "@axa-fr/design-system-apollo-css/dist/Form/ItemLabel/ItemLabelApollo.scss";
-import type { ComponentProps } from "react";
 import { Button } from "../../Button/ButtonApollo";
-import { ItemLabel as ItemLabelCommon } from "./ItemLabelCommon";
+import { ItemLabelCommon, type ItemLabelProps } from "./ItemLabelCommon";
 
-export const ItemLabel = (
-  props: Omit<ComponentProps<typeof ItemLabelCommon>, "ButtonComponent">,
-) => <ItemLabelCommon {...props} ButtonComponent={Button} />;
+export const ItemLabel = (props: ItemLabelProps) => (
+  <ItemLabelCommon {...props} ButtonComponent={Button} />
+);
