1bc319ebe4b0191442866329cc6b2c25ecdd298b
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-09-16 09:22:03 +0000

    deploy: 44a0f6c9475d6d4c6bda6cec0c757bd848be6631

---
+++ look-and-feel/react/2.0.3/assets/ItemMessage-hcqjLnbR.js
@@ -0,0 +1,20 @@
+import{j as e}from"./index-C1mcBe1y.js";import{useMDXComponents as c}from"./index-G7nTXt62.js";import"./index-BYRanq_w.js";import{I as p,E as t,S as a,W as i}from"./ItemMessage.stories-D00QOmsP.js";import{M as d,C as n,a as o}from"./index-B3spd-55.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-BJrEgZu5.js";import"./iframe-iwrWJVMg.js";import"./DocsRenderer-CFRXHY34-DvxwBLwg.js";import"./react-18-Cg43Tkb0.js";import"./ToggleCommon-C4liXewM.js";import"./constants-BcuPttET.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function m(r){const s={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...c(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:p,name:"ItemMessage"}),`
+`,e.jsx(s.h1,{id:"itemmessage",children:"ItemMessage"}),`
+`,e.jsx(s.p,{children:"To use the spinner import it like that:"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-tsx",children:`import { ItemMessage } from "@axa-fr/design-system-look-and-feel-react";
+//or
+import { ItemMessage } from "@axa-fr/design-system-apollo-react/lf";
+
+const MyComponent = () => <ItemMessage type="success" message="My message" />;
+`})}),`
+`,e.jsx(s.h2,{id:"type",children:"Type"}),`
+`,e.jsxs(s.p,{children:["Use the ",e.jsx(s.code,{children:"type"})," prop to change the type of the item message. The available variants are ",e.jsx(s.code,{children:"error"})," (default) or ",e.jsx(s.code,{children:"success"}),"."]}),`
+`,e.jsx(s.h2,{id:"error-message",children:"Error Message"}),`
+`,e.jsx(n,{of:t}),`
+`,e.jsx(o,{of:t}),`
+`,e.jsx(s.h2,{id:"success-message",children:"Success Message"}),`
+`,e.jsx(n,{of:a}),`
+`,e.jsx(o,{of:a}),`
+`,e.jsx(s.h2,{id:"warning-message",children:"Warning Message"}),`
+`,e.jsx(n,{of:i}),`
+`,e.jsx(o,{of:i})]})}function W(r={}){const{wrapper:s}={...c(),...r.components};return s?e.jsx(s,{...r,children:e.jsx(m,{...r})}):m(r)}export{W as default};
