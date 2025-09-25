9ccfa7d0e150bd4a3d6d35beca99defe4315a0e1
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:36:37 +0200

    feat(apollo,look&feel): itemfile sass to css

--- client/apollo/react/src/Form/ItemFile/ItemFileApollo.tsx
+++ client/apollo/react/src/Form/ItemFile/ItemFileApollo.tsx
@@ -1,4 +1,4 @@
-import "@axa-fr/design-system-apollo-css/dist/Form/ItemFile/ItemFileApollo.scss";
+import "@axa-fr/design-system-apollo-css/dist/Form/ItemFile/ItemFileApollo.css";
 import deleteIcon from "@material-symbols/svg-400/outlined/delete.svg";
 import visibilityIcon from "@material-symbols/svg-400/outlined/visibility-fill.svg";
 import { ClickIcon } from "../../ClickIcon/ClickIconApollo";
