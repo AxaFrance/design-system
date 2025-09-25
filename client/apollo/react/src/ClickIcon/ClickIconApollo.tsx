c287e488554bd3e759c4614bdae7406b8c8607b2
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:16:40 +0200

    feat(apollo,look&feel): clickicon sass to css

--- client/apollo/react/src/ClickIcon/ClickIconApollo.tsx
+++ client/apollo/react/src/ClickIcon/ClickIconApollo.tsx
@@ -1,3 +1,3 @@
-import "@axa-fr/design-system-apollo-css/dist/ClickIcon/ClickIconApollo.scss";
+import "@axa-fr/design-system-apollo-css/dist/ClickIcon/ClickIconApollo.css";
 
 export { ClickIcon } from "./ClickIconCommon";
