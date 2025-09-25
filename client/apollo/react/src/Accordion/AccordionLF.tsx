665eb999fa91a3d10b99493dda2aa53078a2ab8c
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:04:26 +0200

    feat(apollo,look&feel): accordion sass to css

--- client/apollo/react/src/Accordion/AccordionLF.tsx
+++ client/apollo/react/src/Accordion/AccordionLF.tsx
@@ -2,7 +2,7 @@ import { AccordionCore } from "../AccordionCore/AccordionCoreLF";
 import { AccordionTagDateContainer } from "./AccordionTagDateContainer/AccordionTagDateContainerLF";
 import { AccordionCommon, type AccordionProps } from "./AccordionCommon";
 import { Icon } from "../Icon/IconCommon";
-import "@axa-fr/design-system-apollo-css/dist/Accordion/AccordionLF.scss";
+import "@axa-fr/design-system-apollo-css/dist/Accordion/AccordionLF.css";
 
 export const Accordion = (props: AccordionProps) => (
   <AccordionCommon
