c5813204557741441c32ad4edb7d87c434147ce5
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:50:12 +0200

    feat(apollo,look&feel): footer sass to css

--- client/apollo/react/src/Layout/Footer/FooterLF.tsx
+++ client/apollo/react/src/Layout/Footer/FooterLF.tsx
@@ -1,3 +1,3 @@
-import "@axa-fr/design-system-apollo-css/dist/Layout/Footer/FooterLF.scss";
+import "@axa-fr/design-system-apollo-css/dist/Layout/Footer/FooterLF.css";
 
 export { type FooterProps, Footer } from "./FooterCommon";
