1f093a77fc54aac6659fdb2f16ac4c412093b300
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:38:29 +0200

    feat(apollo,look&feel): itemmessage sass to css

--- client/apollo/react/src/Form/ItemMessage/ItemMessageLF.tsx
+++ client/apollo/react/src/Form/ItemMessage/ItemMessageLF.tsx
@@ -1,4 +1,4 @@
-import "@axa-fr/design-system-apollo-css/dist/Form/ItemMessage/ItemMessageLF.scss";
+import "@axa-fr/design-system-apollo-css/dist/Form/ItemMessage/ItemMessageLF.css";
 
 export {
   ItemMessage,
