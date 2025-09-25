d3d94142ef07470d35e11351258c6be8ef85ffb0
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 17:00:29 +0200

    feat(apollo,look&feel): tag sass to css

--- client/apollo/react/src/Tag/TagLF.ts
+++ client/apollo/react/src/Tag/TagLF.ts
@@ -1,3 +1,3 @@
-import "@axa-fr/design-system-apollo-css/dist/Tag/TagLF.scss";
+import "@axa-fr/design-system-apollo-css/dist/Tag/TagLF.css";
 
 export { Tag, type TagVariants, tagVariants } from "./TagCommon";
