1e586e8e8431885a73a228304a99fe30879e8fad
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:39:39 +0200

    feat(apollo,look&feel): radio sass to css

--- client/apollo/react/src/Form/Radio/Radio/RadioApollo.tsx
+++ client/apollo/react/src/Form/Radio/Radio/RadioApollo.tsx
@@ -1,3 +1,3 @@
-import "@axa-fr/design-system-apollo-css/dist/Form/Radio/Radio/RadioApollo.scss";
+import "@axa-fr/design-system-apollo-css/dist/Form/Radio/Radio/RadioApollo.css";
 
 export { Radio } from "./RadioCommon";
