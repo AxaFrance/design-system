d531ca4da92bfbd4f539b3bedfeb2e7f5ccfb86f
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:43:04 +0200

    feat(apollo,look&feel): cardradio sass to css

--- client/apollo/react/src/Form/Radio/CardRadio/CardRadioLF.tsx
+++ client/apollo/react/src/Form/Radio/CardRadio/CardRadioLF.tsx
@@ -2,16 +2,12 @@ import { CardRadioOption } from "../CardRadioOption/CardRadioOptionLF";
 import { CardRadioCommon, type CardRadioProps } from "./CardRadioCommon";
 import { ItemMessage } from "../../ItemMessage/ItemMessageLF";
 
-import "@axa-fr/design-system-apollo-css/dist/Form/Radio/CardRadio/CardRadioLF.scss";
+import "@axa-fr/design-system-apollo-css/dist/Form/Radio/CardRadio/CardRadioLF.css";
 
-const CardRadio = (props: CardRadioProps) => (
+export const CardRadio = (props: CardRadioProps) => (
   <CardRadioCommon
     {...props}
     CardRadioOptionComponent={CardRadioOption}
     ItemMessageComponent={ItemMessage}
   />
 );
-
-CardRadio.displayName = "CardRadio";
-
-export { CardRadio };
