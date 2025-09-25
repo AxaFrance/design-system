78f9d5322719b222f1edfea604a53882dc8cd240
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-09-25 16:51:03 +0200

    feat(apollo,look&feel): link sass to css

--- client/apollo/react/src/Link/LinkApollo.tsx
+++ client/apollo/react/src/Link/LinkApollo.tsx
@@ -1,3 +1,3 @@
-import "@axa-fr/design-system-apollo-css/dist/Link/LinkApollo.scss";
+import "@axa-fr/design-system-apollo-css/dist/Link/LinkApollo.css";
 
 export { Link, type LinkVariants, linkVariants } from "./LinkCommon";
