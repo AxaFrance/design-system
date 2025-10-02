20f301d2fd4ce06260e030e9332ee628fd8398d5
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-10-02 08:53:22 +0000

    deploy: b4f3b0a346f97a0d6603356c9ecead44e65fff73

---
+++ apollo/react/next/assets/Stepper-CBPc_2D-.js
@@ -0,0 +1,10 @@
+import{j as t}from"./index-C1mcBe1y.js";import{useMDXComponents as p}from"./index-G7nTXt62.js";import"./index-DoFpg0bu.js";import{S as s,P as r}from"./Stepper.stories-tOc-wiba.js";import{M as i,C as m,a}from"./index-Br0jcalD.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BOKuxaB8.js";import"./index-DbaA8R3S.js";import"./preview-Cyagx4rY.js";import"./iframe-39V0icfA.js";import"./DocsRenderer-CFRXHY34-DVJdLoPl.js";import"./react-18-Cg43Tkb0.js";import"./TimelineVerticalApollo-zxuHVNSp.js";import"./ProgressBarGroupApollo-CKWEeeKc.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function n(o){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...p(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:s,name:"Stepper"}),`
+`,t.jsx(e.h1,{id:"stepper",children:"Stepper"}),`
+`,t.jsx(e.p,{children:"To use the header import it like that:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`import { Stepper } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <Stepper />;
+`})}),`
+`,t.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,t.jsx(m,{of:r}),`
+`,t.jsx(a,{of:r})]})}function _(o={}){const{wrapper:e}={...p(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(n,{...o})}):n(o)}export{_ as default};
--- apollo/react/next/assets/index-C1IL27wN.js
+++
@@ -1,1 +0,0 @@
-import"./preview-BfIw4Seb.js";import"./DocsRenderer-CFRXHY34-0GQR4t5n.js";import"./index-ChXLXVqy.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
