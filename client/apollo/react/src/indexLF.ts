f41fc2df06be2cbe45db86819ca1d364d88234c1
samuel-gomez-axa <samuel.gomez@axa.fr>
2025-10-22 10:48:23 +0200

    feat(apollo,look&feel): inputdatetext

--- client/apollo/react/src/indexLF.ts
+++ client/apollo/react/src/indexLF.ts
@@ -35,6 +35,7 @@ export {
   InputDate as DateInput,
   InputDate,
 } from "./Form/InputDate/InputDateLF";
+export { InputDateText } from "./Form/InputDateText/InputDateTextApollo";
 export { type OptionType } from "./Form/InputPhone/InputPhone.types";
 export { InputPhone } from "./Form/InputPhone/InputPhoneLF";
 export {
