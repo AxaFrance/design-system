40b94d4b02b2bbbd770da756b8d392fec4ffaba0
pplancq <pplancq@users.noreply.github.com>
2025-10-27 15:05:09 +0000

    deploy: 475526528aafe2f301dc8e3880232b3afaa2689a

---
+++ apollo/react/next/assets/Footer-BcmwpsJZ.js
@@ -0,0 +1,17 @@
+import{j as o}from"./index-C-iqcyhV.js";import{useMDXComponents as i}from"./index-DSfWgH82.js";import"./index-CfHFBacd.js";import{F as s,P as r}from"./Footer.stories-PxSaOZx2.js";import{M as a,C as m,a as p}from"./index-DjuYO-gl.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-Bz_unJnr.js";import"./iframe-BaeuaW4V.js";import"./DocsRenderer-CFRXHY34-DY4PpwkD.js";import"./react-18-C_RNIbSA.js";import"./FileUploadApollo-Bdzm6gb6.js";import"./constants-DIJr0UP1.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function e(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:s,name:"Footer"}),`
+`,o.jsx(n.h1,{id:"footer",children:"Footer"}),`
+`,o.jsx(n.p,{children:"To use the Footer import it like that:"}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-tsx",children:`import { Footer } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <Footer
+    links={links}
+    socialMedias={socialMedias}
+    copyright={copyright}
+    expandLinkText={expandLinkText}
+  />
+);
+`})}),`
+`,o.jsx(n.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(m,{of:r}),`
+`,o.jsx(p,{of:r})]})}function P(t={}){const{wrapper:n}={...i(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{P as default};
--- apollo/react/next/assets/index-CjpvKKvG.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CcknuQGN.js";import"./DocsRenderer-CFRXHY34-Dth6xTCX.js";import"./index-BsnfJP7n.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
