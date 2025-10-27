f40c8f5d5bf063e2be257bf1195733e0d222faf0
pplancq <pplancq@users.noreply.github.com>
2025-10-27 15:05:29 +0000

    deploy: 475526528aafe2f301dc8e3880232b3afaa2689a

---
+++ look-and-feel/react/next/assets/Divider.stories-C18HyS2C.js
@@ -0,0 +1,18 @@
+import{j as r}from"./index-C-iqcyhV.js";import"./constants-CAfflvme.js";import"./FileUploadCommon-BZUq6j8J.js";import"./index-ZnYipkzV.js";import"./FileUploadLF-CAVpz7bd.js";import{D as a}from"./DividerCommon-CpcX2P1r.js";const i={title:"Components/Divider",component:a},e={name:"Divider",render:t=>r.jsxs("div",{children:[r.jsx("span",{children:"Hello"}),r.jsx(a,{...t})," ",r.jsx("span",{children:"world!"})]}),args:{classModifier:" "},argTypes:{classModifier:{control:{type:"text"},defaultValue:" "}}};var s,n,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: "Divider",
+  render: args => <div>
+      <span>Hello</span>
+      <Divider {...args} /> <span>world!</span>
+    </div>,
+  args: {
+    classModifier: " "
+  },
+  argTypes: {
+    classModifier: {
+      control: {
+        type: "text"
+      },
+      defaultValue: " "
+    }
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const d=["Default"],v=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:d,default:i},Symbol.toStringTag,{value:"Module"}));export{v as D,e as a};
--- look-and-feel/react/next/assets/index-sXUNX-3N.js
+++
@@ -1,1 +0,0 @@
-import"./preview-DZqRzWI6.js";import"./DocsRenderer-CFRXHY34-BHVKN9Rj.js";import"./index-Bj3Weq7q.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
