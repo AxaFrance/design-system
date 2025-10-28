917bbf361fd06943da607641d42405076422a6ed
MartinWeb <MartinWeb@users.noreply.github.com>
2025-10-28 13:54:36 +0000

    deploy: add75b150105dd886c639468646c92fe0d798a7b

---
+++ slash/react/2.0.4/assets/MultiSelect-DwcftXA4.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-DSfWgH82.js";import"./index-zmSwC08q.js";import{M as m,a as n,b as s,c as l}from"./MultiSelect.stories-o4augVtF.js";import{M as p,C as i,a as r}from"./index-aq7iiRo6.js";import"./index-ZnYipkzV.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-BT_T0KwI.js";import"./iframe-Bhtwm46B.js";import"./DocsRenderer-CFRXHY34-amxkM8JA.js";import"./react-18-C_RNIbSA.js";import"./index-CLcWwD1i.js";/* empty css               */import"./CardData-DMLm9pte.js";import"./index-63p0MazF.js";import"./toConsumableArray-BWYAuU_T.js";import"./Popover-BP1-pUYY.js";import"./index-C3XZdkN-.js";import"./index-VGPHIPc6.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function c(o){const t={code:"code",h1:"h1",h2:"h2",p:"p",...a(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:m}),`
+`,e.jsx(t.h1,{id:"multiselect",children:"MultiSelect"}),`
+`,e.jsx(t.p,{children:"The MultiSelect component allows users to select multiple or one option(s) from a drop-down list."}),`
+`,e.jsx(i,{of:n}),`
+`,e.jsx(r,{of:n}),`
+`,e.jsx(t.h1,{id:"multiselect-with-one-value",children:"MultiSelect with one value"}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(r,{of:s}),`
+`,e.jsx(t.h1,{id:"multiselectinput",children:"MultiSelectInput"}),`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(t.h2,{id:"required",children:"Required"}),`
+`,e.jsxs(t.p,{children:["The component can be required. In that case, the label will be followed by a red asterisk. In order to make the component required, set the ",e.jsx(t.code,{children:"required"})," prop to ",e.jsx(t.code,{children:"true"}),"."]}),`
+`,e.jsxs(t.p,{children:["Alternatively you can to add to the ",e.jsx(t.code,{children:"classModifier"})," the value ",e.jsx(t.code,{children:"required"}),". This behaviour exists to keep backward compatibility but might disapear in the future."]})]})}function _(o={}){const{wrapper:t}={...a(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(c,{...o})}):c(o)}export{_ as default};
---
+++ slash/react/2.0.4/assets/preview-Br1Jkwlv.js
@@ -0,0 +1,1 @@
+/* empty css               */const t={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},options:{storySort:{order:["Fondations",["Icons"],"Components"]}}},tags:["autodocs"]};export{t as default};
