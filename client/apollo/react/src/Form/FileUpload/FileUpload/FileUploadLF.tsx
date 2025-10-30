2a9718905cccf9a13acd44516746f50fb009b4a4
Paul PLANCQ <paul.plancq@outlook.fr>
2025-10-30 16:26:37 +0100

    fix(look&feel,apollo): rename FileUploadCommon.css to FileUploadAll.css and update imports

--- client/apollo/react/src/Form/FileUpload/FileUpload/FileUploadLF.tsx
+++ client/apollo/react/src/Form/FileUpload/FileUpload/FileUploadLF.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 import { InputFile } from "../InputFile/InputFileLF";
 import { FileUploadCommon, type FileUploadProps } from "./FileUploadCommon";
 
-import "@axa-fr/design-system-apollo-css/dist/Form/FileUpload/FileUpload/FileUploadLF.css";
+import "@axa-fr/design-system-apollo-css/dist/Form/FileUpload/FileUpload/FileUploadAll.css";
 
 export const FileUpload = forwardRef<HTMLInputElement, FileUploadProps>(
   (props, ref) => (
