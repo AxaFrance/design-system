e9d98bb5f76d69e3b0f8c7bc5f335e84357703a3
pplancq <pplancq@users.noreply.github.com>
2025-10-30 10:51:01 +0000

    deploy: c21ce8e217f0c2cbd9d9e48cac29d9f49ac68f35

---
+++ apollo/react/next/assets/ContentItemDuoAction.stories-COxJ3xKD.js
@@ -0,0 +1,17 @@
+import{j as t}from"./index-C-iqcyhV.js";import{b as u}from"./account_balance_wallet-fill-DXqnmHYk.js";import{g as C,o as x,n as r}from"./FileUploadApollo-mKF1Nije.js";import"./index-ZnYipkzV.js";import{C as j}from"./ContentItemMonoApollo-B_Exkb4Y.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";const y=({className:o,state:m,ContentItemMonoComponent:p,contentItemProps:l,ToggleComponent:g,...n})=>{const d=C("af-content-item-duo-action",o);return t.jsxs("div",{className:d,children:[t.jsx(p,{...l}),t.jsx("div",{className:"af-action-container",children:m==="edit"?n.buttons:t.jsx(g,{...n.toggleProps})})]})},c=o=>t.jsx(y,{...o,ContentItemMonoComponent:j,ToggleComponent:x}),T={component:c,title:"Components/ContentItemDuoAction",args:{contentItemProps:{type:"icon",iconProps:{src:u,variant:"primary",hasBackground:!0},title:"Texte principale",subtitle1:"Texte secondaire",subtitle2:"Texte tertiare"},buttons:t.jsxs(t.Fragment,{children:[t.jsx(r,{variant:"ghost",children:"Modifier"}),t.jsx(r,{variant:"ghost",children:"Supprimer"})]})}},e={name:"ContentItemDuoAction",render:o=>t.jsx(c,{...o}),argTypes:{state:{options:["edit","toggle"],control:{type:"select"}},toggleProps:{control:{type:"object"}}}};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: "ContentItemDuoAction",
+  render: (props: ContentItemDuoActionProps) => <ContentItemDuoAction {...props} />,
+  argTypes: {
+    state: {
+      options: ["edit", "toggle"],
+      control: {
+        type: "select"
+      }
+    },
+    toggleProps: {
+      control: {
+        type: "object"
+      }
+    }
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const v=["Playground"];export{e as Playground,v as __namedExportsOrder,T as default};
---
+++ apollo/react/next/assets/index-MkaU8cTn.js
@@ -0,0 +1,1 @@
+import"./preview-CSUX-EE9.js";import"./DocsRenderer-CFRXHY34-B267I5H-.js";import"./index-DFaO7g54.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
