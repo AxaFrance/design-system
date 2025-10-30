fb23884cbd4c2a9f9cf03316a60bb64bd3a36ce7
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2025-10-30 13:31:23 +0000

    deploy: 554b8b46fcc5a017e2e17a7abbafe6a79cc91c79

---
+++ apollo/react/next/assets/InputText-ed9Cy7ow.js
@@ -0,0 +1,23 @@
+import{j as n}from"./index-C-iqcyhV.js";import{useMDXComponents as a}from"./index-DSfWgH82.js";import"./index-Jb2titjp.js";import{I as i,a as o}from"./InputText.stories-Cv9laoLX.js";import{M as m,C as p,a as s}from"./index-MoTHVQrr.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-BVuFsN0Y.js";import"./iframe-CoF-Wk1x.js";import"./DocsRenderer-CFRXHY34-BGIj7-kF.js";import"./react-18-C_RNIbSA.js";import"./TimelineVerticalApollo-RGSpAHlT.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function r(e){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...a(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:i,name:"InputText"}),`
+`,n.jsx(t.h1,{id:"inputtext",children:"InputText"}),`
+`,n.jsx(t.p,{children:"To use the InputText import it like that:"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { InputText } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <>
+    <InputText
+      value="John Doe"
+      label="Label"
+      placeholder="Placeholder"
+      helper="Informations complémentaires"
+      name="name"
+      id="nameid"
+      required
+      buttonLabel="my btn"
+    />
+  </>
+);
+`})}),`
+`,n.jsx(t.h2,{id:"placeholder",children:"Placeholder"}),`
+`,n.jsx(p,{of:o}),`
+`,n.jsx(s,{of:o})]})}function D(e={}){const{wrapper:t}={...a(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(r,{...e})}):r(e)}export{D as default};
--- apollo/react/next/assets/index-MkaU8cTn.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CSUX-EE9.js";import"./DocsRenderer-CFRXHY34-B267I5H-.js";import"./index-DFaO7g54.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
