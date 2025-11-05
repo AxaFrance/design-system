dede0d387d722a73816b033ddc6b38b8965e1236
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-11-05 16:03:20 +0000

    deploy: da0a6d95254e6aeedf782f3a9405f8163c8f17ec

---
+++ apollo/react/3.1.0/assets/TimelineVertical.stories-CC_r2N8W.js
@@ -0,0 +1,8 @@
+import{A as l,v as m}from"./TimelineVerticalApollo-B78RF22T.js";import{j as e}from"./index-C-iqcyhV.js";import"./index-ZnYipkzV.js";const d=({title:s,children:t,tag:i,className:c})=>e.jsxs("section",{className:l("af-timeline-vertical",c),children:[e.jsxs("header",{className:"af-timeline-vertical__header",children:[i,e.jsx("h4",{className:"af-timeline-vertical__title",children:s})]}),!!t&&e.jsx("main",{className:"af-timeline-vertical__description",children:t})]}),u=({tag:s,tagProps:t={},...i})=>e.jsx(d,{tag:e.jsx(m,{...t,children:s}),...i}),p={component:u,title:"Components/TimelineVertical"},a={name:"TimelineVertical",args:{tag:"Étape 1",title:"Envoi de documents via Sécur'AXA",children:"Vous allez recevoir un e-mail d'accès à Sécur'AXA, un espace où transmettre vos documents confidentiels dont vos documents médicaux."}};var r,o,n;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: "TimelineVertical",
+  args: {
+    tag: "Étape 1",
+    title: "Envoi de documents via Sécur'AXA",
+    children: "Vous allez recevoir un e-mail d'accès à Sécur'AXA, un espace où transmettre vos documents confidentiels dont vos documents médicaux."
+  }
+}`,...(n=(o=a.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const v=["Playground"],g=Object.freeze(Object.defineProperty({__proto__:null,Playground:a,__namedExportsOrder:v,default:p},Symbol.toStringTag,{value:"Module"}));export{a as P,g as T};
