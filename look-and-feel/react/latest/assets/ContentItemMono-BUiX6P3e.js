34f0a416d879abdcbf02c4b0f7bdccf99547980f
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-11-05 16:03:42 +0000

    deploy: da0a6d95254e6aeedf782f3a9405f8163c8f17ec

---
+++ look-and-feel/react/latest/assets/ContentItemMono-BUiX6P3e.js
@@ -0,0 +1,1 @@
+import"./TimelineVerticalLF-Cp69eB0d.js";import"./index-C-iqcyhV.js";import"./index-ZnYipkzV.js";import{C as o}from"./ContentItemMonoLF-C386IVOD.js";const e=o;export{e as C};
--- look-and-feel/react/latest/assets/IconBg.stories-DDv7TRy2.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./index-C-iqcyhV.js";import{S as c}from"./constants-CAfflvme.js";import"./FileUploadCommon-BZUq6j8J.js";import"./index-ZnYipkzV.js";import"./FileUploadLF-CAVpz7bd.js";import{I as t}from"./IconBg-P1rinyfQ.js";import{a as i}from"./article-fill-B_-J39Ry.js";const m={title:"Components/IconBg",parameters:{backgrounds:{default:"dark"}},component:t},r={name:"IconBg",render:n=>e.jsx(t,{...n}),args:{children:e.jsx(c,{src:i}),isDisabled:!1,classModifier:"secondary"}};var o,s,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  name: "IconBg",
-  render: args => <IconBg {...args} />,
-  args: {
-    children: <Svg src={article} />,
-    isDisabled: false,
-    classModifier: "secondary"
-  }
-}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const d=["Default"],b=Object.freeze(Object.defineProperty({__proto__:null,Default:r,__namedExportsOrder:d,default:m},Symbol.toStringTag,{value:"Module"}));export{r as D,b as I};
