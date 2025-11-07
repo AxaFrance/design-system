3e2ce577863a8edcec7254fc067f70621647c4f3
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-11-07 14:36:34 +0000

    deploy: dd51c3f4692f36b013c3da046588036293df6534

---
+++ apollo/react/3.1.1/assets/Dropdown-CV3rE0lc.js
@@ -0,0 +1,12 @@
+import{j as o}from"./index-C-iqcyhV.js";import{useMDXComponents as s}from"./index-DSfWgH82.js";import"./index-BFPUn5BH.js";import{D as i,a as r}from"./Dropdown.stories-C7pipyx8.js";import{M as p,C as d,a}from"./index-Dauax1_r.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-dNqVtPzu.js";import"./iframe-lzFuZlUu.js";import"./DocsRenderer-CFRXHY34-yNgxxnJJ.js";import"./react-18-C_RNIbSA.js";import"./TimelineVerticalApollo-BA7cnjgh.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function t(n){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:i,name:"Dropdown"}),`
+`,o.jsx(e.h1,{id:"dropdown",children:"Dropdown"}),`
+`,o.jsx(e.p,{children:"To use Dropdown import it like this:"}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => <Dropdown label="label">
+`})}),`
+`,o.jsx(e.h2,{id:"type",children:"Type"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"type"})," prop to change the type of the select. The available variants are ",o.jsx(e.code,{children:"default"}),", ",o.jsx(e.code,{children:"error"})," and ",o.jsx(e.code,{children:"disabled"}),"."]}),`
+`,o.jsx(e.h2,{id:"dropdown-1",children:"Dropdown"}),`
+`,o.jsx(d,{of:r}),`
+`,o.jsx(a,{of:r})]})}function v(n={}){const{wrapper:e}={...s(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(t,{...n})}):t(n)}export{v as default};
