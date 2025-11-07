d0721d4613b8c1a496b7623b6a1e522ae0b92751
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-11-07 14:37:02 +0000

    deploy: dd51c3f4692f36b013c3da046588036293df6534

---
+++ look-and-feel/react/3.1.1/assets/Divider-CEFlw128.js
@@ -0,0 +1,18 @@
+import{j as n}from"./index-C-iqcyhV.js";import{useMDXComponents as t}from"./index-DSfWgH82.js";import{M as s,C as d,a}from"./index-Fte7kC7M.js";import{D as m,a as i}from"./Divider.stories-DuBjN6aj.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./iframe-CUnmSfOp.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./Skeleton-CBq3yDyS.js";import"./TimelineVerticalLF-Ca0Boe8Y.js";import"./DividerCommon-Dy5HNHKF.js";function o(r){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...t(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:m,name:"Divider"}),`
+`,n.jsx(e.h1,{id:"divider",children:"Divider"}),`
+`,n.jsx(e.h3,{id:"import",children:"Import"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Divider } from "@axa-fr/design-system-look-and-feel-react";
+`})}),`
+`,n.jsx(e.h3,{id:"use",children:"Use"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`export const DividerComponent = () => (
+  <div>
+  <span>Hello</span>
+  <Divider />
+  <span>world!</span>
+  <div>
+);
+`})}),`
+`,n.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(e.h3,{id:"default-divider",children:"Default Divider"}),`
+`,n.jsx(d,{of:i}),`
+`,n.jsx(a,{of:i})]})}function y(r={}){const{wrapper:e}={...t(),...r.components};return e?n.jsx(e,{...r,children:n.jsx(o,{...r})}):o(r)}export{y as default};
---
+++ look-and-feel/react/3.1.1/assets/Toggle-3ip148Db.js
@@ -0,0 +1,17 @@
+import{j as e}from"./index-C-iqcyhV.js";import{useMDXComponents as s}from"./index-DSfWgH82.js";import"./index-DLuDfI8m.js";import{T as i,P as t}from"./Toggle.stories-B_zfz-Al.js";import{M as a,C as c,a as l}from"./index-Fte7kC7M.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-BmdxQkQ_.js";import"./iframe-CUnmSfOp.js";import"./DocsRenderer-CFRXHY34-7L-Fn09-.js";import"./react-18-C_RNIbSA.js";import"./Skeleton-CBq3yDyS.js";import"./TimelineVerticalLF-Ca0Boe8Y.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function r(n){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:i,name:"Toggle"}),`
+`,e.jsx(o.h1,{id:"toggle",children:"Toggle"}),`
+`,e.jsx(o.p,{children:"To use the toggle import it like that:"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-tsx",children:`import { Toggle } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <>
+    <Toggle checked={checked} disabled={disabled} onChange={onChange} />
+    <Toggle ref={checkboxRef} />
+    <Toggle checked disabled />
+  </>
+);
+`})}),`
+`,e.jsx(o.h2,{id:"playground",children:"Playground"}),`
+`,e.jsx(c,{of:t}),`
+`,e.jsx(l,{of:t}),`
+`,e.jsxs(o.p,{children:["The toggle accepts all input props except for the ",e.jsx(o.code,{children:"type"})," and ",e.jsx(o.code,{children:"style"}),"."]})]})}function P(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(r,{...n})}):r(n)}export{P as default};
