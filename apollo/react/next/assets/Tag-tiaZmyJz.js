f6a76b1c2924a30a7d988b6ecb6b236aad9f4f37
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-11-17 08:46:24 +0000

    deploy: 8130372ba7cf6207e7a91acfa4a9d50b161f9dcd

---
+++ apollo/react/next/assets/Tag-tiaZmyJz.js
@@ -0,0 +1,19 @@
+import{j as n}from"./index-C-iqcyhV.js";import{useMDXComponents as s}from"./index-DSfWgH82.js";import"./index-35FhT6r3.js";import{T as i,P as o,A as l}from"./Tag.stories-CvOTtPFs.js";import{M as m,C as r,a as p}from"./index-C9Nf6Qmn.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-pWnwifau.js";import"./iframe-CkwXSD9p.js";import"./DocsRenderer-CFRXHY34-QxOJrPQA.js";import"./react-18-C_RNIbSA.js";import"./ItemPaginationApollo-CrUGJoxV.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function e(a){const t={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...a.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:i,name:"Tag"}),`
+`,n.jsx(t.h1,{id:"tag",children:"Tag"}),`
+`,n.jsx(t.p,{children:"To use the tag import it like that:"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { Tag } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <>
+    <Tag variant="info">My tag</Tag>
+    <Tag variant="success">My tag</Tag>
+  </>
+);
+`})}),`
+`,n.jsx(t.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(r,{of:o}),`
+`,n.jsx(p,{of:o}),`
+`,n.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,n.jsxs(t.p,{children:["Use the ",n.jsx(t.code,{children:"variant"})," prop to change the look of the tag. ",n.jsx(t.a,{href:"#all-variants",children:"See the examples"}),"."]}),`
+`,n.jsx(t.h2,{id:"all-variants",children:"All variants"}),`
+`,n.jsx(r,{of:l,layout:"fullscreen"})]})}function P(a={}){const{wrapper:t}={...s(),...a.components};return t?n.jsx(t,{...a,children:n.jsx(e,{...a})}):e(a)}export{P as default};
--- apollo/react/next/assets/index-BI2FyT1O.js
+++
@@ -1,1 +0,0 @@
-import"./preview-ShRTlrBq.js";import"./DocsRenderer-CFRXHY34-BKbgY4-_.js";import"./index-CUtspvLX.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
