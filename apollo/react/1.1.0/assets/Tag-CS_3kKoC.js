417a9138ebca9903d3ce7a70bda3a8c8fd8bc36e
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-12-01 15:57:02 +0000

    deploy: 6417f8b72acc6017c3a941afb0800d235329e940

---
+++ apollo/react/1.1.0/assets/Tag-CS_3kKoC.js
@@ -0,0 +1,19 @@
+import{j as n}from"./index-C-iqcyhV.js";import{useMDXComponents as s}from"./index-DSfWgH82.js";import"./index-CXALH1tg.js";import{T as i,P as o,A as l}from"./Tag.stories-zdKidcOm.js";import{M as m,C as r,a as p}from"./index-DgXJf4Ff.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-C2HZR10c.js";import"./iframe-CL269BJU.js";import"./DocsRenderer-CFRXHY34-CMsVw-4m.js";import"./react-18-C_RNIbSA.js";import"./ItemPaginationApollo-BfQktbo0.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function e(a){const t={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...a.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:i,name:"Tag"}),`
+`,n.jsx(t.h1,{id:"tag",children:"Tag"}),`
+`,n.jsx(t.p,{children:"To use the tag import it like that:"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { Tag } from "@axa-fr/design-system-apollo-react";
+
+const MyComponent = () => (
+  <>
+    <Tag variant="info">My tag</Tag>
+    <Tag variant="success">My tag</Tag>
+  </>
+);
+`})}),`
+`,n.jsx(t.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(r,{of:o}),`
+`,n.jsx(p,{of:o}),`
+`,n.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,n.jsxs(t.p,{children:["Use the ",n.jsx(t.code,{children:"variant"})," prop to change the look of the tag. ",n.jsx(t.a,{href:"#all-variants",children:"See the examples"}),"."]}),`
+`,n.jsx(t.h2,{id:"all-variants",children:"All variants"}),`
+`,n.jsx(r,{of:l,layout:"fullscreen"})]})}function P(a={}){const{wrapper:t}={...s(),...a.components};return t?n.jsx(t,{...a,children:n.jsx(e,{...a})}):e(a)}export{P as default};
