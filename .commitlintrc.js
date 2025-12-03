03f14c4b8f6e2f589f57666d20c7311c62ef6a90
Samuel Gomez <samuel.gomez@axa.fr>
2025-12-03 15:21:01 +0100

    ci(canopee): fix scope for distributeur

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -12,7 +12,7 @@ const config = {
         'canopee',
         'prospect',
         'client',
-        'distributeurs',
+        'distributeur',
         'look&feel',
         'slash',
         'apollo',
