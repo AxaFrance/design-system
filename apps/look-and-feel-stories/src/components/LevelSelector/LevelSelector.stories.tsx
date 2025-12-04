5be8f9e528b52a236574e4fb8fb0caea226fc75c
SOW Mouhamadou moustapha <b469bb@login.axa>
2025-12-04 17:58:50 +0100

    feat(client,prospect): add level selector

---
+++ apps/look-and-feel-stories/src/components/LevelSelector/LevelSelector.stories.tsx
@@ -0,0 +1,30 @@
+import { LevelSelector } from "@axa-fr/canopee-react/client";
+import { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+
+const meta: Meta<typeof LevelSelector> = {
+  title: "Components/LevelSelector",
+  component: LevelSelector,
+};
+export default meta;
+
+export const Default: StoryObj<typeof LevelSelector> = {
+  render: ({ ...args }) => {
+    const [value, setValue] = useState(args.value || 0);
+    return (
+      <div style={{ maxWidth: 500 }}>
+        <LevelSelector {...args} value={value} onChange={setValue} />
+      </div>
+    );
+  },
+  args: {
+    stepsCount: 3,
+    title: "Titre du niveau actif",
+    description:
+      "Texte secondaire dynamique sur le niveau actif avec un contenu plus ou moins long selon le besoin",
+    value: 0,
+  },
+  argTypes: {
+    onChange: { action: "onClick" },
+  },
+};
