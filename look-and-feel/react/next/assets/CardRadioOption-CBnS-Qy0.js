8b1325b684f0276f55cdd842be499901cc6a174c
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-12-05 15:07:58 +0000

    deploy: dce6090e2d476f74b9181766e7688360b044cdc7

---
+++ look-and-feel/react/next/assets/CardRadioOption-CBnS-Qy0.js
@@ -0,0 +1,20 @@
+import{j as o}from"./index-C-iqcyhV.js";import{useMDXComponents as e}from"./index-DSfWgH82.js";import{M as a,C as s,a as p}from"./index-SOtCOitk.js";import{C as d,a as r}from"./CardRadioOption.stories-CMrGxa3G.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./iframe-BdcKQRGs.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./CardLF-DLd5PwB1.js";import"./CardRadioOptionLF-BbHl4WTv.js";import"./home-BuLaEOyh.js";function i(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...e(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:d,name:"CardRadioOption"}),`
+`,o.jsx(n.h1,{id:"cardradiooption-components",children:"CardRadioOption components"}),`
+`,o.jsx(n.h2,{id:"how-to-use",children:"How to use"}),`
+`,o.jsxs(n.p,{children:["To use the ",o.jsx(n.code,{children:"CardRadioOption"}),"component import it like this:"]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-tsx",children:`import { CardRadioOption } from "@axa-fr/canopee-react/client";
+
+const MyComponent = () => (
+  <CardRadioOption
+    label="Titre"
+    description="Sous-titre 1"
+    subtitle="Sous-titre 2"
+    icon={icon}
+    name="foo"
+    value="bar"
+  />
+);
+`})}),`
+`,o.jsx(n.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(s,{of:r}),`
+`,o.jsx(p,{of:r})]})}function y(t={}){const{wrapper:n}={...e(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(i,{...t})}):i(t)}export{y as default};
--- look-and-feel/react/next/assets/index-C6JVdyPu.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Df7beY1J.js";import"./DocsRenderer-CFRXHY34-C3_Phk0H.js";import"./index-mCh8l6kB.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
