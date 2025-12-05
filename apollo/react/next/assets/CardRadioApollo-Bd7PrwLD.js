6b3525ca31f43adcd3de0c0f04c468af813c4cf2
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-12-05 15:07:42 +0000

    deploy: dce6090e2d476f74b9181766e7688360b044cdc7

--- apollo/react/next/assets/CardRadioApollo-Bd7PrwLD.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-C-iqcyhV.js";import{C as m}from"./CardRadioOptionApollo-NpSPLUqy.js";import{b as s,I as t}from"./ItemPaginationApollo-chh4BP9L.js";const n=o=>a.jsx(s,{...o,CardRadioOptionComponent:m,ItemMessageComponent:t});export{n as C};
---
+++ apollo/react/next/assets/Link-Bywt_DuP.js
@@ -0,0 +1,15 @@
+import{j as n}from"./index-C-iqcyhV.js";import{useMDXComponents as i}from"./index-DSfWgH82.js";import"./index-BW6rCX70.js";import{L as s,D as e}from"./Link.stories-CKTN5GAj.js";import{M as a,C as m,a as p}from"./index-B3EmjVUe.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-C8HS5ZSa.js";import"./iframe-CuuRAuCO.js";import"./DocsRenderer-CFRXHY34-Dcronj-F.js";import"./react-18-C_RNIbSA.js";import"./CardApollo-Ct2KtOfb.js";import"./LinkCommon-Cwq4IalP.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function t(r){const o={code:"code",h2:"h2",h3:"h3",pre:"pre",...i(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:s,name:"Link"}),`
+`,n.jsx(o.h2,{id:"link",children:"Link"}),`
+`,n.jsx(o.h3,{id:"import",children:"Import"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-tsx",children:`import { Link } from "@axa-fr/canopee-react/prospect";
+`})}),`
+`,n.jsx(o.h3,{id:"use",children:"Use"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-tsx",children:`export const LinkComponent = () => (\r
+  <Link openInNewTab href="https://fakelink.com">\r
+    Plus de détails\r
+  </Link>\r
+);
+`})}),`
+`,n.jsx(o.h2,{id:"playground",children:"Playground"}),`
+`,n.jsx(m,{of:e}),`
+`,n.jsx(p,{of:e})]})}function X(r={}){const{wrapper:o}={...i(),...r.components};return o?n.jsx(o,{...r,children:n.jsx(t,{...r})}):t(r)}export{X as default};
