fedb0e5cbf7a647a92e6d710830fbe5bbffb82c7
Samuel Gomez <samuel.gomez@axa.fr>
2025-12-10 15:55:44 +0100

    feat(client,prospect): style for ol

--- packages/canopee-react/src/prospect-client/List/List/ListCommon.tsx
+++ packages/canopee-react/src/prospect-client/List/List/ListCommon.tsx
@@ -5,7 +5,7 @@ import {
 } from "../../Card/CardCommon";
 import { generateId } from "../../utilities/generateId";
 
-export type ListProps = CardCommonProps<"ul">;
+export type ListProps = CardCommonProps<"ul" | "ol">;
 
 type ListPropsCommon = ListProps & {
   CardComponent: ComponentType<ComponentProps<typeof Card>>;
@@ -14,9 +14,10 @@ type ListPropsCommon = ListProps & {
 export const ListCommon = ({
   children,
   CardComponent,
+  as = "ul",
   ...listProps
 }: ListPropsCommon) => (
-  <CardComponent as="ul" {...listProps}>
+  <CardComponent as={as} {...listProps}>
     {Children.toArray(children).map((child) => (
       <li key={generateId(child as object)}>{child}</li>
     ))}
