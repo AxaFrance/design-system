3cca8ce3d2cbc37c6a2bcf004acdf14a710df8a6
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2025-12-15 15:41:58 +0000

    deploy: 2336de5a87c5c0504ef862418e18e78c8fadfdb7

---
+++ distributeur/react/1.2.0/assets/preview-CQAHupgg.js
@@ -0,0 +1,1 @@
+const o={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},options:{storySort:{order:["Fondations",["Icons"],"Components"]}}},tags:["autodocs"]};export{o as default};
