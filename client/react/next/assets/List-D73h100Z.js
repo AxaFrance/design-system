7a3db3fc7aff6603bacb578e5a0d3ff7f18657f0
tejb-axa <tejb-axa@users.noreply.github.com>
2026-01-16 06:21:50 +0000

    deploy: 3eee85415f3664ba2303d37f8a7bb168dd77aa41

---
+++ client/react/next/assets/List-D73h100Z.js
@@ -0,0 +1,27 @@
+import{j as t}from"./index-C-iqcyhV.js";import{useMDXComponents as r}from"./index-DSfWgH82.js";import{M as s,C as m,a as p}from"./index-CZ1V4vRy.js";import{L as c,D as e}from"./List.stories-C_0_z-dU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./iframe-BdF4AZ20.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./SkeletonLF-JnM1AauS.js";import"./ContentItemDuoActionLF-Dcn9Wzru.js";import"./ContentItemMonoLF-B3zuh4OD.js";import"./ClickItemLF-Dc593vyH.js";import"./ContentItemDuoLF-Cmm6tHU3.js";import"./ListLF-DhB7adOl.js";import"./CardCommon-BiEUqzEs.js";function i(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c,name:"List"}),`
+`,t.jsx(n.h1,{id:"list",children:"List"}),`
+`,t.jsx(n.h2,{id:"how-to-use",children:"How to use"}),`
+`,t.jsx(n.p,{children:"To use the List component import it like that:"}),`
+`,t.jsx(n.pre,{children:t.jsx(n.code,{className:"language-tsx",children:`import {
+  ClickItem,
+  ContentItemMono,
+  List,
+  Svg,
+} from "@axa-fr/canopee-react/client";
+import trashIcon from "@material-symbols/svg-400/outlined/delete.svg";
+import publishedWithChangesIcon from "@material-symbols/svg-400/outlined/published_with_changes-fill.svg";
+
+const MyComponent = () => (
+  <List>
+    <ContentItemMono secondaryText="nom.prénom@mail.fr">
+      Prénom NOM
+    </ContentItemMono>
+    <ClickItem icon={<Svg src={publishedWithChangesIcon} />}>
+      Modifier le profil
+    </ClickItem>
+    <ClickItem icon={<Svg src={trashIcon} />}>Supprimer le profil</ClickItem>
+  </List>
+);
+`})}),`
+`,t.jsx(m,{of:e}),`
+`,t.jsx(p,{of:e})]})}function D(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{D as default};
---
+++ client/react/next/assets/index-BjRVE5n_.js
@@ -0,0 +1,1 @@
+import"./preview-Bvoxwrkt.js";import"./DocsRenderer-CFRXHY34-B4hKlizJ.js";import"./index-CZ1V4vRy.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
