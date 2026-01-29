220cb69f4c524c83de97fd4f5b14c43bfca6ec3e
MartinWeb <MartinWeb@users.noreply.github.com>
2026-01-29 13:08:54 +0000

    deploy: ba47446982ef36a4ada0f678878f989f1adc91ee

---
+++ client/react/1.3.0/assets/ContentItemMono-DBQ_WKgk.js
@@ -0,0 +1,10 @@
+import{j as o}from"./index-C-iqcyhV.js";import{useMDXComponents as m}from"./index-DSfWgH82.js";import"./index-BHspAZp2.js";import{C as i,P as e}from"./ContentItemMono.stories-dLo0XyG3.js";import{M as s,C as p,a}from"./index-B5hhUGR6.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CLcWwD1i.js";import"./index-ZnYipkzV.js";import"./preview-Iep-Q75W.js";import"./iframe-CxjWHBUx.js";import"./DocsRenderer-CFRXHY34-BsaRBeX6.js";import"./react-18-C_RNIbSA.js";import"./account_balance_wallet-fill-DXqnmHYk.js";import"./FieldsetLF-CT0NIl8m.js";import"./ContentItemMonoLF-CkuXK1T8.js";import"./ContentItemMonoCore-BXFukymD.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function r(n){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:i,name:"Content Item Mono"}),`
+`,o.jsx(t.h1,{id:"contentitemmono",children:"ContentItemMono"}),`
+`,o.jsx(t.p,{children:"To use the header import it like that:"}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-tsx",children:`import { ContentItemMono } from "@axa-fr/canopee-react/client";
+
+const MyComponent = () => <ContentItemMono />;
+`})}),`
+`,o.jsx(t.h2,{id:"playground",children:"Playground"}),`
+`,o.jsx(p,{of:e}),`
+`,o.jsx(a,{of:e})]})}function v(n={}){const{wrapper:t}={...m(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(r,{...n})}):r(n)}export{v as default};
