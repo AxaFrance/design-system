1ada9365ce38845c16bdc426d56741100f414637
MartinWeb <MartinWeb@users.noreply.github.com>
2026-01-29 13:07:54 +0000

    deploy: ba47446982ef36a4ada0f678878f989f1adc91ee

---
+++ distributeur/react/latest/assets/MenuTitleWrapper-551w6b7s.js
@@ -0,0 +1,137 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-DSfWgH82.js";import"./index-W5lOLZQP.js";import{M as i,D as t}from"./MenuTitleWrapper.stories-DgzHeIjs.js";import{M as l,C as o,a as m}from"./index-D5nSs15M.js";import"./index-ZnYipkzV.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-BB9upFFP.js";import"./iframe-C0OJtToi.js";import"./DocsRenderer-CFRXHY34-DZB6gQIv.js";import"./react-18-C_RNIbSA.js";import"./index-CLcWwD1i.js";import"./CardData-BIJbskTL.js";import"./index-63p0MazF.js";import"./HeaderTitle-a7_S5cBL.js";import"./NavBar-D_lkLGef.js";import"./NavBarItem-DLMPV8q1.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function r(e){const a={code:"code",h1:"h1",h2:"h2",pre:"pre",...s(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(l,{of:i}),`
+`,n.jsx(a.h1,{id:"menu-title-wrapper",children:"Menu Title Wrapper"}),`
+`,n.jsx(a.h2,{id:"import",children:"Import"}),`
+`,n.jsx(a.pre,{children:n.jsx(a.code,{className:"language-tsx",children:`import {
+  MenuTitleWrapper,
+  NavBarItem,
+} from "@axa-fr/canopee-react/distributeur";
+`})}),`
+`,n.jsx(a.h2,{id:"use",children:"Use"}),`
+`,n.jsx(a.pre,{children:n.jsx(a.code,{className:"language-tsx",children:`const MenuTitleWrapperComponent = () => (
+  <MenuTitleWrapper
+    menuVisible={false}
+    title="Toolkit Axa"
+    subtitle="Info complémentaire"
+  >
+    <NavBarItem
+      actionElt={
+        <a className="af-nav__link" href="https://axa.fr">
+          Home
+        </a>
+      }
+    />
+
+    <NavBarItem
+      actionElt={
+        <a className="af-nav__link" href="https://axa.fr">
+          Form
+        </a>
+      }
+    />
+
+    <NavBarItem
+      actionElt={
+        <a className="af-nav__link" href="https://axa.fr">
+          Fielupload
+        </a>
+      }
+    />
+
+    <NavBarItem
+      className="af-nav__item--haschild af-nav__item"
+      aria-haspopup="true"
+      aria-expanded="false"
+      ariaLabel="Table"
+      actionElt={<span className="af-nav__link">Table</span>}
+    >
+      <NavBarItem
+        key="table-1"
+        actionElt={
+          <a className="af-nav__link" href="https://axa.fr">
+            Sous lien 1
+          </a>
+        }
+      />
+      <NavBarItem
+        key="table-2"
+        actionElt={
+          <a className="af-nav__link" href="https://axa.fr">
+            Sous lien 2
+          </a>
+        }
+      />
+
+      <NavBarItem
+        key="table-3"
+        actionElt={
+          <a className="af-nav__link" href="https://axa.fr">
+            Sous lien 3
+          </a>
+        }
+      />
+    </NavBarItem>
+
+    <NavBarItem
+      actionElt={
+        <a className="af-nav__link" href="https://axa.fr">
+          Accordions
+        </a>
+      }
+    />
+
+    <NavBarItem
+      actionElt={
+        <a className="af-nav__link" href="https://axa.fr">
+          Alert
+        </a>
+      }
+    />
+
+    <NavBarItem
+      actionElt={
+        <a className="af-nav__link" href="https://axa.fr">
+          Validation
+        </a>
+      }
+    />
+
+    <NavBarItem
+      className="af-nav__item--haschild af-nav__item"
+      aria-haspopup="true"
+      aria-expanded="false"
+      ariaLabel="Table"
+      actionElt={<span className="af-nav__link">Doc</span>}
+    >
+      <NavBarItem
+        key="table-1"
+        actionElt={
+          <a className="af-nav__link" href="https://axa.fr">
+            Sous lien 1
+          </a>
+        }
+      />
+      <NavBarItem
+        key="table-2"
+        actionElt={
+          <a className="af-nav__link" href="https://axa.fr">
+            Sous lien 2
+          </a>
+        }
+      />
+
+      <NavBarItem
+        key="table-3"
+        actionElt={
+          <a className="af-nav__link" href="https://axa.fr">
+            Sous lien 3
+          </a>
+        }
+      />
+    </NavBarItem>
+  </MenuTitleWrapper>
+);
+export default MenuTitleWrapper;
+`})}),`
+`,n.jsx(a.h2,{id:"stories",children:"Stories"}),`
+`,n.jsx(o,{of:t}),`
+`,n.jsx(m,{of:t})]})}function y(e={}){const{wrapper:a}={...s(),...e.components};return a?n.jsx(a,{...e,children:n.jsx(r,{...e})}):r(e)}export{y as default};
---
+++ distributeur/react/latest/assets/Slider-MtDPMS6I.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-DSfWgH82.js";import"./index-W5lOLZQP.js";import{S as p,a as t}from"./Slider.stories-5sIU1q9w.js";import{SliderInputStory as r}from"./SliderInput.stories-C8YIjz_o.js";import{M as l,C as i,a as s}from"./index-D5nSs15M.js";import"./index-ZnYipkzV.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-BB9upFFP.js";import"./iframe-C0OJtToi.js";import"./DocsRenderer-CFRXHY34-DZB6gQIv.js";import"./react-18-C_RNIbSA.js";import"./index-CLcWwD1i.js";import"./CardData-BIJbskTL.js";import"./index-63p0MazF.js";import"./Slider-CMZQGlqE.js";import"./toConsumableArray-BWYAuU_T.js";import"./index-VGPHIPc6.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function a(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{of:p}),`
+`,e.jsx(n.h1,{id:"slider",children:"Slider"}),`
+`,e.jsx(n.h2,{id:"default-slider",children:"Default Slider"}),`
+`,e.jsx(n.p,{children:"To use the Slider, import it like that:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Slider } from "@axa-fr/canopee-react/distributeur";
+
+const MyComponent = () => (
+  <Slider
+    id={id}
+    name={name}
+    options={options}
+    onChangeComplete={({ id, name, value }) => {
+      console.log(id, name, value);
+    }}
+  />
+);
+`})}),`
+`,e.jsx(i,{of:t}),`
+`,e.jsx(s,{of:t}),`
+`,e.jsx(n.h2,{id:"sliderinput",children:"SliderInput"}),`
+`,e.jsx(n.p,{children:"To use the SliderInput, import it like that:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { SliderInput } from "@axa-fr/canopee-react/distributeur";
+
+const MyComponent = () => (
+  <SliderInput
+    id={id}
+    name={name}
+    label={label}
+    options={options}
+    onChangeComplete={({ id, name, value }) => {
+      console.log(id, name, value);
+    }}
+    message={message}
+    messageType={MessageTypes.error}
+  />
+);
+`})}),`
+`,e.jsx(i,{of:r}),`
+`,e.jsx(s,{of:r})]})}function _(o={}){const{wrapper:n}={...m(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(a,{...o})}):a(o)}export{_ as default};
