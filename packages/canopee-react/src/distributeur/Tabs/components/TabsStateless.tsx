69abed12a45debbc50dfa41011664e015dc057dc
JLou <jeanlou.pierme@gmail.com>
2026-02-05 15:38:00 +0100

    feat(distributeur): improved design & accessibility of Tabs
    - added keyboard navigation
    - deprecated classModifier in favor of className
    - improved design of Tabs up to specs
    - Tags are now next to text instead of above
    
    fixes #1000

--- packages/canopee-react/src/distributeur/Tabs/components/TabsStateless.tsx
+++ packages/canopee-react/src/distributeur/Tabs/components/TabsStateless.tsx
@@ -1,25 +1,53 @@
 /* eslint-disable react/no-array-index-key */
 import {
   Children,
-  type ReactNode,
   isValidElement,
+  useId,
+  useRef,
+  type FormEvent,
+  type KeyboardEvent,
   type ReactElement,
-  type MouseEvent,
 } from "react";
-import { getComponentClassName } from "../../utilities";
-import { Pane } from "./Pane";
+import { getClassName } from "../../utilities/helpers/getClassName";
 import type { TabProps } from "./Tab";
-import { Title } from "./Title";
 
-export interface TabsStatelessProps {
-  children: ReactNode;
-  activeIndex: string;
+export type TabsStatelessProps = {
+  children: ReactElement<TabProps> | ReactElement<TabProps>[];
+  activeIndex: number;
   className?: string;
+  /**
+   * @deprecated Use `className` instead
+   */
   classModifier?: string;
-}
+};
 
-export interface TabsStatelessHandlers {
-  onChange: (event: MouseEvent<HTMLButtonElement>, index: string) => void;
+export type TabsStatelessHandlers = {
+  onChange: (event: FormEvent<HTMLButtonElement>, index: number) => void;
+};
+
+function handleKeyDown(
+  event: KeyboardEvent<HTMLButtonElement>,
+  moveToTab: (event: KeyboardEvent<HTMLButtonElement>, index: number) => void,
+  index: number,
+  totalNumberOfTabs: number,
+) {
+  let targetIndex: number | null = null;
+
+  if (event.key === "ArrowLeft") {
+    targetIndex = index - 1;
+  } else if (event.key === "ArrowRight") {
+    targetIndex = index + 1;
+  } else if (event.key === "Home") {
+    targetIndex = 0;
+  } else if (event.key === "End") {
+    targetIndex = totalNumberOfTabs - 1;
+  }
+
+  if (targetIndex !== null) {
+    moveToTab(event, targetIndex);
+    event.preventDefault();
+    event.stopPropagation();
+  }
 }
 
 type Props = TabsStatelessProps & TabsStatelessHandlers;
@@ -30,26 +58,64 @@ const TabsStateless = ({
   children,
   onChange,
 }: Props) => {
-  const componentClassName = getComponentClassName(className, classModifier);
+  const componentClassName = getClassName({
+    baseClassName: "af-tabs",
+    className,
+    modifiers: classModifier?.split(" "),
+  });
+
+  const tabRefs = useRef<HTMLButtonElement[]>([]);
+
+  const moveToTab = (
+    event: KeyboardEvent<HTMLButtonElement>,
+    index: number,
+  ) => {
+    if (index < 0 || index >= Children.count(children)) {
+      return;
+    }
+    tabRefs.current[index]?.focus();
+    onChange(event, index);
+  };
+
+  const id = useId();
 
   return (
-    <div className={componentClassName}>
+    <div className={componentClassName} role="tablist">
       <ul className="af-tabs__control">
         {Children.map(
           children,
           (child, index) =>
             isValidElement(child) && (
-              <Title
-                active={activeIndex === index.toString()}
-                onChange={onChange}
-                id={`${index}`}
-                classModifier={
-                  (child as ReactElement<TabProps>).props.classModifier
-                }
-                key={`title-${index}`}
+              <li
+                className={getClassName({
+                  className,
+                  modifiers: [activeIndex === index && "active"],
+                  baseClassName: "af-tabs__item",
+                })}
               >
-                {(child as ReactElement<TabProps>).props.title}
-              </Title>
+                <button
+                  type="button"
+                  id={`${id}-tab-${index}`}
+                  ref={(el) => {
+                    tabRefs.current[index] = el!;
+                  }}
+                  aria-controls={`${id}-pane-${index}`}
+                  onChange={(event) => onChange(event, index)}
+                  onKeyDown={(event) => {
+                    handleKeyDown(
+                      event,
+                      moveToTab,
+                      index,
+                      Children.count(children),
+                    );
+                  }}
+                  className="af-tabs__link"
+                  onClick={(event) => onChange(event, index)}
+                  tabIndex={activeIndex === index ? 0 : -1}
+                >
+                  {child.props.title}
+                </button>
+              </li>
             ),
         )}
       </ul>
@@ -58,12 +124,19 @@ const TabsStateless = ({
           children,
           (child, index) =>
             isValidElement(child) && (
-              <Pane
-                active={activeIndex === index.toString()}
+              <div
+                id={`${id}-pane-${index}`}
+                aria-labelledby={`${id}-tab-${index}`}
+                className={getClassName({
+                  baseClassName: "af-tabs__pane",
+                  modifiers: [activeIndex === index && "active"],
+                })}
+                role="tabpanel"
+                tabIndex={0}
                 key={`pane-${index}`}
               >
-                {(child as ReactElement<TabProps>).props.children}
-              </Pane>
+                {child.props.children}
+              </div>
             ),
         )}
       </div>
