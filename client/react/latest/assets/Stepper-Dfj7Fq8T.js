8f3a0ab9ded7c640d03cea2cf7fd99ec9409c6d9
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-03-10 14:46:39 +0000

    deploy: dadc2c311a6db132b182d6ad4d0646af0865104b

---
+++ client/react/latest/assets/Stepper-Dfj7Fq8T.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as p}from"./index-DSfWgH82.js";import"./index-BNwWK3ub.js";import{S as i,P as r}from"./Stepper.stories-CLCo6dqq.js";import{M as m,C as s,a}from"./index-BIZjljOG.js";import"./index-ZnYipkzV.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preview-S6D5BkA4.js";import"./iframe-DWU-u64B.js";import"./DocsRenderer-CFRXHY34-BIBHOz9G.js";import"./react-18-C_RNIbSA.js";import"./index-CLcWwD1i.js";import"./FormLayout-Dpb6mS7Y.js";import"./index-63p0MazF.js";/* empty css               */import"./ProgressBarGroupLF-D1sP706P.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function n(o){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...p(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:i,name:"Stepper Client"}),`
+`,t.jsx(e.h1,{id:"stepper",children:"Stepper"}),`
+`,t.jsx(e.p,{children:"To use the header import it like that:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`import { Stepper } from "@axa-fr/canopee-react/client";
+
+const MyComponent = () => <Stepper />;
+`})}),`
+`,t.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(a,{of:r})]})}function v(o={}){const{wrapper:e}={...p(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(n,{...o})}):n(o)}export{v as default};
