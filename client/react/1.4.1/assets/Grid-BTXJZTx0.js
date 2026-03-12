48b7804e618c2a1c87efb0857331c8f03b804fd1
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-03-12 13:14:11 +0000

    deploy: be3a898a86a71dccd7a6abb0fb985397725c4a99

---
+++ client/react/1.4.1/assets/Grid-BTXJZTx0.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as n}from"./index-DSfWgH82.js";import{M as s,C as m,a}from"./index-FHZTg1b2.js";import{G as p,W as e}from"./GridWireframe.stories-gW3VqAAy.js";import"./index-ZnYipkzV.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CgtmBpr5.js";import"./index-63p0MazF.js";import"./index-CLcWwD1i.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./FormLayout-DrOZh3Eg.js";/* empty css               */import"./DebugGridLF-D9TOWSTt.js";function i(o){const t={a:"a",h2:"h2",p:"p",...n(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{name:"How to use Grid ?",of:p}),`
+`,r.jsx(t.h2,{id:"grid-system",children:"Grid System"}),`
+`,r.jsxs(t.p,{children:["More info here : ",r.jsx(t.a,{href:"https://axafrance.github.io/design-system/apollo/react/latest?path=/docs/guidelines-grid-system--grid",rel:"nofollow",children:"Grid documentation"})]}),`
+`,r.jsx(t.h2,{id:"wireframe",children:"Wireframe"}),`
+`,r.jsx(m,{of:e}),`
+`,r.jsx(a,{of:e})]})}function W(o={}){const{wrapper:t}={...n(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(i,{...o})}):i(o)}export{W as default};
