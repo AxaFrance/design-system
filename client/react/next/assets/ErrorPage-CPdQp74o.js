0a1d1e245ab8d3bdc104c16768cb71c06c14f61d
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2026-03-18 15:58:44 +0000

    deploy: c07989f6468bae00f6b14231e57307630e639d89

---
+++ client/react/next/assets/ErrorPage-CPdQp74o.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-DSfWgH82.js";import{M as i,C as s,a as p}from"./index-DqR5wUpB.js";import{E as m,P as e}from"./ErrorPage.stories-BWBs5yEK.js";import"./index-ZnYipkzV.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CasEhlXz.js";import"./index-63p0MazF.js";import"./index-CLcWwD1i.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./FormLayout-BQJhE1A1.js";/* empty css               */import"./LinkCommon-DiwMhT0e.js";import"./arrow_left_alt-BzLPLjyo.js";import"./LayoutDemo-DWww-Tjy.js";import"./DebugGridLF-ChsQvpdN.js";import"./FooterCommon-CiH5lwys.js";function t(n){const o={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...a(),...n.components};return r.jsxs(r.Fragment,{children:[r.jsx(i,{of:m,name:"Error Page"}),`
+`,r.jsx(o.h1,{id:"error-page",children:"Error page"}),`
+`,r.jsx(o.h2,{id:"import",children:"Import"}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-tsx",children:`import { ErrorPage } from "@axa-fr/canopee-react/prospect";
+
+export const MyErrorPage = () => (
+  <>
+    <Myheader />
+    <main className="grid">
+      <ErrorPage headingProps={headingProps} linkProps={linkProps}>
+        {children}
+      </ErrorPage>
+    </main>
+    <MyFooter />
+  </>
+);
+`})}),`
+`,r.jsx(o.h2,{id:"uses",children:"Uses"}),`
+`,r.jsx(o.h3,{id:"error-page-playground",children:"Error Page Playground"}),`
+`,r.jsx(s,{of:e}),`
+`,r.jsx(p,{of:e})]})}function N(n={}){const{wrapper:o}={...a(),...n.components};return o?r.jsx(o,{...n,children:r.jsx(t,{...n})}):t(n)}export{N as default};
---
+++ client/react/next/assets/index-Byo1kHc8.js
@@ -0,0 +1,1 @@
+import"./preview-C3I35Suh.js";import"./DocsRenderer-CFRXHY34-CMdIOe1o.js";import"./index-DqR5wUpB.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
