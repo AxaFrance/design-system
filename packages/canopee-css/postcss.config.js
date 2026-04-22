04429be48e7ecd63fbd3fd3e5b75001b1b30ebed
JLou <jeanlou.pierme@gmail.com>
2026-04-22 15:04:07 +0200

    chore(design-system): delete postcss internal package

--- packages/canopee-css/postcss.config.js
+++ packages/canopee-css/postcss.config.js
@@ -1,4 +1,34 @@
-const { cssConfig } = require("@axa-fr/postcss-config-design-system");
+const postcssFlexbugsFixes = require("postcss-flexbugs-fixes");
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssNormalize = require("postcss-normalize");
+const postcssSass = require("@csstools/postcss-sass");
+const postcssGlobalData = require("@csstools/postcss-global-data");
+const postcssImport = require("postcss-import");
+const cssnano = require("cssnano");
+
+const cssConfig = ({ env }, globalFiles) => {
+  const prod = env === "production";
+  return {
+    plugins: [
+      postcssImport(),
+      /* nécessaire pour le remplacement des custom-medias */
+      postcssGlobalData({
+        files: globalFiles,
+      }),
+      postcssPresetEnv({
+        stage: 4,
+        minimumVendorImplementations: 2,
+        features: {
+          "custom-media-queries": true,
+          "custom-selectors": true,
+          "blank-pseudo-class": true,
+          "relative-color-syntax": true,
+        },
+      }),
+      prod && cssnano,
+    ],
+  };
+};
 
 module.exports = (ctx) => {
   const [universe, env] = ctx.env.split("-");
