6a99e8cafb9ff43d860cadef40ce5ff68a8e0822
tbeghin <tbeghin@users.noreply.github.com>
2026-04-23 08:56:09 +0000

    deploy: 428a1c054e61a9c012173773e4dd7a3dc71ad6d4

---
+++ client/react/next/assets/ValidPage-DHCng3Y0.js
@@ -0,0 +1,36 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as a}from"./index-D3yXdDgY.js";import{M as i,C as s,a as p}from"./index-BlNkYhYw.js";import{V as m,P as o}from"./ValidPage.stories-CFD45wjW.js";import"./index-CtOEgLBf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CqrC1R4i.js";import"./index-BjCk13Vd.js";import"./index-xQ9y_n2y.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./LoaderLF-B8djIx1K.js";import"./LoaderCommon-DCg8_8er.js";/* empty css               */import"./DataAgentLF-BoBHtDEz.js";import"./ClickItemLF-BK4Y1Nid.js";import"./ContentItemMonoLF-CrpooQTR.js";import"./ContentItemMonoCore-Bs3Rkqlr.js";import"./DividerCommon-ByVIAfOl.js";import"./LinkCommon-BAqtFG4t.js";import"./check-ByUiVwBv.js";import"./arrow_left_alt-BzLPLjyo.js";import"./LayoutDemo-B_JFT-WM.js";import"./DebugGridLF-B8Pogy0D.js";import"./FooterCommon-B78Yq0ae.js";function t(r){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...a(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:m,name:"Valid Page"}),`
+`,n.jsx(e.h1,{id:"valid-page",children:"Valid page"}),`
+`,n.jsx(e.h2,{id:"import",children:"Import"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { ValidPage } from "@axa-fr/canopee-react/prospect";
+
+export const MyValidPage = () => (
+  <>
+    <Myheader />
+    <main className="grid">
+      <ValidPage
+        headingProps={headingProps}
+        dataAgentProps={dataAgentProps}
+        linkProps={linkProps}
+      >
+        <p>
+          Vous recevrez dans quelques instants un e-mail récapitulatif avec le
+          détail de votre tarif.
+        </p>
+        <p>
+          Votre Conseiller AXA vous contactera rapidement pour confirmer vos
+          informations et finaliser votre contrat.
+        </p>
+        <p>
+          Vous souhaitez prendre les devants ? N&apos;hésitez pas à le contacter
+          directement !
+        </p>
+      </ValidPage>
+    </main>
+    <MyFooter />
+  </>
+);
+`})}),`
+`,n.jsx(e.h2,{id:"uses",children:"Uses"}),`
+`,n.jsx(e.h3,{id:"valid-page-playground",children:"Valid Page Playground"}),`
+`,n.jsx(s,{of:o}),`
+`,n.jsx(p,{of:o})]})}function E(r={}){const{wrapper:e}={...a(),...r.components};return e?n.jsx(e,{...r,children:n.jsx(t,{...r})}):t(r)}export{E as default};
--- client/react/next/assets/index-Cyw7cAoL.js
+++
@@ -1,1 +0,0 @@
-import"./preview-ClpwhfDe.js";import"./DocsRenderer-CFRXHY34-Cf5rY6ED.js";import"./index-D2OXstFl.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
