3484bfe4c7e9e1d1e777bb0cfcca50f2f5be6541
debaerdm <mathieu.debaerdemaeker@gmail.com>
2026-04-23 23:16:58 +0200

    feat(client, prospect): unify Toggle styles with 40px width and check/close icons

--- packages/canopee-react/src/prospect-client/Toggle/ToggleCommon.tsx
+++ packages/canopee-react/src/prospect-client/Toggle/ToggleCommon.tsx
@@ -1,4 +1,7 @@
+import check from "@material-symbols/svg-400/rounded/check.svg";
+import close from "@material-symbols/svg-400/rounded/close.svg";
 import { forwardRef, type InputHTMLAttributes, useId } from "react";
+import { Svg } from "../Svg/Svg";
 import { getComponentClassName } from "../utilities/getComponentClassName";
 
 export type ToggleProps = Omit<
@@ -17,12 +20,26 @@ export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
         htmlFor={inputId}
       >
         <div className="af-toggle__root">
-          <span className="af-toggle__handle" />
+          <span className="af-toggle__handle">
+            <Svg
+              src={check}
+              aria-hidden="true"
+              className="af-toggle__icon af-toggle__icon--check"
+              width={16}
+              height={16}
+            />
+            <Svg
+              src={close}
+              aria-hidden="true"
+              className="af-toggle__icon af-toggle__icon--close"
+              width={16}
+              height={16}
+            />
+          </span>
         </div>
         <input {...inputProps} ref={ref} type="checkbox" id={inputId} />
       </label>
     );
   },
 );
-
 Toggle.displayName = "Toggle";
