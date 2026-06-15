165e4fee50e8f23acf5a2afb92d585c089495a40
JLou <JLou@users.noreply.github.com>
2026-06-15 07:33:16 +0000

    deploy: 65e9f064d335a8ea185233bdd11fe6fc3dab30ae

---
+++ distributeur/react/1.7.0/assets/MandatoryMention-Cpe9O8EK.js
@@ -0,0 +1,26 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{r as t}from"./react-DaqufBRT.js";import{c as n,m as r,s as i,u as a,w as o}from"./iframe-BLrDEELY.js";import{t as s}from"./mdx-react-shim-m59I_HzQ.js";import{n as c}from"./distributeur-G7DyhWof.js";import{Default as l,n as u,t as d}from"./MandatoryMention.stories-CU7Tf6iO.js";function f(e){let r={code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,pre:`pre`,ul:`ul`,...t(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(a,{of:d}),`
+`,(0,m.jsx)(r.h1,{id:`mandatorymention`,children:`MandatoryMention`}),`
+`,(0,m.jsx)(r.p,{children:`Mandatory Mention item is used to inform the user of the expected completion of a form: either all fields are required, or only some of them`}),`
+`,(0,m.jsx)(r.h2,{id:`import`,children:`Import`}),`
+`,(0,m.jsx)(r.pre,{children:(0,m.jsx)(r.code,{className:`language-tsx`,children:`import { MandatoryMention } from "@axa-fr/canopee-react/distributeur";
+`})}),`
+`,(0,m.jsx)(r.h2,{id:`use`,children:`Use`}),`
+`,(0,m.jsx)(r.pre,{children:(0,m.jsx)(r.code,{className:`language-tsx`,children:`import { MandatoryMention } from "@axa-fr/canopee-react/distributeur";
+
+function FormHeader() {
+  return (
+    <>
+      <MandatoryMention variant="one" />
+      <MandatoryMention variant="all" />
+    </>
+  );
+}
+`})}),`
+`,(0,m.jsx)(r.h2,{id:`playground`,children:`Playground`}),`
+`,(0,m.jsx)(i,{of:l}),`
+`,(0,m.jsx)(n,{of:l}),`
+`,(0,m.jsx)(r.h3,{id:`variants`,children:`Variants`}),`
+`,(0,m.jsxs)(r.ul,{children:[`
+`,(0,m.jsx)(r.li,{children:`variant="one": Champs obligatoires *`}),`
+`,(0,m.jsx)(r.li,{children:`variant="all": Tous les champs sont obligatoires`}),`
+`]})]})}function p(e={}){let{wrapper:n}={...t(),...e.components};return n?(0,m.jsx)(n,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=o(),s(),r(),u(),c()}))();export{p as default};
