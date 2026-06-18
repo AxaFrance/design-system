c51f726b3a6a095f13d06929c3ac2522e711a61e
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-06-18 13:46:10 +0000

    deploy: 2a2c5aa7daf914ec0a4f7057bf52e9f2e5159147

--- client/react/next/assets/ItemMessage.stories-14AvZfP1.js
+++
@@ -1,5 +0,0 @@
-import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./client-BVw2ymGC.js";import{Qt as r,en as i}from"./LoaderCommon-CamWhjCk.js";var a=t({Playground:()=>s,__namedExportsOrder:()=>c,default:()=>o}),o,s,c,l=e((()=>{n(),o={title:`Components/Form/ItemMessage`,component:r,argTypes:{messageType:{options:Object.values(i),control:{type:`select`}}}},s={args:{message:`Message`}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: "Message"
-  }
-}`,...s.parameters?.docs?.source}}},c=[`Playground`]}));l();export{s as Playground,c as __namedExportsOrder,o as default,l as n,a as t};
