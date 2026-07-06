4f0f4a6b950a1c2cfc4cd712e9d78a3d9c7f8052
MartinWeb <MartinWeb@users.noreply.github.com>
2026-07-06 12:52:28 +0000

    deploy: e9a44a691fcde280898dc65062208f42ff06704c

---
+++ distributeur/react/next/assets/NumberInput.stories-Dbxz-JJg.js
@@ -0,0 +1,17 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n,w as r}from"./iframe-CpwZR4q2.js";import{n as i,rt as a,s as o,wt as s}from"./distributeur-BZxz3JHq.js";var c,l,u,d,f,p=e((()=>{i(),n(),c=r(),l=t.type().meta({title:`Components/Form/Input/Number`,args:{required:!0,classModifier:[],value:5,placeholder:`Your name`,name:`name`,id:`nameid`,readOnly:!1,disabled:!1,autoFocus:!1,className:``,label:`Your name`,helpMessage:`Aide à la saisie`,forceDisplayMessage:!1,message:``,messageType:s.error,classNameContainerLabel:`col-md-2`,classNameContainerInput:`col-md-10`,"aria-disabled":!1},argTypes:{classModifier:{options:[``,`required`,`disabled`],control:{type:`inline-check`}}}}),u=l.story({name:`NumberInput`,render:({classModifier:e,...t})=>(0,c.jsx)(a,{classModifier:e.join(` `),...t})}),d=l.story({name:`NumberInput with help button`,render:({classModifier:e,...t})=>(0,c.jsx)(a,{classModifier:e.join(` `),...t,children:(0,c.jsx)(o,{mode:`hover`,children:`Help`})})}),u.input.parameters={...u.input.parameters,docs:{...u.input.parameters?.docs,source:{originalSource:`meta.story({
+  name: "NumberInput",
+  render: ({
+    classModifier,
+    ...args
+  }) => <NumberInput classModifier={classModifier.join(" ")} {...args} />
+})`,...u.input.parameters?.docs?.source}}},d.input.parameters={...d.input.parameters,docs:{...d.input.parameters?.docs,source:{originalSource:`meta.story({
+  name: "NumberInput with help button",
+  render: ({
+    classModifier,
+    ...args
+  }) =>
+  // Add a button as children to the NumberInput
+  <NumberInput classModifier={classModifier.join(" ")} {...args}>
+      <HelpButton mode="hover">Help</HelpButton>
+    </NumberInput>
+})`,...d.input.parameters?.docs?.source}}},f=[`NumberInputStory`,`NumberInputWithChildrenStory`]}));p();export{u as NumberInputStory,d as NumberInputWithChildrenStory,f as __namedExportsOrder,l as default,p as t};
---
+++ distributeur/react/next/assets/Title.stories-CdnNQgm-.js
@@ -0,0 +1,70 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{n,t as r,w as i}from"./iframe-CpwZR4q2.js";import{Dt as a,H as o,g as s,n as c}from"./distributeur-BZxz3JHq.js";var l=t({Template:()=>h,TitleWithContent:()=>g,WithoutDivider:()=>_,__namedExportsOrder:()=>v,default:()=>p}),u,d,f,p,m,h,g,_,v,y=e((()=>{c(),r(),u=i(),d=[``,`content`],f=[`Button`,`Link`,`None`],p=n.type().meta({title:`Components/Title`,args:{children:`Sample Title`,classModifier:``,className:``,heading:`h2`,withDivider:!0,contentLeft:`None`,contentRight:`None`},argTypes:{contentLeft:{options:f,control:{type:`select`}},contentRight:{options:f,control:{type:`select`}},classModifier:{options:d,control:{type:`select`}}}}),m=e=>{switch(e){case`Link`:return(0,u.jsx)(o,{href:`/`,children:`Click me`});case`Button`:return(0,u.jsx)(a,{children:`Click me`});default:return}},h=p.story({name:`Title`,render:({children:e,classModifier:t,...n})=>(0,u.jsx)(s,{classModifier:t,...n,contentLeft:void 0,contentRight:void 0,children:e}),args:{children:`Sample Title`,classModifier:``,className:``,heading:`h2`,withDivider:!0},argTypes:{contentLeft:{control:!1},contentRight:{control:!1}}}),g=p.story({render:({children:e,contentLeft:t,contentRight:n,heading:r,withDivider:i})=>(0,u.jsx)(s,{heading:r,withDivider:i,contentLeft:m(t),contentRight:m(n),children:e}),args:{children:`Title with content`,heading:`h2`,withDivider:!0,contentLeft:`Button`,contentRight:`Link`}}),_=p.story({render:({children:e,heading:t})=>(0,u.jsx)(s,{heading:t,withDivider:!1,children:e}),args:{children:`Title without divider`,heading:`h2`,withDivider:!1},argTypes:{classModifier:{control:!1},className:{control:!1},contentLeft:{control:!1},contentRight:{control:!1}}}),h.input.parameters={...h.input.parameters,docs:{...h.input.parameters?.docs,source:{originalSource:`meta.story({
+  name: "Title",
+  render: ({
+    children: text,
+    classModifier,
+    ...args
+  }) => <Title classModifier={classModifier} {...args} contentLeft={undefined} contentRight={undefined}>
+      {text}
+    </Title>,
+  args: {
+    children: "Sample Title",
+    classModifier: "",
+    className: "",
+    heading: "h2",
+    withDivider: true
+  },
+  argTypes: {
+    contentLeft: {
+      control: false
+    },
+    contentRight: {
+      control: false
+    }
+  }
+})`,...h.input.parameters?.docs?.source}}},g.input.parameters={...g.input.parameters,docs:{...g.input.parameters?.docs,source:{originalSource:`meta.story({
+  render: ({
+    children,
+    contentLeft,
+    contentRight,
+    heading,
+    withDivider
+  }) => {
+    return <Title heading={heading} withDivider={withDivider} contentLeft={getContent(contentLeft)} contentRight={getContent(contentRight)}>
+        {children}
+      </Title>;
+  },
+  args: {
+    children: "Title with content",
+    heading: "h2",
+    withDivider: true,
+    contentLeft: "Button",
+    contentRight: "Link"
+  }
+})`,...g.input.parameters?.docs?.source}}},_.input.parameters={..._.input.parameters,docs:{..._.input.parameters?.docs,source:{originalSource:`meta.story({
+  render: ({
+    children,
+    heading
+  }) => <Title heading={heading} withDivider={false}>
+      {children}
+    </Title>,
+  args: {
+    children: "Title without divider",
+    heading: "h2",
+    withDivider: false
+  },
+  argTypes: {
+    classModifier: {
+      control: false
+    },
+    className: {
+      control: false
+    },
+    contentLeft: {
+      control: false
+    },
+    contentRight: {
+      control: false
+    }
+  }
+})`,..._.input.parameters?.docs?.source}}},v=[`Template`,`TitleWithContent`,`WithoutDivider`]}));y();export{h as Template,g as TitleWithContent,_ as WithoutDivider,v as __namedExportsOrder,p as default,y as n,l as t};
