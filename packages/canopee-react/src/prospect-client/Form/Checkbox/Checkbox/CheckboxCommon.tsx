4d9ab1a5903c64e48513ce35ac87ee3d30ea88ae
Julie NONNON <julie.nonnon@axa.fr>
2026-07-22 17:51:14 +0200

    fix(client,prospect): add --warning and --hover-warning variant for checkbox(#1813)

--- packages/canopee-react/src/prospect-client/Form/Checkbox/Checkbox/CheckboxCommon.tsx
+++ packages/canopee-react/src/prospect-client/Form/Checkbox/Checkbox/CheckboxCommon.tsx
@@ -1,19 +1,26 @@
 import { type ComponentProps, forwardRef } from "react";
 
+import { getClassName } from "../../../utilities/getClassName";
+
 export type CheckboxProps = {
   /** @deprecated Use `aria-errormessage` instead */
   errorId?: string;
   /** @deprecated Use `aria-invalid` instead */
   hasError?: boolean;
+  hasWarning?: boolean;
 } & Omit<ComponentProps<"input">, "disabled" | "type">;
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ errorId, hasError, className, ...inputProps }, ref) => (
+  ({ errorId, hasError, hasWarning, className, ...inputProps }, ref) => (
     <input
       aria-errormessage={errorId}
       aria-invalid={hasError}
       {...inputProps}
-      className={["af-checkbox", className].filter(Boolean).join(" ")}
+      className={getClassName({
+        baseClassName: "af-checkbox",
+        modifiers: [hasWarning && "warning"],
+        className,
+      })}
       ref={ref}
       type="checkbox"
     />
