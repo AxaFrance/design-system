1d737e1f9fea61bde1f5e705be587050e95ad5fb
Julie NONNON <julie.nonnon@axa.fr>
2026-07-28 16:33:43 +0200

    fix(client,prospect): add --warning and --hover-warning variant for checkbox - new (#1813)

--- packages/canopee-react/src/prospect-client/Form/Checkbox/Checkbox/CheckboxCommon.tsx
+++ packages/canopee-react/src/prospect-client/Form/Checkbox/Checkbox/CheckboxCommon.tsx
@@ -1,25 +1,42 @@
-import { type ComponentProps } from "react";
+import { type ComponentProps, forwardRef } from "react";
+
+import { getClassName } from "../../../utilities/getClassName";
 
 export type CheckboxProps = {
   /** @deprecated Use `aria-errormessage` instead */
   errorId?: string;
   /** @deprecated Use `aria-invalid` instead */
   hasError?: boolean;
+  hasWarning?: boolean;
 } & Omit<ComponentProps<"input">, "disabled" | "type">;
 
-export const Checkbox = ({
-  errorId,
-  hasError,
-  className,
-  ...inputProps
-}: CheckboxProps) => (
-  <input
-    aria-errormessage={errorId}
-    aria-invalid={hasError}
-    {...inputProps}
-    className={["af-checkbox", className].filter(Boolean).join(" ")}
-    type="checkbox"
-  />
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  (props, ref) => {
+    const {
+      errorId,
+      hasError,
+      hasWarning,
+      className,
+      "aria-errormessage": ariaErrorMessage,
+      "aria-invalid": ariaInvalid,
+      ...inputProps
+    } = props;
+
+    return (
+      <input
+        aria-errormessage={ariaErrorMessage ?? errorId}
+        aria-invalid={ariaInvalid ?? hasError}
+        {...inputProps}
+        className={getClassName({
+          baseClassName: "af-checkbox",
+          modifiers: [hasWarning && "warning"],
+          className,
+        })}
+        ref={ref}
+        type="checkbox"
+      />
+    );
+  },
 );
 
 Checkbox.displayName = "Checkbox";
