0183aef2e07e2f5872d6cb37580df67d05227a98
letaaz <letaaz@users.noreply.github.com>
2026-07-29 12:25:42 +0000

    deploy: 3b34d59cd00768054b1c2c8975421e7e5bb892be

---
+++ client/react/next/assets/TabMenu.stories-0uNmlnIT.js
@@ -0,0 +1,13 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{t as r}from"./client-C-tq6Akd.js";import{dt as i}from"./TimelineVerticalCommon-COJR0bPC.js";var a=t({Playground:()=>c,__namedExportsOrder:()=>l,default:()=>s}),o,s,c,l,u=e((()=>{r(),o=n(),s={title:`Components/TabMenu 🚧`,component:i},c={render:()=>(0,o.jsx)(i,{items:[{href:`#contracts`,label:`Mes contrats`,isActive:!0},{href:`#claims`,label:`Mes sinistres`},{href:`#documents`,label:`Mes documents`}]})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: () => <TabMenu items={[{
+    href: "#contracts",
+    label: "Mes contrats",
+    isActive: true
+  }, {
+    href: "#claims",
+    label: "Mes sinistres"
+  }, {
+    href: "#documents",
+    label: "Mes documents"
+  }]} />
+}`,...c.parameters?.docs?.source}}},l=[`Playground`]}));u();export{c as Playground,l as __namedExportsOrder,s as default,u as n,a as t};
