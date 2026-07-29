4b5212599372d1460ced7929eb3c168a2e271a77
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-07-29 12:44:53 +0000

    deploy: 1ed74ebb7977e7b95d36e16d9d8ff3611085976c

---
+++ prospect/react/next/assets/ExitLayout.stories-CgFHJU4M.js
@@ -0,0 +1,16 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{M as n,mt as r,pt as i,t as a,yt as o}from"./prospect-B1mImIZ3.js";import{n as s,t as c}from"./LayoutDemo-TdfhY2j9.js";var l,u,d,f,p;e((()=>{a(),r(),s(),l=t(),u={title:`Layouts/Exit Layout`,parameters:{layout:`fullscreen`},argTypes:{showAgent:{control:`boolean`},actions:{control:`number`,min:1},content:{control:`number`,min:1}}},d=({headingProps:e,showAgent:t,iconProps:r,actions:i=1,content:a=4,heightAgent:s=400})=>(0,l.jsx)(c,{title:`Form Layout Title`,children:(0,l.jsxs)(n,{headingProps:e,iconProps:r,children:[(0,l.jsx)(n.Content,{children:Array.from({length:a},(e,t)=>(0,l.jsx)(o,{children:`Slot CONTENT`},t))}),(0,l.jsx)(n.Agent,{children:t?(0,l.jsx)(o,{style:{height:s},children:`AGENT`}):null}),(0,l.jsx)(n.Action,{children:Array.from({length:i},(e,t)=>(0,l.jsx)(o,{children:`Slot ACTION`},t))})]})}),f={name:`ExitLayout`,render:d,args:{headingProps:{title:`Title`,firstSubtitle:`firstSubtitle`},showAgent:!0,iconProps:{src:i,hasBackground:!0,variant:`success`}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  name: "ExitLayout",
+  render: Demo,
+  args: {
+    headingProps: {
+      title: "Title",
+      firstSubtitle: "firstSubtitle"
+    },
+    showAgent: true,
+    iconProps: {
+      src: check,
+      hasBackground: true,
+      variant: "success"
+    }
+  }
+}`,...f.parameters?.docs?.source}}},p=[`ExitLayoutStory`]}))();export{f as ExitLayoutStory,p as __namedExportsOrder,u as default};
