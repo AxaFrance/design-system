2f39dfcec4674f7e1d322f826ec7cc2c9107d231
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-07-29 08:39:00 +0000

    deploy: da7ab6efba8d4605192a9204bf218bbcecdc7225

---
+++ client/react/next/assets/ItemMessage.stories-oZysMjws.js
@@ -0,0 +1,5 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./client-CVB3Mtd5.js";import{in as r,on as i}from"./TimelineVerticalCommon-C3UlEI5-.js";var a=t({Playground:()=>s,__namedExportsOrder:()=>c,default:()=>o}),o,s,c,l=e((()=>{n(),o={title:`Components/Form/ItemMessage`,component:r,argTypes:{messageType:{options:Object.values(i),control:{type:`select`}}}},s={args:{message:`Message`}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: "Message"
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Playground`]}));l();export{s as Playground,c as __namedExportsOrder,o as default,l as n,a as t};
---
+++ client/react/next/assets/mdx-react-shim-7n_bJYOm.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-CHiW55ls.js";var n=e((()=>{t()}));export{n as t};
