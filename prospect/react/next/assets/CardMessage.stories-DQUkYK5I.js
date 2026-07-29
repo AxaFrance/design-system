e8d5f235df2ebcae31115347228795db0dd8c952
letaaz <letaaz@users.noreply.github.com>
2026-07-29 12:25:27 +0000

    deploy: 3b34d59cd00768054b1c2c8975421e7e5bb892be

---
+++ prospect/react/next/assets/CardMessage.stories-DQUkYK5I.js
@@ -0,0 +1,18 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{_t as r,t as i,vt as a}from"./prospect-BPgu04TS.js";var o=t({Default:()=>l,__namedExportsOrder:()=>u,default:()=>c}),s,c,l,u,d=e((()=>{i(),s=n(),c={component:r,title:`Components/CardMessage`},l={name:`CardMessage`,render:({...e})=>(0,s.jsx)(r,{...e}),args:{title:`Title`,text:`This is a message`},argTypes:{variant:{options:Object.values(a),control:{type:`select`}}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: "CardMessage",
+  render: ({
+    ...args
+  }) => <CardMessage {...args} />,
+  args: {
+    title: "Title",
+    text: "This is a message"
+  },
+  argTypes: {
+    variant: {
+      options: Object.values(cardMessageVariants),
+      control: {
+        type: "select"
+      }
+    }
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Default`]}));d();export{l as Default,u as __namedExportsOrder,c as default,d as n,o as t};
