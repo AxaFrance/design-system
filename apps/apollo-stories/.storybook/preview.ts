6f775854af917ef3fa65c4b6016b84414deab2d0
Rémi <remi.touton@axa.fr>
2026-07-30 10:53:53 +0200

    feat(client,prospect): heading-convergence-taille-font

--- apps/apollo-stories/.storybook/preview.ts
+++ apps/apollo-stories/.storybook/preview.ts
@@ -4,6 +4,11 @@ import "@axa-fr/canopee-css/prospect/common/tokens.css";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: ["*", ["Tokens", "Font Style", "*"]],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
