2d7340a7a273dd161152adf288698059a025b3e9
letaaz <letaaz@users.noreply.github.com>
2026-07-31 08:50:19 +0000

    deploy: 70d8fffe1854687f49a90a914a4025dee99b57a2

---
+++ client/react/next/assets/Getstarted-CjbIbE7i.js
@@ -0,0 +1,14 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{r as n}from"./react-8h-nZ2x-.js";import{t as r}from"./mdx-react-shim-CJ9YBtIG.js";import{l as i,s as a}from"./blocks-CdflbimN.js";function o(e){let t={a:`a`,code:`code`,h2:`h2`,li:`li`,p:`p`,strong:`strong`,ul:`ul`,...n(),...e.components};return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(a,{parameters:{docs:{canvas:{layout:`fullscreen`}},layout:`fullscreen`}}),`
+`,(0,c.jsx)(`p`,{style:{textAlign:`center`},children:(0,c.jsx)(`img`,{alt:`hero-client`,title:`hero-client`,src:`./hero-client.jpg`,width:`100%`})}),`
+`,(0,c.jsx)(t.h2,{id:`get-started-with-canopee-design-system---univers-client`,children:`Get Started with Canopee Design System - Univers Client`}),`
+`,(0,c.jsxs)(t.p,{children:[`See all guidelines on `,(0,c.jsx)(t.strong,{children:(0,c.jsx)(t.a,{href:`https://zeroheight.com/49b6215d6/v/latest/p/817fe8-client`,rel:`nofollow`,children:`ZeroHeight`})})]}),`
+`,(0,c.jsxs)(t.p,{children:[`See open source code on `,(0,c.jsx)(t.strong,{children:(0,c.jsx)(t.a,{href:`https://github.com/AxaFrance/design-system/`,rel:`nofollow`,children:`Github`})})]}),`
+`,(0,c.jsx)(t.h2,{id:`layers`,children:`Layers`}),`
+`,(0,c.jsxs)(t.p,{children:[`To make customization easier, the Slash design system is making use of CSS layers. This allows you to easily override styles without having to worry about specificity or the order of your CSS files.
+See `,(0,c.jsx)(t.a,{href:`https://developer.mozilla.org/fr/docs/Web/CSS/Reference/At-rules/@layer`,rel:`nofollow`,children:`MDN documentation`}),` for more information on CSS layers.`]}),`
+`,(0,c.jsx)(t.p,{children:`The layers are defined as follows:`}),`
+`,(0,c.jsxs)(t.ul,{children:[`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@layer reset`}),` for global styles and resets;`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@layer canopee`}),` for component styles;`]}),`
+`]}),`
+`,(0,c.jsxs)(t.p,{children:[`By default, the layers are order `,(0,c.jsx)(t.code,{children:`reset`}),` first, then `,(0,c.jsx)(t.code,{children:`canopee`}),`.`]})]})}function s(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,c.jsx)(t,{...e,children:(0,c.jsx)(o,{...e})}):o(e)}var c;e((()=>{c=t(),r(),i()}))();export{s as default};
