1746a4e28a2679911519bc6562633ef89cddccf3
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-08-03 07:36:42 +0000

    deploy: b6c9eae3d53307681138706969a356c03ffaadb3

---
+++ prospect/react/next/assets/ItemMenu.stories-11pHvpjV.js
@@ -0,0 +1,7 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{P as n,t as r}from"./prospect-Dl5gilUL.js";var i=t({Playground:()=>o,__namedExportsOrder:()=>s,default:()=>a}),a,o,s,c=e((()=>{r(),a={title:`Components/ItemMenu 🚧`,component:n},o={args:{href:`https://example.com/contracts`,children:`Mes contrats`,isActive:!1}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    href: "https://example.com/contracts",
+    children: "Mes contrats",
+    isActive: false
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Playground`]}));c();export{o as Playground,s as __namedExportsOrder,a as default,c as n,i as t};
