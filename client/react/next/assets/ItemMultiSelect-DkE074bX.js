e6afc0a08ae516f6866454b1fdaac7d9aef7fa39
JLou <JLou@users.noreply.github.com>
2026-08-25 08:08:36 +0000

    deploy: ee3d6d1c3d2e8bcf7f0cbacdeb5dacbf1b40ac5b

---
+++ client/react/next/assets/ItemMultiSelect-DkE074bX.js
@@ -0,0 +1,32 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{r as n}from"./react-BpvhHsRw.js";import{t as r}from"./mdx-react-shim-Bfux0tQg.js";import{i,l as a,n as o,s}from"./blocks-Dsk8VUcy.js";import{Checked as c,Playground as l,Primary as u,Secondary as d,WithLongLabel as f,n as p,t as m}from"./ItemMultiSelect.stories-DPOPnkRM.js";function h(e){let t={code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,pre:`pre`,...n(),...e.components};return(0,_.jsxs)(_.Fragment,{children:[(0,_.jsx)(s,{of:m,name:`ItemMultiSelect`}),`
+`,(0,_.jsx)(t.h1,{id:`itemmultiselect`,children:`ItemMultiSelect`}),`
+`,(0,_.jsx)(t.p,{children:`The ItemMultiSelect component combines a checkbox with a label aligned horizontally.
+It supports two background variants: primary (blue-040) and secondary (white-1000).
+Only the checkbox changes on hover or selection, while the background container remains stable.`}),`
+`,(0,_.jsx)(t.p,{children:`Example usage:`}),`
+`,(0,_.jsx)(t.pre,{children:(0,_.jsx)(t.code,{className:`language-tsx`,children:`import { ItemMultiSelect } from "@axa-fr/canopee-react/client";
+
+const MyComponent = () => (
+  <ItemMultiSelect
+    label="I agree to the terms"
+    variant="primary"
+    name="agree"
+    value="agree"
+  />
+);
+`})}),`
+`,(0,_.jsx)(t.h2,{id:`playground`,children:`Playground`}),`
+`,(0,_.jsx)(o,{of:l,sourceState:`shown`}),`
+`,(0,_.jsx)(i,{of:l}),`
+`,(0,_.jsx)(t.h2,{id:`variants`,children:`Variants`}),`
+`,(0,_.jsx)(t.h3,{id:`primary-default`,children:`Primary (Default)`}),`
+`,(0,_.jsx)(t.p,{children:`The primary variant uses a blue-040 background.`}),`
+`,(0,_.jsx)(o,{of:u}),`
+`,(0,_.jsx)(t.h3,{id:`secondary`,children:`Secondary`}),`
+`,(0,_.jsx)(t.p,{children:`The secondary variant uses a white-1000 background.`}),`
+`,(0,_.jsx)(o,{of:d}),`
+`,(0,_.jsx)(t.h2,{id:`states`,children:`States`}),`
+`,(0,_.jsx)(t.h3,{id:`checked`,children:`Checked`}),`
+`,(0,_.jsx)(o,{of:c}),`
+`,(0,_.jsx)(t.h3,{id:`with-long-label`,children:`With Long Label`}),`
+`,(0,_.jsx)(o,{of:f})]})}function g(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,_.jsx)(t,{...e,children:(0,_.jsx)(h,{...e})}):h(e)}var _;e((()=>{_=t(),r(),a(),p()}))();export{g as default};
