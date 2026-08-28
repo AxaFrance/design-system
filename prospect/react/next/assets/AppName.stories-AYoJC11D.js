cafcd3463778c6a1a305c21fcc987ff20b12551d
J-Nonnon <J-Nonnon@users.noreply.github.com>
2026-08-28 14:27:01 +0000

    deploy: 6b74dca863e9a8fe45f2aa154b2096ebb10e9d77

--- prospect/react/next/assets/AppName.stories-AYoJC11D.js
+++
@@ -1,8 +0,0 @@
-import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{Pt as n,t as r}from"./prospect-DTwhPsB5.js";var i=t({Playground:()=>o,__namedExportsOrder:()=>s,default:()=>a}),a,o,s,c=e((()=>{r(),a={title:`Components/AppName`,component:n},o={args:{label:`Mon application`,logoLinkProps:{href:`/`}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: "Mon application",
-    logoLinkProps: {
-      href: "/"
-    }
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Playground`]}));c();export{o as Playground,s as __namedExportsOrder,a as default,c as n,i as t};
