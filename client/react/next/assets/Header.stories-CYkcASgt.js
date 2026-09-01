d8efc7309cb0e2170f8580ac74b6fd398ac2e903
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2026-09-01 12:37:49 +0000

    deploy: e7be5e0f23031f8f0e80adc8e6dc0622dd75753c

---
+++ client/react/next/assets/Header.stories-CYkcASgt.js
@@ -0,0 +1,66 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{q as r,t as i,x as a}from"./client-DgkcTR5O.js";import{br as o}from"./TimelineVerticalCommon-CZpyL8A-.js";import{a as s,i as c,n as l,o as u,r as d,t as f}from"./settings-fill-CEvnba5m.js";var p=t({Default:()=>y,Logo:()=>b,LogoAndActionChildren:()=>S,LogoAndActionChildrenAndMenuBurger:()=>C,LogoAndMenu:()=>x,LogoAndMenuBurger:()=>w,LogoAndMenuWithTitle:()=>T,LogoWithTitle:()=>E,__namedExportsOrder:()=>D,default:()=>g}),m,h,g,_,v,y,b,x,S,C,w,T,E,D,O=e((()=>{i(),u(),d(),f(),m=n(),{action:h}=__STORYBOOK_MODULE_ACTIONS__,g={title:`Components/Header`,parameters:{layout:`fullscreen`},component:a},_={items:[{href:`#accueil`,label:`Accueil`},{href:`#contrats`,label:`Contrats`}]},v={buttonLabel:`Mon espace`,children:(0,m.jsx)(r,{variant:`tertiary`,iconLeft:(0,m.jsx)(o,{src:c,variant:`primary`,size:`S`}),onClick:h(`Logout clicked`),children:`Déconnexion`}),clickItems:[{icon:s,title:`Mon profil`,subtitle:`Informations personnelles`,textSecondary:`Consulter et modifier vos données`,state:`default`,ariaLabelForActionIcon:`Aller à mon profil`,tagLabel:`Nouveau`,tagProps:{variant:`info`},onClick:h(`Profil clicked`)},{icon:l,title:`Préférences`,subtitle:`Notifications et sécurité`,state:`default`,ariaLabelForActionIcon:`Aller aux préférences`,onClick:h(`Preferences clicked`)},{icon:l,title:`Préférences`,state:`default`,onClick:h(`Preferences clicked`)},{icon:l,title:`Préférences`,state:`default`,onClick:h(`Preferences clicked`)},{icon:l,title:`Préférences`,state:`default`,onClick:h(`Preferences clicked`)}]},y={args:{appNameProps:{label:`Mon application`},tabMenuProps:_,menuBurgerProps:v,actionChildren:(0,m.jsx)(r,{variant:`secondary`,iconLeft:(0,m.jsx)(o,{src:s,size:`S`}),onClick:h(`Logout clicked`),children:`Accéder à AXA Banque`})}},b={args:{appNameProps:{label:`Mon application`}}},x={args:{appNameProps:{label:`Mon application`},tabMenuProps:_}},S={args:{appNameProps:{label:`Mon application`},actionChildren:(0,m.jsx)(r,{variant:`secondary`,iconLeft:(0,m.jsx)(o,{src:s,size:`S`}),onClick:h(`Logout clicked`),children:`Accéder à AXA Banque`})}},C={args:{appNameProps:{label:`Mon application`},actionChildren:(0,m.jsx)(r,{variant:`secondary`,iconLeft:(0,m.jsx)(o,{src:s,size:`S`}),onClick:h(`Logout clicked`),children:`Accéder à AXA Banque`}),menuBurgerProps:v}},w={args:{appNameProps:{label:`Mon application`},menuBurgerProps:v}},T={args:{title:`Tableau de bord`,appNameProps:{label:`Mon application`},tabMenuProps:_}},E={args:{title:`Tableau de bord`,appNameProps:{label:`Mon application`}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    appNameProps: {
+      label: "Mon application"
+    },
+    tabMenuProps,
+    menuBurgerProps,
+    actionChildren: <Button variant="secondary" iconLeft={<Icon src={accountBalance} size="S" />} onClick={action("Logout clicked")}>
+        Accéder à AXA Banque
+      </Button>
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    appNameProps: {
+      label: "Mon application"
+    }
+  }
+}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  args: {
+    appNameProps: {
+      label: "Mon application"
+    },
+    tabMenuProps
+  }
+}`,...x.parameters?.docs?.source}}},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    appNameProps: {
+      label: "Mon application"
+    },
+    actionChildren: <Button variant="secondary" iconLeft={<Icon src={accountBalance} size="S" />} onClick={action("Logout clicked")}>
+        Accéder à AXA Banque
+      </Button>
+  }
+}`,...S.parameters?.docs?.source}}},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
+  args: {
+    appNameProps: {
+      label: "Mon application"
+    },
+    actionChildren: <Button variant="secondary" iconLeft={<Icon src={accountBalance} size="S" />} onClick={action("Logout clicked")}>
+        Accéder à AXA Banque
+      </Button>,
+    menuBurgerProps
+  }
+}`,...C.parameters?.docs?.source}}},w.parameters={...w.parameters,docs:{...w.parameters?.docs,source:{originalSource:`{
+  args: {
+    appNameProps: {
+      label: "Mon application"
+    },
+    menuBurgerProps
+  }
+}`,...w.parameters?.docs?.source}}},T.parameters={...T.parameters,docs:{...T.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Tableau de bord",
+    appNameProps: {
+      label: "Mon application"
+    },
+    tabMenuProps
+  }
+}`,...T.parameters?.docs?.source}}},E.parameters={...E.parameters,docs:{...E.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Tableau de bord",
+    appNameProps: {
+      label: "Mon application"
+    }
+  }
+}`,...E.parameters?.docs?.source}}},D=[`Default`,`Logo`,`LogoAndMenu`,`LogoAndActionChildren`,`LogoAndActionChildrenAndMenuBurger`,`LogoAndMenuBurger`,`LogoAndMenuWithTitle`,`LogoWithTitle`]}));O();export{y as Default,b as Logo,S as LogoAndActionChildren,C as LogoAndActionChildrenAndMenuBurger,x as LogoAndMenu,w as LogoAndMenuBurger,T as LogoAndMenuWithTitle,E as LogoWithTitle,D as __namedExportsOrder,g as default,O as n,p as t};
