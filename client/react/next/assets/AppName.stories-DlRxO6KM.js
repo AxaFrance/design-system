54e6b83c768042c5b0cae640c45c922e9d13c898
Remi-DS <Remi-DS@users.noreply.github.com>
2026-09-01 15:02:46 +0000

    deploy: 91d18fef7d6ec41cafc13d409aa0fd0eb199ca88

--- client/react/next/assets/AppName.stories-DlRxO6KM.js
+++
@@ -1,8 +0,0 @@
-import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./client-DgkcTR5O.js";import{ur as r}from"./TimelineVerticalCommon-CZpyL8A-.js";var i=t({Playground:()=>o,__namedExportsOrder:()=>s,default:()=>a}),a,o,s,c=e((()=>{n(),a={title:`Components/AppName`,component:r},o={args:{label:`Mon application`,logoLinkProps:{href:`/`}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: "Mon application",
-    logoLinkProps: {
-      href: "/"
-    }
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Playground`]}));c();export{o as Playground,s as __namedExportsOrder,a as default,c as n,i as t};
