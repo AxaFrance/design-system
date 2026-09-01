2dd48cb49ff72872fea19e41e5c303eec0947731
Remi-DS <Remi-DS@users.noreply.github.com>
2026-09-01 15:19:38 +0000

    deploy: 3a5ecde6dc4193d81cbdae479cf9ec20a8c28aaf

---
+++ client/react/next/assets/CardRadio.stories-Cpz2OBh7.js
@@ -0,0 +1,18 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{k as r,t as i}from"./client-zZ0vXdml.js";import{n as a,t as o}from"./home-DJdG_X7N.js";var s,c=e((()=>{s=`data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20width='48'%20height='48'%20viewBox='0%20-960%20960%20960'%3e%3cpath%20d='M212-241v-339h60v339h-60Zm242%200v-339h60v339h-60ZM80-121v-60h800v60H80Zm608-120v-339h60v339h-60ZM80-640v-53l400-228%20400%20228v53H80Z'/%3e%3c/svg%3e`})),l=t({CardRadioStory:()=>p,__namedExportsOrder:()=>m,default:()=>f}),u,d,f,p,m,h=e((()=>{i(),c(),a(),u=n(),d={homeIcon:o,accountBalanceIcon:s,none:void 0},f={title:`Components/Form/Radio/CardRadio`,component:r,parameters:{layout:`centered`},argTypes:{position:{control:{type:`inline-radio`},options:[`vertical`,`horizontal`]},label:{control:`text`},description:{control:`text`},subtitle:{control:`text`},iconVariant:{control:{type:`inline-radio`},options:[`icon`,`base picture`],if:{arg:`position`,eq:`horizontal`}},icon:{control:`select`,options:Object.keys(d),if:{arg:`iconVariant`,eq:`icon`}},src:{if:{arg:`iconVariant`,eq:`base picture`}},variant:{control:{type:`select`},options:[`error`,`warning`]}},args:{position:`vertical`,label:`Titre`,description:`Sous-titre 1`,subtitle:`Sous-titre 2`,name:`foo`,value:`bar`,iconVariant:`icon`,icon:`accountBalanceIcon`,src:`https://picsum.photos/48`}},p={name:`Playground`,render:({icon:e,description:t,subtitle:n,name:i,value:a,...o})=>(0,u.jsx)(r,{...o,icon:d[e??`none`],value:a===``?void 0:a,name:i===``?void 0:i,description:t===``?void 0:t,subtitle:n===``?void 0:n}),decorators:[(e,{args:t})=>(0,u.jsx)(`div`,{style:{width:t.position===`horizontal`?500:300},children:(0,u.jsx)(e,{})})]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  name: "Playground",
+  render: ({
+    icon,
+    description,
+    subtitle,
+    name,
+    value,
+    ...args
+  }: ComponentProps<typeof CardRadio>) => <CardRadio {...args} icon={ICONS[icon ?? "none"]} value={value !== "" ? value : undefined} name={name !== "" ? name : undefined} description={description !== "" ? description : undefined} subtitle={subtitle !== "" ? subtitle : undefined} />,
+  decorators: [(Story, {
+    args
+  }) => <div style={{
+    width: args.position === "horizontal" ? 500 : 300
+  }}>
+        <Story />
+      </div>]
+}`,...p.parameters?.docs?.source}}},m=[`CardRadioStory`]}));h();export{p as CardRadioStory,m as __namedExportsOrder,f as default,h as n,l as t};
--- client/react/next/assets/ItemMenu.stories-C3h1zbeb.js
+++
@@ -1,7 +0,0 @@
-import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./client-CgOUfDmF.js";import{xt as r}from"./TimelineVerticalCommon-BZHRhpeM.js";var i=t({Playground:()=>o,__namedExportsOrder:()=>s,default:()=>a}),a,o,s,c=e((()=>{n(),a={title:`Components/ItemMenu 🚧`,component:r},o={args:{href:`https://example.com/contracts`,children:`Mes contrats`,isActive:!1}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    href: "https://example.com/contracts",
-    children: "Mes contrats",
-    isActive: false
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Playground`]}));c();export{o as Playground,s as __namedExportsOrder,a as default,c as n,i as t};
