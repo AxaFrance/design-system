b9e9aa730c389840138f2b03f7f2655e70e0e5cd
Remi-DS <Remi-DS@users.noreply.github.com>
2026-09-01 15:18:56 +0000

    deploy: 9115f1940be21aded7c2dbadd29de43a14730c63

---
+++ prospect/react/next/assets/Card-DE_G5A6C.js
@@ -0,0 +1,35 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{r as n}from"./react-BvXQrjDX.js";import{t as r}from"./mdx-react-shim-XboKyHgP.js";import{i,l as a,n as o,s}from"./blocks-DL0JjLvg.js";import{ButtonCard as c,Default as l,Unstyled as u,n as d,t as f}from"./Card.stories-BZPmpUSx.js";function p(e){let t={code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,pre:`pre`,strong:`strong`,...n(),...e.components};return(0,h.jsxs)(h.Fragment,{children:[(0,h.jsx)(s,{of:f,name:`Card`}),`
+`,(0,h.jsx)(t.h1,{id:`card`,children:`Card`}),`
+`,(0,h.jsxs)(t.p,{children:[`The `,(0,h.jsx)(t.strong,{children:`Card`}),` component is a simple wrapper that adds `,(0,h.jsx)(t.strong,{children:`padding and a border`}),`.
+Use the `,(0,h.jsx)(t.code,{children:`as`}),` prop to control the underlying HTML element.`]}),`
+`,(0,h.jsx)(t.h2,{id:`import`,children:`Import`}),`
+`,(0,h.jsx)(t.pre,{children:(0,h.jsx)(t.code,{className:`language-tsx`,children:`import { Card, Heading } from "@axa-fr/canopee-react/prospect";
+`})}),`
+`,(0,h.jsx)(t.h2,{id:`usage`,children:`Usage`}),`
+`,(0,h.jsx)(t.pre,{children:(0,h.jsx)(t.code,{className:`language-tsx`,children:`// Basic card
+<Card>
+  <Heading level={2} className="af-card__title">My card title</Heading>
+  <p>My card content</p>
+</Card>
+
+// Clickable card rendered as a button
+<Card as="button" type="button" onClick={handleClick}>
+  <Heading level={2} className="af-card__title">My card title</Heading>
+  <p>My card content</p>
+</Card>
+
+// Unstyled variant (removes border and padding)
+<Card variant="unstyled">
+  <p>My card content</p>
+</Card>
+`})}),`
+`,(0,h.jsx)(t.h2,{id:`playground`,children:`Playground`}),`
+`,(0,h.jsx)(t.h3,{id:`basic-content`,children:`Basic content`}),`
+`,(0,h.jsx)(o,{of:l}),`
+`,(0,h.jsx)(i,{of:l}),`
+`,(0,h.jsx)(t.h3,{id:`button-card`,children:`Button card`}),`
+`,(0,h.jsx)(o,{of:c}),`
+`,(0,h.jsx)(i,{of:c}),`
+`,(0,h.jsx)(t.h3,{id:`unstyled`,children:`Unstyled`}),`
+`,(0,h.jsx)(o,{of:u}),`
+`,(0,h.jsx)(i,{of:u})]})}function m(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,h.jsx)(t,{...e,children:(0,h.jsx)(p,{...e})}):p(e)}var h;e((()=>{h=t(),r(),a(),d()}))();export{m as default};
---
+++ prospect/react/next/assets/TabMenu.stories-BPgPocQq.js
@@ -0,0 +1,13 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{R as r,t as i}from"./prospect-1QzEDyFx.js";var a=t({Playground:()=>c,__namedExportsOrder:()=>l,default:()=>s}),o,s,c,l,u=e((()=>{i(),o=n(),s={title:`Components/TabMenu 🚧`,component:r},c={render:()=>(0,o.jsx)(r,{items:[{href:`#contracts`,label:`Mes contrats`,isActive:!0},{href:`#claims`,label:`Mes sinistres`},{href:`#documents`,label:`Mes documents`}]})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: () => <TabMenu items={[{
+    href: "#contracts",
+    label: "Mes contrats",
+    isActive: true
+  }, {
+    href: "#claims",
+    label: "Mes sinistres"
+  }, {
+    href: "#documents",
+    label: "Mes documents"
+  }]} />
+}`,...c.parameters?.docs?.source}}},l=[`Playground`]}));u();export{c as Playground,l as __namedExportsOrder,s as default,u as n,a as t};
