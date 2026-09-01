364f8f40bfcfa41f0b821dfbc02754ec1e32241e
samuel-gomez-axa <samuel-gomez-axa@users.noreply.github.com>
2026-09-01 08:24:41 +0000

    deploy: edc4e92422346c03c4ab616ea93f6c664a2cf719

---
+++ prospect/react/next/assets/ContentItemDuoAction.stories-Bh2tzIo0.js
@@ -0,0 +1,17 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{bt as n,kt as r,t as i}from"./prospect-CQEQ6HHB.js";import{n as a,t as o}from"./account_balance_wallet-fill-CATi1BVB.js";var s,c,l,u;e((()=>{a(),i(),s=t(),c={component:n,title:`Components/List/ContentItemDuoAction`,args:{contentItemProps:{type:`icon`,iconProps:{src:o,variant:`primary`,hasBackground:!0},title:`Texte principale`,subtitle1:`Texte secondaire`,subtitle2:`Texte tertiare`},buttons:(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(r,{variant:`ghost`,children:`Modifier`}),(0,s.jsx)(r,{variant:`ghost`,children:`Supprimer`})]})}},l={name:`ContentItemDuoAction`,render:e=>(0,s.jsx)(n,{...e}),argTypes:{state:{options:[`edit`,`toggle`],control:{type:`select`}},toggleProps:{control:{type:`object`}}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: "ContentItemDuoAction",
+  render: (props: ContentItemDuoActionProps) => <ContentItemDuoAction {...props} />,
+  argTypes: {
+    state: {
+      options: ["edit", "toggle"],
+      control: {
+        type: "select"
+      }
+    },
+    toggleProps: {
+      control: {
+        type: "object"
+      }
+    }
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Playground`]}))();export{l as Playground,u as __namedExportsOrder,c as default};
