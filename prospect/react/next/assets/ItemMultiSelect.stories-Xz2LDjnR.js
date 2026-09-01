60d378fc0a154799c4425e365272f2c01e2d88f5
Remi-DS <Remi-DS@users.noreply.github.com>
2026-09-01 15:04:27 +0000

    deploy: 346a2c7e71cbcc10f9e02a1a5187cb8703765702

---
+++ prospect/react/next/assets/ItemMultiSelect.stories-Xz2LDjnR.js
@@ -0,0 +1,18 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{Z as n,t as r}from"./prospect-DIfKjVMA.js";var i=t({Checked:()=>l,Playground:()=>o,Primary:()=>s,Secondary:()=>c,__namedExportsOrder:()=>u,default:()=>a}),a,o,s,c,l,u,d=e((()=>{r(),a={title:`Components/Form/ItemMultiSelect`,component:n,argTypes:{label:{control:{type:`text`}},variant:{options:[`primary`,`secondary`],control:{type:`radio`}},name:{control:{type:`text`}},value:{control:{type:`text`}},checked:{control:{type:`boolean`}}},args:{label:`Option 1`,variant:`primary`,name:`option1`,value:`option1`,checked:!1}},o={name:`Playground`},s={args:{variant:`primary`,label:`Primary background`}},c={args:{variant:`secondary`,label:`Secondary background`}},l={args:{checked:!0,label:`Checked state`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  name: "Playground"
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    label: "Primary background"
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    label: "Secondary background"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    label: "Checked state"
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Playground`,`Primary`,`Secondary`,`Checked`]}));d();export{l as Checked,o as Playground,s as Primary,c as Secondary,u as __namedExportsOrder,a as default,d as n,i as t};
---
+++ prospect/react/next/assets/TagList.stories-C_wOfsKU.js
@@ -0,0 +1,24 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{Rt as r,r as i,t as a}from"./prospect-DIfKjVMA.js";var o=t({CustomThreshold:()=>d,Default:()=>l,WithOverflow:()=>u,__namedExportsOrder:()=>f,default:()=>c}),s,c,l,u,d,f,p=e((()=>{a(),s=n(),c={title:`Components/TagList`,component:i,args:{hideThreshold:2}},l={name:`TagList`,render:e=>(0,s.jsxs)(i,{...e,children:[(0,s.jsx)(r,{children:`Remboursement`}),(0,s.jsx)(r,{children:`Santé`})]})},u={name:`TagList with overflow`,render:e=>(0,s.jsxs)(i,{...e,children:[(0,s.jsx)(r,{children:`Remboursement`}),(0,s.jsx)(r,{children:`Santé`}),(0,s.jsx)(r,{children:`Auto`}),(0,s.jsx)(r,{children:`Habitation`}),(0,s.jsx)(r,{children:`Prévoyance`})]})},d={name:`TagList with custom threshold`,render:e=>(0,s.jsxs)(i,{...e,hideThreshold:3,children:[(0,s.jsx)(r,{children:`Remboursement`}),(0,s.jsx)(r,{children:`Santé`}),(0,s.jsx)(r,{children:`Auto`}),(0,s.jsx)(r,{children:`Habitation`})]})},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: "TagList",
+  render: args => <TagList {...args}>
+      <Tag>Remboursement</Tag>
+      <Tag>Santé</Tag>
+    </TagList>
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: "TagList with overflow",
+  render: args => <TagList {...args}>
+      <Tag>Remboursement</Tag>
+      <Tag>Santé</Tag>
+      <Tag>Auto</Tag>
+      <Tag>Habitation</Tag>
+      <Tag>Prévoyance</Tag>
+    </TagList>
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  name: "TagList with custom threshold",
+  render: args => <TagList {...args} hideThreshold={3}>
+      <Tag>Remboursement</Tag>
+      <Tag>Santé</Tag>
+      <Tag>Auto</Tag>
+      <Tag>Habitation</Tag>
+    </TagList>
+}`,...d.parameters?.docs?.source}}},f=[`Default`,`WithOverflow`,`CustomThreshold`]}));p();export{d as CustomThreshold,l as Default,u as WithOverflow,f as __namedExportsOrder,c as default,p as n,o as t};
