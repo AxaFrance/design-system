a0d4b27a2ac6a5b2191f4a00991ed463e51f990d
JLou <JLou@users.noreply.github.com>
2026-09-14 06:58:05 +0000

    deploy: 1a91e58ac35c3b0c20c43dca5e641ee2663c8787

---
+++ client/react/next/assets/mdx-react-shim-B15J0wCG.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-0ScQ7rYl.js";var n=e((()=>{t()}));export{n as t};
--- client/react/next/assets/mdx-react-shim-NS1yjWX2.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-Djusf7IW.js";var n=e((()=>{t()}));export{n as t};
