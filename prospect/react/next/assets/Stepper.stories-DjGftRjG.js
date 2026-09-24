5c909fe5032927c0bdd2dd3a500c42fc2b595631
GuillaumeKESTEMAN <GuillaumeKESTEMAN@users.noreply.github.com>
2026-09-24 15:05:18 +0000

    deploy: 111c3b803ad0a146a602e831bc6b153763456d1c

---
+++ prospect/react/next/assets/Stepper.stories-DjGftRjG.js
@@ -0,0 +1,12 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-CXZ2plg1.js";import{l as r,t as i}from"./prospect-HPDcGD7c.js";var a=t({Playground:()=>c,__namedExportsOrder:()=>l,default:()=>s}),o,s,c,l,u=e((()=>{i(),o=n(),s={component:r,title:`Components/Stepper`,parameters:{layout:`centered`},argTypes:{messageType:{control:`select`,options:[`error`,`success`]},titleLevel:{control:`select`,options:[1,2,3,4]}},decorators:[e=>(0,o.jsx)(`div`,{style:{minWidth:`70vw`},children:(0,o.jsx)(e,{})})]},c={name:`Playground`,args:{nbSteps:8,currentTitle:`Titre étape`,currentStep:2,currentSubtitle:`Étape X sur X`,currentStepProgress:50,message:`Titre message`,helper:`Sauf mention du contraire, tous les champs sont obligatoires.`}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: "Playground",
+  args: {
+    nbSteps: 8,
+    currentTitle: "Titre étape",
+    currentStep: 2,
+    currentSubtitle: "Étape X sur X",
+    currentStepProgress: 50,
+    message: "Titre message",
+    helper: "Sauf mention du contraire, tous les champs sont obligatoires."
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Playground`]}));u();export{c as Playground,l as __namedExportsOrder,s as default,u as n,a as t};
--- prospect/react/next/assets/mdx-react-shim-BXbWiqlU.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-Dn0ZQ20d.js";var n=e((()=>{t()}));export{n as t};
